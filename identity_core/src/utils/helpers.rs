270d63e7f26ce88b5d9d291584492102affcdb38
tensor-programming <tensordeveloper@gmail.com>
2020-08-14 17:17:54 -0400

    cleanup

--- identity_core/src/utils/helpers.rs
+++ identity_core/src/utils/helpers.rs
@@ -1,5 +1,5 @@
 use serde::{
-    de::{self, MapAccess, SeqAccess, Visitor},
+    de::{self, SeqAccess, Visitor},
     Deserialize, Deserializer,
 };
 
@@ -10,7 +10,6 @@ where
     T: Deserialize<'de> + FromStr<Err = crate::Error>,
     D: Deserializer<'de>,
 {
-    // This is a Visitor that wraps string types in a Vec and deserializes Vecs.
     struct StringOrList<T>(PhantomData<fn() -> T>);
 
     impl<'de, T> Visitor<'de> for StringOrList<T>
@@ -34,55 +33,9 @@ where
         where
             S: SeqAccess<'de>,
         {
-            // deserialize the sequence
             Deserialize::deserialize(de::value::SeqAccessDeserializer::new(seq))
         }
     }
 
     deserializer.deserialize_any(StringOrList(PhantomData))
-}
-
-// Code lifted verbatim from https://serde.rs/string-or-struct.html
-pub fn string_or_struct<'de, T, D>(deserializer: D) -> Result<T, D::Error>
-where
-    T: Deserialize<'de> + FromStr<Err = crate::Error>,
-    D: Deserializer<'de>,
-{
-    // This is a Visitor that forwards string types to T's `FromStr` impl and
-    // forwards map types to T's `Deserialize` impl. The `PhantomData` is to
-    // keep the compiler from complaining about T being an unused generic type
-    // parameter. We need T in order to know the Value type for the Visitor
-    // impl.
-    struct StringOrStruct<T>(PhantomData<fn() -> T>);
-
-    impl<'de, T> Visitor<'de> for StringOrStruct<T>
-    where
-        T: Deserialize<'de> + FromStr<Err = crate::Error>,
-    {
-        type Value = T;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("string or map")
-        }
-
-        fn visit_str<E>(self, value: &str) -> Result<T, E>
-        where
-            E: de::Error,
-        {
-            Ok(FromStr::from_str(value).unwrap())
-        }
-
-        fn visit_map<M>(self, map: M) -> Result<T, M::Error>
-        where
-            M: MapAccess<'de>,
-        {
-            // `MapAccessDeserializer` is a wrapper that turns a `MapAccess`
-            // into a `Deserializer`, allowing it to be used as the input to T's
-            // `Deserialize` implementation. T then deserializes itself using
-            // the entries from the map visitor.
-            Deserialize::deserialize(de::value::MapAccessDeserializer::new(map))
-        }
-    }
-
-    deserializer.deserialize_any(StringOrStruct(PhantomData))
 }
