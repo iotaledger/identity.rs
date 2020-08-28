1a2475fa3c3e4ef8e0172883595dcf48d9afc395
l1h3r <devin.turner09@gmail.com>
2020-08-28 11:26:41 -0700

    Clean up validation

---
+++ identity_vc/src/utils/validation.rs
@@ -0,0 +1,93 @@
+use anyhow::Result;
+
+use crate::{
+  common::{Context, OneOrMany, URI},
+  credential::Credential,
+  error::Error,
+  presentation::Presentation,
+};
+
+pub fn validate_credential_structure(credential: &Credential) -> Result<()> {
+  // Ensure the base context is present and in the correct location
+  validate_context("Credential", &credential.context)?;
+
+  // The set of types MUST contain the base type
+  validate_types("Credential", Credential::BASE_TYPE, &credential.types)?;
+
+  // Ensure the id URI (if provided) adheres to the correct format
+  validate_opt_uri("Credential id", credential.id.as_ref())?;
+
+  // Ensure the issuer URI adheres to the correct format
+  validate_uri("Credential issuer", credential.issuer.uri())?;
+
+  // Credentials MUST have at least one subject
+  ensure!(
+    !credential.credential_subject.is_empty(),
+    Error::MissingCredentialSubject
+  );
+
+  // Each subject is defined as one or more properties - no empty objects
+  for subject in credential.credential_subject.iter() {
+    ensure!(
+      subject.id.is_some() || !subject.properties.is_empty(),
+      Error::InvalidCredentialSubject
+    );
+  }
+
+  Ok(())
+}
+
+pub fn validate_presentation_structure(presentation: &Presentation) -> Result<()> {
+  // Ensure the base context is present and in the correct location
+  validate_context("Presentation", &presentation.context)?;
+
+  // The set of types MUST contain the base type
+  validate_types("Presentation", Presentation::BASE_TYPE, &presentation.types)?;
+
+  // Ensure the id URI (if provided) adheres to the correct format
+  validate_opt_uri("Presentation id", presentation.id.as_ref())?;
+
+  // Ensure the holder URI (if provided) adheres to the correct format
+  validate_opt_uri("Presentation holder", presentation.holder.as_ref())?;
+
+  // Validate all verifiable credentials
+  for credential in presentation.verifiable_credential.iter() {
+    credential.validate()?;
+  }
+
+  Ok(())
+}
+
+pub fn validate_types(name: &'static str, base: &str, types: &OneOrMany<String>) -> Result<()> {
+  ensure!(types.contains(&base.into()), Error::MissingBaseType(name));
+
+  Ok(())
+}
+
+pub fn validate_context(name: &'static str, context: &OneOrMany<Context>) -> Result<()> {
+  // The first Credential/Presentation context MUST be a URI representing the base context
+  match context.get(0) {
+    Some(Context::URI(uri)) if uri == Credential::BASE_CONTEXT => Ok(()),
+    Some(_) => bail!(Error::InvalidBaseContext(name)),
+    None => bail!(Error::MissingBaseContext(name)),
+  }
+}
+
+pub fn validate_uri(name: &'static str, uri: &URI) -> Result<()> {
+  const KNOWN: [&str; 4] = ["did:", "urn:", "http:", "https:"];
+
+  // TODO: Proper URI validation
+  ensure!(
+    KNOWN.iter().any(|scheme| uri.starts_with(scheme)),
+    Error::InvalidURI(name),
+  );
+
+  Ok(())
+}
+
+pub fn validate_opt_uri(name: &'static str, uri: Option<&URI>) -> Result<()> {
+  match uri {
+    Some(uri) => validate_uri(name, uri),
+    None => Ok(()),
+  }
+}
