59298af681a7790b7daef42f134ee0f855113a52
l1h3r <devin.turner09@gmail.com>
2020-08-30 16:27:44 -0700

    Add KeyPair type

---
+++ identity_crypto/src/key_pair.rs
@@ -0,0 +1,34 @@
+use zeroize::Zeroize;
+
+use crate::key::{PublicKey, SecretKey};
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct KeyPair {
+  public: PublicKey,
+  secret: SecretKey,
+}
+
+impl KeyPair {
+  pub fn new(public: PublicKey, secret: SecretKey) -> Self {
+    Self { public, secret }
+  }
+
+  pub fn public(&self) -> &PublicKey {
+    &self.public
+  }
+
+  pub fn secret(&self) -> &SecretKey {
+    &self.secret
+  }
+
+  pub fn to_hex(&self) -> (String, String) {
+    (self.public.to_hex(), self.secret.to_hex())
+  }
+}
+
+impl Zeroize for KeyPair {
+  fn zeroize(&mut self) {
+    self.public.zeroize();
+    self.secret.zeroize();
+  }
+}
--- identity_crypto/src/lib.rs
+++ identity_crypto/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod key;
+pub mod key_pair;
