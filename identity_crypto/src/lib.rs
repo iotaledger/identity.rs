3e08a0581b4bacb35c2639dd3386b3810a3c7207
l1h3r <devin.turner09@gmail.com>
2020-08-30 16:59:07 -0700

    Placeholder types

--- identity_crypto/src/lib.rs
+++ identity_crypto/src/lib.rs
@@ -5,3 +5,11 @@ pub mod error;
 pub mod key;
 pub mod proof;
 pub mod signature;
+
+pub mod identity_core {
+  #[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
+  pub struct Object;
+
+  #[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
+  pub struct Timestamp;
+}
