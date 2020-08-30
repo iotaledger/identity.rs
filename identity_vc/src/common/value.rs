fc7fe3fd2dc2b8f722738a812ec0d6b7af0ee95e
l1h3r <devin.turner09@gmail.com>
2020-08-30 14:29:34 -0700

    Replace custom Value with serde_json::Value

--- identity_vc/src/common/value.rs
+++ identity_vc/src/common/value.rs
@@ -1,54 +1,12 @@
-use std::fmt;
+use std::iter::FromIterator;
+use serde_json::Map;
+
+/// Re-export `Value` from `serde_json` as the catch-all value type.
+///
+/// It's not ONLY compatible with JSON (implements Deserialize/Serialize)
+pub use serde_json::Value;
 
 use crate::common::{Object, URI};
-
-#[derive(Clone, PartialEq, Deserialize, Serialize)]
-#[serde(untagged)]
-pub enum Value {
-  Null,
-  Boolean(bool),
-  Number(Number),
-  String(String),
-  Array(Vec<Self>),
-  Object(Object),
-}
-
-impl fmt::Debug for Value {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match self {
-      Self::Null => write!(f, "Null"),
-      Self::Boolean(inner) => fmt::Debug::fmt(inner, f),
-      Self::Number(inner) => fmt::Debug::fmt(inner, f),
-      Self::String(inner) => fmt::Debug::fmt(inner, f),
-      Self::Array(inner) => fmt::Debug::fmt(inner, f),
-      Self::Object(inner) => fmt::Debug::fmt(inner, f),
-    }
-  }
-}
-
-impl From<()> for Value {
-  fn from(_: ()) -> Self {
-    Self::Null
-  }
-}
-
-impl From<bool> for Value {
-  fn from(other: bool) -> Self {
-    Self::Boolean(other)
-  }
-}
-
-impl From<&'_ str> for Value {
-  fn from(other: &'_ str) -> Self {
-    Self::String(other.into())
-  }
-}
-
-impl From<String> for Value {
-  fn from(other: String) -> Self {
-    Self::String(other)
-  }
-}
 
 impl From<URI> for Value {
   fn from(other: URI) -> Self {
@@ -56,63 +14,14 @@ impl From<URI> for Value {
   }
 }
 
-impl<T> From<T> for Value where T: Into<Number> {
-  fn from(other: T) -> Self {
-    Self::Number(other.into())
-  }
-}
-
-impl<T> From<Vec<T>> for Value
-where
-  T: Into<Value>,
-{
-  fn from(other: Vec<T>) -> Self {
-    Self::Array(other.into_iter().map(Into::into).collect())
+impl From<Object> for Map<String, Value> {
+  fn from(other: Object) -> Self {
+    Map::from_iter(other.into_inner().into_iter())
   }
 }
 
 impl From<Object> for Value {
   fn from(other: Object) -> Self {
-    Self::Object(other)
+    Value::Object(other.into())
   }
 }
-
-#[derive(Clone, Copy, PartialEq, PartialOrd, Deserialize, Serialize)]
-#[serde(untagged)]
-pub enum Number {
-  UInt(u64),
-  SInt(i64),
-  Float(f64),
-}
-
-impl fmt::Debug for Number {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match self {
-      Self::UInt(inner) => fmt::Debug::fmt(inner, f),
-      Self::SInt(inner) => fmt::Debug::fmt(inner, f),
-      Self::Float(inner) => fmt::Debug::fmt(inner, f),
-    }
-  }
-}
-
-macro_rules! impl_number_primitive {
-  ($src:ty, $ident:ident) => {
-    impl_number_primitive!($src as $src, $ident);
-  };
-  ($src:ty as $dst:ty, $ident:ident) => {
-    impl From<$src> for Number {
-      fn from(other: $src) -> Self {
-        Self::$ident(other as $dst)
-      }
-    }
-  };
-  ($($src:ty),* as $dst:ty, $ident:ident) => {
-    $(
-      impl_number_primitive!($src as $dst, $ident);
-    )*
-  };
-}
-
-impl_number_primitive!(u8, u16, u32, u64 as u64, UInt);
-impl_number_primitive!(i8, i16, i32, i64 as i64, SInt);
-impl_number_primitive!(f32, f64 as f64, Float);
