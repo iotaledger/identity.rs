6495fa27b8ec3f535ab9839953d4b36e64ff22c1
l1h3r <devin.turner09@gmail.com>
2020-08-30 18:05:39 -0700

    Add convenience methods to Proof

--- identity_crypto/src/proof/proof.rs
+++ identity_crypto/src/proof/proof.rs
@@ -1,6 +1,6 @@
 use crate::{
   error::Result,
-  key::{PublicKey, SecretKey},
+  key::{KeyGenerator, KeyPair, PublicKey, SecretKey},
   proof::{LinkedDataProof, ProofDocument, ProofOptions},
   signature::SignatureSuite,
 };
@@ -10,6 +10,18 @@ pub struct Proof(Box<dyn SignatureSuite>);
 impl Proof {
   pub fn new(suite: impl SignatureSuite + 'static) -> Self {
     Self(Box::new(suite))
+  }
+
+  pub fn signature_type(&self) -> &'static str {
+    self.0.signature_type()
+  }
+
+  pub fn key_type(&self) -> &'static str {
+    self.0.key_type()
+  }
+
+  pub fn keypair(&self, generator: KeyGenerator) -> Result<KeyPair> {
+    self.0.keypair(generator)
   }
 
   pub fn sign(
