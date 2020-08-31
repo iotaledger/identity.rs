9c90dea24806568d1fb3da547e8858d317654f4d
l1h3r <devin.turner09@gmail.com>
2020-08-30 17:27:01 -0700

    Proof outline

--- identity_crypto/src/proof/proof.rs
+++ identity_crypto/src/proof/proof.rs
@@ -1,3 +1,27 @@
-use crate::signature::SignatureSuite;
+use crate::{
+  signature::SignatureSuite,
+  error::Result,
+  key::{PublicKey, SecretKey},
+  proof::{LinkedDataProof, ProofDocument, ProofOptions},
+};
 
 pub struct Proof(Box<dyn SignatureSuite>);
+
+impl Proof {
+  pub fn new(suite: impl SignatureSuite + 'static) -> Self {
+    Self(Box::new(suite))
+  }
+
+  pub fn sign(
+    &self,
+    document: &dyn ProofDocument,
+    secret: &SecretKey,
+    mut options: ProofOptions,
+  ) -> Result<LinkedDataProof> {
+    todo!()
+  }
+
+  pub fn verify(&self, document: &dyn ProofDocument, proof: &LinkedDataProof, public: &PublicKey) -> Result<bool> {
+    todo!()
+  }
+}
