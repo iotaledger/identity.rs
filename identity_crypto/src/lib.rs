f4e5b097024b44ea1b378f5c9d91eb02158b38a1
l1h3r <devin.turner09@gmail.com>
2020-08-30 20:27:50 -0700

    Update placeholder types

--- identity_crypto/src/lib.rs
+++ identity_crypto/src/lib.rs
@@ -11,9 +11,18 @@ pub mod signature;
 pub mod utils;
 
 pub mod identity_core {
-  #[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
-  pub struct Object;
+  // #[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
+  pub type Object = std::collections::HashMap<String, Value>;
+
+  #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
+  pub enum Value {}
 
   #[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
   pub struct Timestamp;
+
+  impl Timestamp {
+    pub fn now() -> Self {
+      Self
+    }
+  }
 }
