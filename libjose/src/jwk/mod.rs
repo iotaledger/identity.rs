fc7860d37f3da5cd256cb0f84ab2056e5bf7594c
l1h3r <devin.turner@iota.org>
2020-09-16 13:46:03 -0700

    Add jwk definition

--- libjose/src/jwk/mod.rs
+++ libjose/src/jwk/mod.rs
@@ -1,7 +1,9 @@
 //! JSON Web Keys ([JWK](https://tools.ietf.org/html/rfc7517))
 
+mod key;
 mod key_params;
 mod key_type;
 
+pub use self::key::*;
 pub use self::key_params::*;
 pub use self::key_type::*;
