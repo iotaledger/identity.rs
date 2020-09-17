d83a6e0d39d230669e601bfefc6d05475167b93b
tensor-programming <tensordeveloper@gmail.com>
2020-09-17 12:11:58 -0400

    fix spacing.

--- identity_core/src/common/macros.rs
+++ identity_core/src/common/macros.rs
@@ -1,32 +1,32 @@
 #[macro_export]
 macro_rules! object {
-  () => {
-    $crate::common::Object::default()
-  };
-  ($($key:ident : $value:expr),* $(,)*) => {
-    {
-      let mut object = ::std::collections::HashMap::new();
+    () => {
+      $crate::common::Object::default()
+    };
+    ($($key:ident : $value:expr),* $(,)*) => {
+      {
+        let mut object = ::std::collections::HashMap::new();
 
-      $(
-        object.insert(
-          stringify!($key).to_string(),
-          $crate::common::Value::from($value),
-        );
-      )*
+        $(
+          object.insert(
+            stringify!($key).to_string(),
+            $crate::common::Value::from($value),
+          );
+        )*
 
-      $crate::common::Object::from(object)
-    }
-  };
+        $crate::common::Object::from(object)
+      }
+    };
 }
 
 // create a line error with the file and the line number.  Good for debugging.
 
 #[macro_export]
 macro_rules! line_error {
-  () => {
-    concat!("Error at ", file!(), ":", line!())
-  };
-  ($string:expr) => {
-    concat!($string, " @", file!(), ":", line!())
-  };
+    () => {
+        concat!("Error at ", file!(), ":", line!())
+    };
+    ($string:expr) => {
+        concat!($string, " @", file!(), ":", line!())
+    };
 }
