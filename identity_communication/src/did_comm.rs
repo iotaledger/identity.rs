c155f11717ea010e0428ac103058b3e80fee377d
huhn511 <sehe89@gmail.com>
2020-09-17 15:10:24 +0200

    add correct type and impolement ToString and FromStr

--- identity_communication/src/did_comm.rs
+++ identity_communication/src/did_comm.rs
@@ -1,7 +1,11 @@
 use identity_core::did::DID;
-#[derive(Debug, PartialEq, Default, Eq, Clone)]
+use serde::{Deserialize, Serialize};
+use std::{str::FromStr};
+
+#[derive(Debug, Deserialize, Serialize, PartialEq, Default, Eq, Clone)]
 pub struct DIDComm {
     pub id: String, // MUST be unique to the sender
+    #[serde(rename = "type")]
     pub comm_type: String, // MUST be a valid Message Type URI
     pub from: Option<DID>, // MUST be a string that is a valid DID which identifies the sender of the message
     pub to: Option<Vec<DID>>,// MUST be an array of strings where each element is a valid DID
@@ -22,6 +26,23 @@ impl DIDComm {
             expires_at: self.expires_at,
             body: self.body
         };
+        Ok(did_comm)
+    }
+}
+
+/// converts a `DIDComm` Message into a string using the `to_string()` method.
+impl ToString for DIDComm {
+    fn to_string(&self) -> String {
+        serde_json::to_string(&self).expect("Unable to serialize DIDComm Message")
+    }
+}
+
+/// takes a &str and converts it into a `DIDComm` Message given the proper format.
+impl FromStr for DIDComm {
+    type Err = crate::Error;
+
+    fn from_str(s: &str) -> crate::Result<Self> {
+        let did_comm = serde_json::from_str(s).expect("Unable to build DIDComm Message");
         Ok(did_comm)
     }
 }
