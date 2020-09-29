52d1d993e13571d64df1b60c5f6b9228b4592ec3
tensor-programming <tensordeveloper@gmail.com>
2020-09-29 17:54:08 -0400

    fix imports on macro.

--- identity_account/src/macros.rs
+++ identity_account/src/macros.rs
@@ -2,9 +2,8 @@
 ///
 /// # Example
 /// ```
-/// use identity_account::{cache, storage::Cache};
-/// use once_cell::sync::Lazy;
-/// use std::sync::Mutex;
+/// use identity_account::cache;
+///
 /// cache! {
 ///    fn fib(n: u32) -> u32 => {
 ///      match n {
@@ -20,6 +19,10 @@
 #[macro_export]
 macro_rules! cache {
     (fn $name:ident ($($arg:ident: $arg_type:ty), *) -> $ret:ty => $body:expr) => {
+        use once_cell::sync::Lazy;
+        use std::sync::Mutex;
+        use identity_account::storage::Cache;
+
         #[allow(unused_parens)]
         fn $name($($arg: $arg_type), *) -> $ret {
             // create a static instance of `Cache<K, V>`
