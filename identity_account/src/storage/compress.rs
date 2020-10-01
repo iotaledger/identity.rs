2f92934459420e8d415d7209da672625685475ed
tensor-programming <tensordeveloper@gmail.com>
2020-10-01 03:27:29 -0400

    fix proptests.

--- identity_account/src/storage/compress.rs
+++ identity_account/src/storage/compress.rs
@@ -175,7 +175,7 @@ mod tests {
 
     #[test]
     fn test_encode_decode() {
-        let expected = "a";
+        let expected = "qwoieuqpowijdaosijdloaisjdoiuqwoiuhidnuaynwciuaywgdniuacygdniuaygwdiuaygwdnuiaygsndiuyascgniduygaasduygaiusygdgggdsauygd";
 
         let compressed = HuffmanCodec::compress(expected.into()).unwrap();
 
@@ -184,26 +184,26 @@ mod tests {
         assert_eq!(expected, decompressed);
     }
 
-    // proptest! {
-    //     #[test]
-    //     fn prop_check_encode_decode(s in "[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+") {
-    //         let expected = String::from(&s);
+    proptest! {
+        #[test]
+        fn prop_check_encode_decode(s in "[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+") {
+            let expected = String::from(&s);
 
-    //         let compressed = HuffmanCodec::compress(s).unwrap();
-    //         let decompressed = HuffmanCodec::decompress(&compressed).unwrap();
+            let compressed = HuffmanCodec::compress(s).unwrap();
+            let decompressed = HuffmanCodec::decompress(&compressed).unwrap();
 
-    //         assert_eq!(expected, decompressed);
+            assert_eq!(expected, decompressed);
 
-    //     }
+        }
 
-    //     #[test]
-    //     fn prop_test_encode_decode_len(s in "[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+") {
-    //         let expected = s.len();
+        #[test]
+        fn prop_test_encode_decode_len(s in "[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+[a-zA-Z0-9._!~$&'()*+;,=/?:@-]+") {
+            let expected = s.len();
 
-    //         let compressed = HuffmanCodec::compress(s).unwrap();
-    //         let decompressed = HuffmanCodec::decompress(&compressed).unwrap();
+            let compressed = HuffmanCodec::compress(s).unwrap();
+            let decompressed = HuffmanCodec::decompress(&compressed).unwrap();
 
-    //         assert_eq!(expected, decompressed.len());
-    //     }
-    // }
+            assert_eq!(expected, decompressed.len());
+        }
+    }
 }
