2926e32c5e2e6764c559085abf4f9da8d83814bf
Thoralf-M <thoralf.mue@gmail.com>
2020-10-07 20:49:51 +0200

    address more comments

--- identity_core/src/did/mod.rs
+++ identity_core/src/did/mod.rs
@@ -1,13 +1,11 @@
 #[allow(clippy::module_inception)]
 mod did;
 mod document;
+mod io;
 mod parser;
-mod reader_writer;
-mod resolver;
 mod types;
 
 pub use did::*;
 pub use document::*;
-pub use reader_writer::*;
-pub use resolver::*;
+pub use io::*;
 pub use types::*;
