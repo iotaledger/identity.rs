29e1789d41503313c11e936a11c2f3a517e603b5
l1h3r <devin.turner@iota.org>
2020-10-21 18:38:03 -0700

    Add TangleDID type

--- identity_iota/src/io/writer.rs
+++ identity_iota/src/io/writer.rs
@@ -10,7 +10,7 @@ use serde_json::to_string;
 use std::{thread, time::Duration};
 
 use crate::{
-    did::create_address,
+    did::deprecated::create_address,
     error::{DocumentError, Error, Result, TransactionError},
     network::{Network, NodeList},
     utils::{create_address_from_trits, encode_trits, txn_hash},
