7fd0c6ff49bb6439694635977284383d948399f8
huhn511 <sehe89@gmail.com>
2020-11-07 16:04:08 +0100

    export wasm functions, publish to 0.0.5

--- libraries/wasm/web.js
+++ libraries/wasm/web.js
@@ -2,7 +2,7 @@ import initWasm, * as wasm from './web/iota_identity_wasm'
 let __initializedIotaWasm = false
 
 
-function __getLib() {
+export default function identity() {
     if (__initializedIotaWasm) {
         return Promise.resolve(wasm)
     }
@@ -18,14 +18,14 @@ function __getLib() {
  * Get a Ed25519 keypair
  */
 function newKey() {
-    return __getLib().then(lib => new lib.Key())
+    return identity().then(lib => new lib.Key())
 }
 
 /**
  * Generate a DID from a public key
  */
 function newDID(publicKey) {
-    return __getLib().then(lib => new lib.DID(publicKey))
+    return identity().then(lib => new lib.DID(publicKey))
 }
 
 export {
