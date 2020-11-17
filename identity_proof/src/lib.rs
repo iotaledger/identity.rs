9cff8adaa9b3ae8a046712dd8830fcef9f1c038f
l1h3r <devin.turner@iota.org>
2020-11-17 14:38:00 -0800

    Move identity_proof to identity_core::proof

--- identity_proof/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod document;
-pub mod error;
-pub mod signature;
-
-pub use document::{HasProof, LdDocument, LdRead, LdWrite};
-pub use error::{Error, Result};
-pub use signature::{LdSignature, SignatureData, SignatureOptions, SignatureValue};
