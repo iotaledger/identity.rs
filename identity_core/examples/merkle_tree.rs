0e8b9bb287bfe96534ab582d289e9b1c17c52460
l1h3r <devin.turner@iota.org>
2021-01-21 10:10:39 -0800

    Remove timer from example. Add comments

--- identity_core/examples/merkle_tree.rs
+++ identity_core/examples/merkle_tree.rs
@@ -11,22 +11,8 @@ use identity_core::proof::JcsEd25519Signature2020;
 use rand::rngs::OsRng;
 use rand::Rng;
 use sha2::Sha256;
-use std::time::Instant;
 
 const LEAVES: usize = 1 << 8;
-
-struct Timer(Instant);
-
-impl Timer {
-    fn new() -> Self {
-        Self(Instant::now())
-    }
-
-    fn step(&mut self, label: &str) {
-        println!("{}: {:?}", label, self.0.elapsed());
-        self.0 = Instant::now();
-    }
-}
 
 fn generate_leaves(count: usize) -> Vec<KeyPair> {
     (0..count).map(|_| JcsEd25519Signature2020::new_keypair()).collect()
@@ -47,32 +33,37 @@ where
 
 fn main() -> Result<()> {
     let mut digest: Sha256 = Sha256::new();
-    let mut timer: Timer = Timer::new();
 
+    // Choose a random index from 0..LEAVES.
+    //
+    // We will generate a proof-of-inclusion for this public key.
     let index: usize = OsRng.gen_range(0, LEAVES);
 
     println!("Target Leaves: {}", LEAVES);
     println!("Target Index:  {}", index);
 
+    // Generate a list of keypairs to use for the Merkle tree.
     let kpairs: Vec<KeyPair> = generate_leaves(LEAVES);
-    timer.step("Keys");
 
+    // Hash all keypairs with SHA-256.
     let leaves: _ = kpairs.iter().map(KeyPair::public);
     let hashes: Vec<Hash<Sha256>> = generate_hashes(&mut digest, leaves);
-    timer.step("Hash");
 
+    // Construct the Merkle tree from the list of hashes.
     let tree: MTree<Sha256> = MTree::from_leaves(&hashes).unwrap();
-    timer.step("Tree");
-
-    let proof: Proof<Sha256> = tree.proof(index).unwrap();
-    timer.step("Proof");
-
-    let target: Hash<Sha256> = digest.hash_data(kpairs[index].public().as_ref());
-    let verified: bool = tree.verify(&proof, target);
-    timer.step("Verify");
-
     println!("Merkle Tree: {:#?}", tree);
+
+    // Generate a proof-of-inclusion for the leaf node at the specified index.
+    let proof: Proof<Sha256> = tree.proof(index).unwrap();
     println!("Inclusion Proof: {:#?}", proof);
+
+    // Hash the target public key with SHA-256.
+    let target: Hash<Sha256> = digest.hash_data(kpairs[index].public().as_ref());
+    println!("Target Hash: {:?}", target);
+
+    // Use the generated proof to verify inclusion of the target hash in the
+    // Merkle tree.
+    let verified: bool = tree.verify(&proof, target);
     println!("Proof Verified: {:#?}", verified);
 
     Ok(())
