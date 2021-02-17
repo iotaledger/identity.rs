f52ff73229f5466dedf28ba78679647bcbd60567
l1h3r <devin.turner@iota.org>
2021-02-17 10:57:46 -0800

    Update web example

--- bindings/wasm/examples/web.js
+++ bindings/wasm/examples/web.js
@@ -1,143 +1,134 @@
-import("../pkg/index.js").then(async identity => {
-    try {
-        console.log(identity)
-        const { resolve, publish, checkCredential, Key, Doc, DID, PubKey, VerifiableCredential } = identity
+async function run(Identity) {
+  console.log(Identity)
 
-        await playground()
-        await alice_bob()
-        await testVC()
-        restore_keypair()
+  const {
+    Digest,
+    DID,
+    Document,
+    KeyCollection,
+    KeyPair,
+    KeyType,
+    Method,
+    VerifiableCredential,
+    VerifiablePresentation,
+  } = Identity
 
-        function restore_keypair() {
-            let secret = "Bac8bArn1tX9rrqawk9cWM6aK5KHNbhrvnV1VzBXMDrF"
-            let public = "7zwMYWKnoUJKArSxigqk5kHoCVw6vhsrWoKDRTcoXuhj"
-            let keypair = Key.fromBase58(secret, public)
-            console.log(keypair);
-        }
+  const CLIENT_CONFIG = {
+    network: "main",
+    node: "https://nodes.thetangle.org:443",
+  }
 
-        async function testVC() {
-            var { key, doc } = Doc.generateEd25519("main")
-            doc.sign(key)
-            let issuerKey = key
-            let issuerDoc = doc
-            console.log("vc issuer doc published https://explorer.iota.org/mainnet/transaction/" + await publish(doc.toJSON(), { node: "https://nodes.thetangle.org:443", network: "main" }))
-            var { key, doc } = Doc.generateEd25519("main")
-            doc.sign(key)
-            let subjectDoc = doc
-            console.log("vc subject doc published https://explorer.iota.org/mainnet/transaction/" + await publish(doc.toJSON(), { node: "https://nodes.thetangle.org:443", network: "main" }))
-            let credentialSubject = {
-                id: subjectDoc.id,
-                name: "Subject",
-                degree: {
-                    name: "Bachelor of Science and Arts",
-                    type: "BachelorDegree"
-                }
-            }
-            let vc = new VerifiableCredential(
-                issuerDoc,
-                issuerKey,
-                credentialSubject,
-                "UniversityDegreeCredential",
-                "http://example.edu/credentials/3732",
-            );
-            console.log("vc", vc.toJSON());
-            console.log("vc valid: ", await checkCredential(vc.toString(), { node: "https://nodes.thetangle.org:443", network: "main" }))
-            let vc_fromJson = VerifiableCredential.fromJSON(vc.toJSON())
-            console.log("vc_fromJson: ", vc_fromJson);
-            console.log("vc_fromJson valid: ", await checkCredential(vc_fromJson.toString(), { node: "https://nodes.thetangle.org:443", network: "main" }))
-        }
+  function generateUser(name) {
+    const { doc, key } = new Document(KeyType.Ed25519)
 
-        async function playground() {
-            console.log("key", Key.generateEd25519())
-
-            console.log("did", new DID(Key.generateEd25519()))
-
-            console.log("did", new DID(Key.generateEd25519(), "main"))
-
-            const { key, doc } = Doc.generateEd25519("main")
-
-            console.log("key (generated)", key)
-            console.log("doc (generated)", doc)
-
-            console.log("doc (unsigned)", doc.toJSON())
-
-            doc.sign(key)
-
-            console.log("doc (signed)", doc.toJSON())
-
-            console.log("doc valid?", doc.verify())
-
-            console.log("From JSON >", Doc.fromJSON(doc.toJSON()))
-
-            console.log("published https://explorer.iota.org/mainnet/transaction/" + await publish(doc.toJSON(), { node: "https://nodes.thetangle.org:443", network: "main" }))
-            console.log("resolved", await resolve(doc.id, { node: "https://nodes.thetangle.org:443", network: "main" }))
-        }
-
-        function alice_bob() {
-            // Generate Keypairs
-            const alice_keypair = Key.generateEd25519()
-            console.log("alice_keypair: ", alice_keypair)
-
-            const bob_keypair = Key.generateEd25519()
-            console.log("bob_keypair: ", bob_keypair)
-
-            // Create the DIDs
-            let alice_did = new DID(alice_keypair)
-            console.log("alice_did: ", alice_did.toString(), alice_did.address)
-
-            let bob_did = new DID(bob_keypair)
-            console.log("bob_did: ", bob_did.toString(), bob_did.address)
-
-            // Create the DID Documents
-            let alice_pubkey = PubKey.generateEd25519(alice_did, alice_keypair.public)
-            let alice_document = new Doc(alice_pubkey)
-            console.log("alice_document: ", alice_document.toJSON())
-
-            let bob_pubkey = PubKey.generateEd25519(bob_did, bob_keypair.public)
-            let bob_document = new Doc(bob_pubkey)
-            console.log("bob_document: ", bob_document.toJSON())
-
-            let update = { ...bob_document.toJSON() }
-
-            update["foo"] = 123
-            update["bar"] = 456
-            update = Doc.fromJSON(update)
-
-            console.log("Update: ", update)
-
-            let diff = bob_document.diff(update, bob_keypair)
-
-            console.log("diff: ", JSON.stringify(diff, null, 2))
-
-            let json = JSON.stringify(diff)
-
-            console.log("Diff has valid signature: ", bob_document.verifyDiff(json))
-
-            bob_document.updateService(DID.parse(bob_document.toJSON().id + "#messages"), "https://example.com/messages/8377464", "MessagingService")
-            console.log("Doc with service ", bob_document.toJSON());
-            bob_document.clearServices()
-            console.log("Doc with services cleared ", bob_document.toJSON());
-            let keypair = Key.generateEd25519();
-            let publicKey = PubKey.generateEd25519(bob_document.did, keypair.public, "#keys-1")
-            bob_document.updatePublicKey(publicKey)
-            console.log("Doc with public key ", bob_document.toJSON());
-            bob_document.updatePublicKey(publicKey)
-            bob_document.updateAuth(publicKey)
-            bob_document.updateAssert(publicKey)
-            bob_document.updateVerification(publicKey)
-            bob_document.updateDelegation(publicKey)
-            bob_document.updateInvocation(publicKey)
-            bob_document.updateAgreement(publicKey)
-            bob_document.updateTime()
-            console.log("Doc with A LOT", bob_document.toJSON());
-            let bob_auth_key = bob_document.resolveKey(0, "Authentication")
-            console.log("bob_auth_key: ", bob_auth_key);
-            setTimeout(() => {
-                bob_document.updateTime()
-                console.log("bob_document with updated time: ", bob_document.toJSON())
-            }, 1000)
-        }
-    } catch (e) {
-        console.error(e)
+    return {
+      doc,
+      key,
+      name,
     }
-});
+  }
+
+  // Generate a KeyPair, DID, and Document for Alice and Bob
+  const user1 = generateUser("Alice")
+  const user2 = generateUser("Bob")
+
+  console.log("User (user1): ", user1)
+  console.log("User (user2): ", user2)
+
+  // Add a Merkle Key Collection method for Bob, so compromised keys can be revoked.
+  const keys = new KeyCollection(KeyType.Ed25519, 8)
+  const method = Method.createMerkleKey(Digest.Sha256, user2.doc.id, keys, "key-collection")
+
+  // Add to the DID Document as a general-purpose verification method
+  user2.doc.insertMethod(method, "VerificationMethod")
+
+  // Sign all DID documents
+  user1.doc.sign(user1.key)
+  user2.doc.sign(user2.key)
+
+  console.log("Verified (user1): ", user1.doc.verify())
+  console.log("Verified (user2): ", user2.doc.verify())
+
+  user1.message = await Identity.publish(user1.doc.toJSON(), CLIENT_CONFIG)
+  user2.message = await Identity.publish(user2.doc.toJSON(), CLIENT_CONFIG)
+
+  // Publish all DID documents
+  console.log(`Publish Result (user1): https://explorer.iota.org/mainnet/transaction/${user1.message}`)
+  console.log(`Publish Result (user2): https://explorer.iota.org/mainnet/transaction/${user2.message}`)
+
+  // Prepare a credential subject indicating the degree earned by Alice
+  let credentialSubject = {
+    id: user1.doc.id.toString(),
+    name: user1.name,
+    degree: {
+      name: "Bachelor of Science and Arts",
+      type: "BachelorDegree",
+    }
+  }
+
+  // Issue an unsigned `UniversityDegree` credential for Alice
+  const unsignedVc = VerifiableCredential.extend({
+    id: "http://example.edu/credentials/3732",
+    type: "UniversityDegreeCredential",
+    issuer: user2.doc.id.toString(),
+    credentialSubject,
+  })
+
+  // Sign the credential with Bob's Merkle Key Collection method
+  const signedVc = user2.doc.signCredential(unsignedVc, {
+    method: method.id.toString(),
+    public: keys.public(0),
+    secret: keys.secret(0),
+    proof: keys.merkleProof(Digest.Sha256, 0),
+  })
+
+  // Ensure the credential signature is valid
+  console.log("Verifiable Credential", signedVc)
+  console.log("Verified (credential)", user2.doc.verify(signedVc))
+
+  // Check the validation status of the Verifiable Credential
+  console.log("Credential Validation", await Identity.checkCredential(signedVc.toString(), CLIENT_CONFIG))
+
+  // Create a Verifiable Presentation from the Credential - signed by Alice's key
+  const unsignedVp = new VerifiablePresentation(user1.doc, signedVc.toJSON())
+
+  const signedVp = user1.doc.signPresentation(unsignedVp, {
+    method: "#key",
+    secret: user1.key.secret,
+  })
+
+  // Check the validation status of the Verifiable Presentation
+  console.log("Presentation Validation", await Identity.checkPresentation(signedVp.toString(), CLIENT_CONFIG))
+
+  // Bobs key was compromised - mark it as revoked and publish an update
+  user2.doc.revokeMerkleKey(method.id.toString(), 0)
+
+  user2.doc = Document.fromJSON({
+    previous_message_id: user2.message,
+    ...user2.doc.toJSON()
+  })
+
+  // The "authentication" key was not compromised so it's safe to publish an update
+  user2.doc.sign(user2.key)
+
+  user2.message = await Identity.publish(user2.doc.toJSON(), CLIENT_CONFIG)
+
+  console.log("Publish Result (user2): https://explorer.iota.org/mainnet/transaction/" + user2.message)
+
+  // Resolve DID documents
+  console.log("Resolve Result (user1): ", await Identity.resolve(user1.doc.id.toString(), CLIENT_CONFIG))
+  console.log("Resolve Result (user2): ", await Identity.resolve(user2.doc.id.toString(), CLIENT_CONFIG))
+
+  // Check the validation status of the Verifiable Presentation
+  //
+  // This should return `false` since we revoked the key used to sign the credential
+  console.log("Presentation Validation", await Identity.checkPresentation(signedVp.toString(), CLIENT_CONFIG))
+}
+
+import("../pkg/index.js").then(async identity => {
+  try {
+    await run(identity)
+  } catch (e) {
+    console.error(e)
+  }
+})
