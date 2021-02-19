51935c63465ebe84e16634fc4e55d286f3a380cf
l1h3r <devin.turner@iota.org>
2021-02-19 08:33:52 -0800

    cargo fmt

--- identity/src/lib.rs
+++ identity/src/lib.rs
@@ -7,14 +7,13 @@
 //!
 //! # Example
 //! ```
+//! use identity::crypto::KeyPair;
 //! use identity::iota::Client;
 //! use identity::iota::IotaDocument;
-//! use identity::crypto::KeyPair;
 //! use identity::iota::Result;
 //!
 //! #[smol_potat::main]
 //! async fn main() -> Result<()> {
-//! 
 //!   // Create a client to interact with the IOTA Tangle.
 //!   let client: Client = Client::new()?;
 //!
@@ -29,23 +28,25 @@
 //!
 //!   // Use the client to publish the DID Document to the IOTA Tangle.
 //!   let transaction: _ = client.publish_document(&document).await?;
-//! 
+//!
 //!   // Print the DID Document transaction link.
 //!   println!("DID Document Transaction > {}", client.transaction_url(&transaction));
 //!
 //!   Ok(())
 //! }
-//!
 //! ```
-//! 
+//!
 //! **Output**: Example DID Document in the [Tangle Explorer](https://explorer.iota.org/mainnet/transaction/LESUXJUMJCOWGHU9CQQUIHCIPYELOBMHZT9CHCYHJPO9BONQ9IQIFJSREYNOCTYCTQYBHBMBBWJJZ9999).
-//! 
+//!
 //! # Documentation & Community Resources
 //! - [identity.rs](https://github.com/iotaledger/identity.rs): Rust source code of this library on GitHub.
-//! - [Identity Documentation Pages](https://identity.docs.iota.org/welcome.html): Supplementing documentation with simple examples on library usage to get you started.
-//! - [More Examples](https://github.com/iotaledger/identity.rs/tree/dev/examples): Practical examples to get started with the library.
-//! - [IOTA Identity Experience Team Website](https://iota-community.github.io/X-Team_IOTA_Identity/): Website of aforementioned team.
-//! 
+//! - [Identity Documentation Pages](https://identity.docs.iota.org/welcome.html): Supplementing documentation with
+//!   simple examples on library usage to get you started.
+//! - [More Examples](https://github.com/iotaledger/identity.rs/tree/dev/examples): Practical examples to get started
+//!   with the library.
+//! - [IOTA Identity Experience Team Website](https://iota-community.github.io/X-Team_IOTA_Identity/): Website of
+//!   aforementioned team.
+//!
 //! # Structure (Temporary)
 //!
 //! - Resources
@@ -53,9 +54,7 @@
 //!   - X-Team
 //! - Simple Example
 //! - Architecture/Overview
-//! - Get 
-//! 
-//! 
+//! - Get
 
 #![warn(
   rust_2018_idioms,
