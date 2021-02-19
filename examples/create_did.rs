b5e57625eb96c521e661d172c65369d5521ac9fe
l1h3r <devin.turner@iota.org>
2021-02-19 08:30:49 -0800

    fix create_did example

--- examples/create_did.rs
+++ examples/create_did.rs
@@ -1,25 +1,30 @@
-use identity::iota::Client;
-use identity::iota::IotaDocument;
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use identity::crypto::KeyPair;
-
+use identity::iota::Client;
+use identity::iota::Document;
+use identity::iota::Network;
 use identity::iota::Result;
-
+use identity::iota::TangleRef;
 
 #[smol_potat::main]
 async fn main() -> Result<()> {
   let client: Client = Client::new()?;
 
-  let (mut document, keypair): (IotaDocument, KeyPair) = IotaDocument::builder()
-    .authentication_tag("key-1")
-    .did_network(client.network().as_str())
-    .build()?;
+  let keypair: KeyPair = KeyPair::new_ed25519()?;
+  let mut document: Document = Document::from_keypair(&keypair)?;
 
   // Sign the DID Document with the default authentication key.
   document.sign(keypair.secret())?;
 
   // Use the client to publish the DID Document to the Tangle.
-  let transaction: _ = client.publish_document(&document).await?;
-  println!("DID Document Transaction > {}", client.transaction_url(&transaction));
+  document.publish(&client).await?;
+
+  let network: Network = document.id().into();
+  let explore: String = format!("{}/transaction/{}", network.explorer_url(), document.message_id());
+
+  println!("DID Document Transaction > {}", explore);
 
   Ok(())
 }
