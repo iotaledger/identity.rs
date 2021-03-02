0e930c4f9f6d694cd9f36f63af543997b648fd7f
l1h3r <devin.turner@iota.org>
2021-03-02 09:21:25 -0800

    hint helpers

--- identity-account/src/stronghold/mod.rs
+++ identity-account/src/stronghold/mod.rs
@@ -1,6 +1,7 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+mod hint;
 mod records;
 mod result;
 mod runtime;
@@ -10,6 +11,8 @@ mod vault;
 
 pub(crate) use self::runtime::Runtime;
 
+pub use self::hint::default_hint;
+pub use self::hint::hint;
 pub use self::records::RecordIndex;
 pub use self::records::RecordTag;
 pub use self::records::Records;
