433339d6b536f254e3bf6494713de7405be9edcf
Sven Feuchtmüller <sven.feuchtmueller@gmx.de>
2021-03-16 19:59:27 +0100

    added issue_degree for further examples

--- examples/common.rs
+++ examples/common.rs
@@ -7,6 +7,12 @@ use identity::crypto::KeyPair;
 use identity::iota::Client;
 use identity::iota::Document;
 use identity::iota::Result;
+use identity::core::json;
+use identity::core::FromJson;
+use identity::core::Url;
+use identity::credential::Credential;
+use identity::credential::CredentialBuilder;
+use identity::credential::Subject;
 
 // A helper function to generate a new DID Document/KeyPair, sign the
 // document, publish it to the Tangle, and return the Document/KeyPair.
@@ -26,4 +32,27 @@ pub async fn create_did_document(client: &Client) -> Result<(Document, KeyPair)>
 
   // Return document and keypair.
   Ok((document, keypair))
+}
+
+// Helper that takes two DID Documents (identities) for issuer and subject, and
+// creates a credential with claims about subject by issuer.
+pub fn issue_degree(issuer: &Document, subject: &Document) -> Result<Credential> {
+
+  // Create VC "subject" field containing subject ID and claims about it.
+  let subject: Subject = Subject::from_json_value(json!({
+    "id": subject.id().as_str(),
+    "degree": {
+      "type": "BachelorDegree",
+      "name": "Bachelor of Science and Arts"
+    }
+  }))?;
+
+  // Build credential using subject above and issuer.
+  let credential: Credential = CredentialBuilder::default()
+    .issuer(Url::parse(issuer.id().as_str())?)
+    .type_("UniversityDegreeCredential")
+    .subject(subject)
+    .build()?;
+
+  Ok(credential)
 }
