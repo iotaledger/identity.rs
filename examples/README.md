8e6690a68ae53541e1cc62502729e7b1490faad6
Sven Feuchtmüller <sven.feuchtmueller@gmx.de>
2021-03-18 15:50:51 +0100

    added "manipulate_did_documents"

--- examples/README.md
+++ examples/README.md
@@ -24,14 +24,9 @@ The following examples are currently available:
 | :--: | :----------------------------------------------------- | :------------------------------------------------------------------------------------------------------------------------- |
 | 1    | [getting_started](getting_started.rs)                  | Introductory example for you to test whether the library is set up / working properly and compiles.                        |
 | 2    | [create_did_document](create_did_document.rs)          | A basic example that generates and publishes a DID Document, the fundamental building block for decentralized identity.    |
-| 3    | [verifiable_credential](verifiable_credential.rs)      | A basic example that generates and publishes subject and issuer DID Documents, then creates a Verifiable Credential (vc) specifying claims about the subject, and retrieves information through the CredentialValidator API. |
-| 4    | [verifiable_presentation](verifiable_presentation.rs)  | This example explains how to create a Verifiable Presentation from a set of credentials and sign it. |
-| 5    | [resolution](resolution.rs)                            | A basic example that generates a DID Document, publishes it to the Tangle, and retrieves information through DID Document resolution/dereferencing. |
-| 6    | [diff_chain](diff_chain.rs)                            | An example that utilizes a diff and auth chain to publish updates to a DID Document. |
-| 7    | [merkle_key](merkle_key.rs)                            | tbd                        |
-| 8    | [manipulate_did_documents](manipulate_did_document.rs) | tbd                        |
-| 8    | [manipulate_verifiable_credentials](manipulate_verifiable_credentials.rs) | tbd                        |
-| 9    | [create_keys](create_keys.rs)                          | tbd                        |
-
-
-
+| 3    | [manipulate_did_documents](manipulate_did_document.rs) | This example goes into more detail regarding the useage of DID Documents. |
+| 4    | [verifiable_credential](verifiable_credential.rs)      | A basic example that generates and publishes subject and issuer DID Documents, then creates a Verifiable Credential (vc) specifying claims about the subject, and retrieves information through the CredentialValidator API. |
+| 5    | [verifiable_presentation](verifiable_presentation.rs)  | This example explains how to create a Verifiable Presentation from a set of credentials and sign it. |
+| 6    | [resolution](resolution.rs)                            | A basic example that generates a DID Document, publishes it to the Tangle, and retrieves information through DID Document resolution/dereferencing. |
+| 7    | [diff_chain](diff_chain.rs)                            | An example that utilizes a diff and auth chain to publish updates to a DID Document. |
+| 8    | [merkle_key](merkle_key.rs)                            | tbd                        |
--- examples/manipulate_did_document.rs
+++ examples/manipulate_did_document.rs
@@ -1,35 +1,55 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! A basic example that generates and publishes a DID Document,
-//! the fundamental building block for decentralized identity.
+//! This example goes into more detail regarding the useage of DID Documents.
 //!
-//! cargo run --example create_did
+//! cargo run --example manipulate_did_document
 
 use identity::crypto::KeyPair;
-use identity::iota::Client;
 use identity::iota::Document;
-use identity::iota::Network;
 use identity::iota::Result;
-use identity::iota::TangleRef;
 
 #[smol_potat::main]
 async fn main() -> Result<()> {
-  let client: Client = Client::new()?;
 
+  // Create a DID Document out of an ed25519 keypair.
   let keypair: KeyPair = KeyPair::new_ed25519()?;
-  let mut document: Document = Document::from_keypair(&keypair)?;
+  let mut document: identity::iota::Document = Document::from_keypair(&keypair)?;
 
   // Sign the DID Document with the default authentication key.
   document.sign(keypair.secret())?;
+  println!("DID Document (signed) > {:#}", document);
+  println!("");
 
-  // Use the client to publish the DID Document to the Tangle.
-  document.publish(&client).await?;
+  // We can access individual fields of the DID Document as defined below using the appropriate getters:
+  let did: &identity::did::DID = document.id(); // The Document ID.
+  let controller: Option<&identity::did::DID> = document.controller(); // The Document controller.
+  let aka: &[identity::core::Url] = document.also_known_as(); // AKA: Subject of this identifier is also identified by one or more other identifiers.
+  // ... etc. Each getter also has a _mut variant returning a mutable reference instead of an immutable one, e.g. .id_mut()
+  // See also https://identity.docs.iota.org/docs/identity/did/struct.Document.html
 
-  let network: Network = document.id().into();
-  let explore: String = format!("{}/transaction/{}", network.explorer_url(), document.message_id());
+  // We can iterate over a DID Document's verification methods using document.methods(), which returns an iterator:
+  for m in document.methods() { // m is of type &identity::did::Method
+    println!("Method > {:#}", m);
+  };
+  println!("");
 
-  println!("DID Document Transaction > {}", explore);
+  // We can also add and remove methods from a DID Document using insert_method() and remove_method() respectively.
+  let method: &identity::did::Method = document.methods().next().unwrap();
+  let method_did: &identity::did::DID = method.id();
+  //document.remove_method(identity::iota::DID::new(public: &[u8]));
+  //TODO: See discord
+
+  // We can search for a specific method using .resolve(), for instance if we want to have the first #authentication method, we can use:
+  let auth_meth = document.resolve("#authentication").unwrap();
+  println!("Authentication Method > {:#}", auth_meth);
+  println!("");
+
+  // We can sign arbitrary structs using the DID Document signer if they implement the trait `identity::crypto::SetSignature`
+  //document.signer(keypair.secret()).method("#authentication").sign(&mut test);
+  //TODO
+
+  //TODO VERIFY
 
   Ok(())
 }
