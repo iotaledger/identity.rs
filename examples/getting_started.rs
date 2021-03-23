f12b46b61eb8de73ceeefc01173d5427d65f9d5f
Sven Feuchtmüller <sven.feuchtmueller@gmx.de>
2021-03-23 16:37:10 +0100

    formatting fix

--- examples/getting_started.rs
+++ examples/getting_started.rs
@@ -9,23 +9,23 @@ use identity::iota::Result;
 
 #[smol_potat::main]
 async fn main() -> Result<()> {
-	// Print IOTA Identity header
-	println!("");
-	println!(r#"  _____ ____ _______          _____    _            _   _ _         "#);
-	println!(r#" |_   _/ __ \__   __|/\      |_   _|  | |          | | (_) |        "#);
-	println!(r#"   | || |  | | | |  /  \       | |  __| | ___ _ __ | |_ _| |_ _   _ "#);
-	println!(r#"   | || |  | | | | / /\ \      | | / _` |/ _ \ '_ \| __| | __| | | |"#);
-	println!(r#"  _| || |__| | | |/ ____ \    _| || (_| |  __/ | | | |_| | |_| |_| |"#);
-	println!(r#" |_____\____/  |_/_/    \_\  |_____\__,_|\___|_| |_|\__|_|\__|\__, |"#);
-	println!(r#"                                                               __/ |"#);
-	println!(r#"                                                              |___/ "#);
+  // Print IOTA Identity header
+  println!("");
+  println!(r#"  _____ ____ _______          _____    _            _   _ _         "#);
+  println!(r#" |_   _/ __ \__   __|/\      |_   _|  | |          | | (_) |        "#);
+  println!(r#"   | || |  | | | |  /  \       | |  __| | ___ _ __ | |_ _| |_ _   _ "#);
+  println!(r#"   | || |  | | | | / /\ \      | | / _` |/ _ \ '_ \| __| | __| | | |"#);
+  println!(r#"  _| || |__| | | |/ ____ \    _| || (_| |  __/ | | | |_| | |_| |_| |"#);
+  println!(r#" |_____\____/  |_/_/    \_\  |_____\__,_|\___|_| |_|\__|_|\__|\__, |"#);
+  println!(r#"                                                               __/ |"#);
+  println!(r#"                                                              |___/ "#);
 
-	// Print welcome text
-	println!("");
-	println!("Welcome to IOTA Identity! The library was set up correctly and everything works!");
-	println!("You can try out the other examples using: cargo run --example <example_name>");
-	println!("Please visit https://github.com/iotaledger/identity.rs/tree/main/examples for further info!");
-	println!("");
+  // Print welcome text
+  println!("");
+  println!("Welcome to IOTA Identity! The library was set up correctly and everything works!");
+  println!("You can try out the other examples using: cargo run --example <example_name>");
+  println!("Please visit https://github.com/iotaledger/identity.rs/tree/main/examples for further info!");
+  println!("");
 
-	Ok(())
+  Ok(())
 }
