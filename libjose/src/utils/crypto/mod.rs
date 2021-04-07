fd4a19d5abf4aed88a08feeabcfc1f62bf03bd8c
Devin Turner <5410284+l1h3r@users.noreply.github.com>
2021-04-07 19:52:36 +0000

    libjose (#176)
    * Add libjose types and traits
    
    * Add libjose to workflows
    
    * Add basic claims validation
    
    * Add content compression with DEFLATE
    
    * Add JWS/JWE encoding/decoding
    
    * Add JWA utility types
    
    * Add crypto base/internal key type
    
    * Add higher-level key types
    
    * Add ConcatKDF utility
    
    * Add JWK thumbprint
    
    * Add content encryption
    
    * Add JWS algorithms
    
    * Add JWE algorithms

---
+++ libjose/src/utils/crypto/mod.rs
@@ -0,0 +1,16 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+mod concat_kdf;
+mod diffie_hellman;
+mod key_params;
+mod key_repr;
+mod random;
+mod rsa_primes;
+
+pub use self::concat_kdf::*;
+pub use self::diffie_hellman::*;
+pub use self::key_params::*;
+pub use self::key_repr::*;
+pub use self::random::*;
+pub use self::rsa_primes::*;
