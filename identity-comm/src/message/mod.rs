322489d0684fec6402b6cc95a8af238afc125ad2
Marcel Feldmann <feldmann_marcel@gmx.net>
2021-04-13 14:14:57 +0200

    add message types v2

--- identity-comm/src/message/mod.rs
+++ identity-comm/src/message/mod.rs
@@ -11,11 +11,17 @@ mod report;
 mod resolution;
 mod timing;
 mod trustping;
+mod credential_issuance;
+mod credential_revocation;
+mod presentation_verification;
 
 pub use self::authentication::*;
 pub use self::credential::*;
+pub use self::credential_issuance::*;
+pub use self::credential_revocation::*;
 pub use self::discovery::*;
 pub use self::message::*;
+pub use self::presentation_verification::*;
 pub use self::report::*;
 pub use self::resolution::*;
 pub use self::timing::*;
