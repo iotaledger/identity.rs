2abb46de7844b388c1768bbd9603fd9a48b60dba
Druelik <druelik@arcor.de>
2021-04-29 11:49:15 +0200

    remove actor

--- identity-comm/src/lib.rs
+++ identity-comm/src/lib.rs
@@ -2,11 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #[macro_use]
-extern crate riker;
-#[macro_use]
 extern crate serde;
 
-pub mod actor;
 pub mod envelope;
 pub mod error;
 pub mod message;
