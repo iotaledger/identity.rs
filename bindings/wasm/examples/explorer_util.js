124c75dd6ef67dfd850b1f2b8210ca7fb9a5b0ce
Matt Renaud <matt@m-renaud.com>
2021-05-25 13:20:02 -0700

    Fix explorer URL construction.
    Inlined the prefix since it is no longer exported.

--- bindings/wasm/examples/explorer_util.js
+++ bindings/wasm/examples/explorer_util.js
@@ -1,8 +1,6 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-const EXPLORER_URL_BASE = "https://explorer.iota.org/mainnet/transaction";
-
 /*
   Write out the Targle Explorer URL given the network and message ID, with the given preamble.
 
@@ -11,7 +9,7 @@ const EXPLORER_URL_BASE = "https://explorer.iota.org/mainnet/transaction";
   @param {!string} messageId
 */
 function logExplorerUrl(preamble, network, messageId) {
-  console.log(`${preamble} ${EXPLORER_URL_BASE}/${network}net/transaction/${messageId}`);
+  console.log(`${preamble} https://explorer.iota.org/${network}net/transaction/${messageId}`);
 }
 
 exports.logExplorerUrl = logExplorerUrl;
