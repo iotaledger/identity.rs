7a78e716e65960a1716bd90aaceb39e6d4ce2f9f
Adrian Grassl <adrian.grassl@iota.org>
2021-06-11 17:31:29 +0200

    Fix log for manipulate did example

--- bindings/wasm/examples/manipulate_did.js
+++ bindings/wasm/examples/manipulate_did.js
@@ -52,7 +52,7 @@ async function manipulateIdentity(clientConfig) {
     const nextMessageId = await client.publishDocument(doc.toJSON());
 
     // Log the results.
-    logExplorerUrl("Identity Update:", clientConfig.network.toString(), messageId);
+    logExplorerUrl("Identity Update:", clientConfig.network.toString(), nextMessageId);
     return {key, newKey, doc, nextMessageId};
 }
 
