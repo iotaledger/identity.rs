2568857ae79470df7afd7e6573c9107efe381648
PhilippGackstatter <philipp.gackstatter@iota.org>
2021-06-28 12:55:03 +0200

    handler: Implement list identities

--- identity-actor/src/handler.rs
+++ identity-actor/src/handler.rs
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use identity_account::account::Account;
+use identity_iota::did::IotaDocument;
+use core::convert;
+use futures::stream::FuturesOrdered;
+use futures::TryStreamExt;
 
 use crate::types::{IdentityStorageRequest, IdentityStorageResponse};
 use crate::IdentityRequestHandler;
@@ -37,6 +41,23 @@ impl IdentityRequestHandler for IdentityStorageHandler {
         let document = self.account.resolve_identity(did).await?;
 
         IdentityStorageResponse::Create(document)
+      }
+      IdentityStorageRequest::List => {
+        let list = self
+          .account
+          .list_identities()
+          .await
+          .iter()
+          .map(|tag| self.account.find_identity(tag.method_id()))
+          .collect::<FuturesOrdered<_>>()
+          .try_collect::<Vec<_>>()
+          .await?
+          .into_iter()
+          .filter_map(convert::identity)
+          .map(|snapshot| snapshot.identity().to_document())
+          .collect::<identity_account::Result<Vec<_>>>()?;
+
+        IdentityStorageResponse::List(list)
       }
       _ => todo!(),
     };
