9840487ff377b85eb123c1627f2f055708df405e
Craig Bester <craig.bester@iota.org>
2021-07-12 16:32:03 +0200

    Add publicKeyMultibase support
    Add and expand base-encoding tests.

--- identity-did/src/verification/method_data.rs
+++ identity-did/src/verification/method_data.rs
@@ -4,11 +4,14 @@
 use core::fmt::Debug;
 use core::fmt::Formatter;
 use core::fmt::Result as FmtResult;
+
 use identity_core::common::Object;
 use identity_core::utils::decode_b16;
 use identity_core::utils::decode_b58;
+use identity_core::utils::decode_multibase;
 use identity_core::utils::encode_b16;
 use identity_core::utils::encode_b58;
+use identity_core::utils::encode_multibase;
 
 use crate::error::Error;
 use crate::error::Result;
@@ -18,6 +21,7 @@ use crate::error::Result;
 #[serde(rename_all = "camelCase")]
 #[non_exhaustive]
 pub enum MethodData {
+  PublicKeyMultibase(String),
   PublicKeyBase58(String),
   PublicKeyHex(String),
   PublicKeyJwk(Object),
@@ -34,6 +38,13 @@ impl MethodData {
     Self::PublicKeyBase58(encode_b58(&data))
   }
 
+  /// Creates a new `MethodData` variant with [Multibase]-encoded content.
+  ///
+  /// [Multibase]: https://datatracker.ietf.org/doc/html/draft-multiformats-multibase-03
+  pub fn new_multibase(data: impl AsRef<[u8]>) -> Self {
+    Self::PublicKeyMultibase(encode_multibase(&data, None))
+  }
+
   /// Returns a `Vec<u8>` containing the decoded bytes of the `MethodData`.
   ///
   /// This is generally a public key identified by a `MethodType` value.
@@ -44,6 +55,7 @@ impl MethodData {
   /// represented as a vector of bytes.
   pub fn try_decode(&self) -> Result<Vec<u8>> {
     match self {
+      Self::PublicKeyMultibase(input) => decode_multibase(input).map_err(|_| Error::InvalidKeyDataMultibase),
       Self::PublicKeyBase58(input) => decode_b58(input).map_err(|_| Error::InvalidKeyDataBase58),
       Self::PublicKeyHex(input) => decode_b16(input).map_err(|_| Error::InvalidKeyDataBase16),
       Self::PublicKeyJwk(_) => Err(Error::InvalidKeyData),
@@ -54,6 +66,7 @@ impl MethodData {
 impl Debug for MethodData {
   fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
     match self {
+      Self::PublicKeyMultibase(inner) => f.write_fmt(format_args!("PublicKeyMultibase({})", inner)),
       Self::PublicKeyBase58(inner) => f.write_fmt(format_args!("PublicKeyBase58({})", inner)),
       Self::PublicKeyHex(inner) => f.write_fmt(format_args!("PublicKeyHex({})", inner)),
       Self::PublicKeyJwk(inner) => f.debug_tuple("PublicKeyJwk").field(inner).finish(),
