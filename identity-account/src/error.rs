e3aad52ce0d41ffe229ca51ea5ec18400cbbb15f
Philipp <philipp.gackstatter@iota.org>
2021-07-16 14:19:27 +0200

    account: Add stronghold and re-export wasm feature (#291)
    Signed-off-by: PhilippGackstatter <philipp.gackstatter@iota.org>
    
    account: Complete stronghold feature integration
    
    account: Enable new resolver to fix wasm target
    
    account: Use tokio 1.5 & split async feature
    
    account: Add note on `Stronghold` availability
    
    builder: Apply cargo fmt
    Signed-off-by: PhilippGackstatter <philipp.gackstatter@iota.org>

--- identity-account/src/error.rs
+++ identity-account/src/error.rs
@@ -28,9 +28,11 @@ pub enum Error {
   #[error(transparent)]
   IoError(#[from] std::io::Error),
   /// Caused by an internal failure of the riker actor system.
+  #[cfg(feature = "stronghold")]
   #[error(transparent)]
   ActorSystemError(#[from] riker::system::SystemError),
   /// Caused by errors from the [iota_stronghold] crate.
+  #[cfg(feature = "stronghold")]
   #[error(transparent)]
   StrongholdError(#[from] iota_stronghold::Error),
   /// Caused by errors from an invalid Stronghold procedure.
