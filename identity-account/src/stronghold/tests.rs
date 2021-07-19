b125389215013216e615f706ddc5c2762f659062
Craig Bester <craig.bester@iota.org>
2021-07-19 12:54:46 +0200

    Fix inconsistent stronghold storage tests (#320)
    Raise test timeouts and improve handling on slow systems.

--- identity-account/src/stronghold/tests.rs
+++ identity-account/src/stronghold/tests.rs
@@ -22,8 +22,8 @@ use crate::stronghold::Store;
 use crate::utils::derive_encryption_key;
 use crate::utils::EncryptionKey;
 
-const ROOT: &str = "./test-storage";
-const SIZE: usize = 10;
+const TEST_DIR: &str = "./test-storage";
+const RANDOM_FILENAME_SIZE: usize = 10;
 
 fn location(name: &str) -> Location {
   Location::generic(name, name)
@@ -38,7 +38,7 @@ fn rand_string(chars: usize) -> String {
 }
 
 fn generate_filename() -> PathBuf {
-  AsRef::<Path>::as_ref(ROOT).join(format!("{}.stronghold", rand_string(SIZE)))
+  AsRef::<Path>::as_ref(TEST_DIR).join(format!("{}.stronghold", rand_string(RANDOM_FILENAME_SIZE)))
 }
 
 async fn open_snapshot(path: &Path, password: EncryptionKey) -> Snapshot {
@@ -59,7 +59,7 @@ rusty_fork_test! {
   #[test]
   fn test_password_expiration() {
     block_on(async {
-      let interval: Duration = Duration::from_millis(25);
+      let interval: Duration = Duration::from_millis(100);
 
       Snapshot::set_password_clear(interval).unwrap();
 
@@ -68,6 +68,7 @@ rusty_fork_test! {
 
       snapshot.load(Default::default()).await.unwrap();
 
+      // Wait for password to be cleared
       thread::sleep(interval * 3);
 
       let store: Store<'_> = snapshot.store("", &[]);
@@ -89,7 +90,7 @@ rusty_fork_test! {
   #[test]
   fn test_password_persistence() {
     block_on(async {
-      let interval: Duration = Duration::from_millis(25);
+      let interval: Duration = Duration::from_millis(300);
 
       Snapshot::set_password_clear(interval).unwrap();
 
@@ -97,45 +98,53 @@ rusty_fork_test! {
       let snapshot: Snapshot = Snapshot::new(&filename);
 
       snapshot.load(Default::default()).await.unwrap();
+      let mut instant: Instant = Instant::now();
 
       let store: Store<'_> = snapshot.store("", &[]);
-
-      for index in 1..6 {
-        let instant: Instant = Instant::now();
+      for index in 1..=5u8 {
         let location: Location = location(&format!("persists{}", index));
 
-        store.set(location, "STRONGHOLD".to_string(), None).await.unwrap();
-
+        let set_result = store.set(location, format!("STRONGHOLD{}", index), None).await;
         let status: SnapshotStatus = snapshot.status().unwrap();
 
-        assert!(
-          matches!(status, SnapshotStatus::Unlocked(_)),
-          "unexpected snapshot status",
-        );
-
         if let Some(timeout) = interval.checked_sub(instant.elapsed()) {
+          // Prior to the expiration time, the password should not be cleared yet
+          assert!(
+            set_result.is_ok(),
+            "set failed"
+          );
+          assert!(
+            matches!(status, SnapshotStatus::Unlocked(_)),
+            "unexpected snapshot status",
+          );
+
           thread::sleep(timeout / 2);
         } else {
-          // if elapsed > interval, set the password again.
-          // this might happen if the test is stopped by another thread
+          // If elapsed > interval, set the password again.
+          // This might happen if the test is stopped by another thread.
           snapshot.set_password(Default::default()).unwrap();
+          instant = Instant::now();
         }
       }
 
-      let result: Vec<u8> = store.get(location("persists1")).await.unwrap();
+      let mut result: Result<Vec<u8>, Error> = store.get(location("persists1")).await;
 
-      assert_eq!(result, b"STRONGHOLD");
+      // Test may have taken too long / been interrupted and cleared the password already, retry
+      if matches!(result, Err(Error::StrongholdPasswordNotSet)) && interval.checked_sub(instant.elapsed()).is_none() {
+        snapshot.set_password(Default::default()).unwrap();
+        result = store.get(location("persists1")).await;
+      }
+      assert_eq!(result.unwrap(), b"STRONGHOLD1");
 
+      // Wait for password to be cleared
       thread::sleep(interval * 2);
 
       let error: Error = store.get(location("persists1")).await.unwrap_err();
-
       assert!(
         matches!(error, Error::StrongholdPasswordNotSet),
         "unexpected error: {:?}",
         error
       );
-
       assert!(
         matches!(snapshot.status().unwrap(), SnapshotStatus::Locked),
         "unexpected snapshot status",
