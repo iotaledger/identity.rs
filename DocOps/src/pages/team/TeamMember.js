1fa3d7db668db90beeee3f16ba26c1209c20e88d
Abdulrahim Al Methiab <abdulrahim.almethiab@iota.org>
2021-07-20 23:10:09 +0200

    adjust team page

--- DocOps/src/pages/team/TeamMember.js
+++ DocOps/src/pages/team/TeamMember.js
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 import SocialLink from './SocialLink';
 
 const TeamMember = function(props) {
-  let { name, title, image_url, social_links } = props
+  let { name, title, desc, image_url, social_links } = props
 
   if(!social_links) {
     social_links = []
@@ -16,6 +16,7 @@ const TeamMember = function(props) {
           <img className={styles.image} src={image_url} alt={`Image of ${name}`} />
           <h1 className={styles.name}>{name}</h1>
           <p className={styles.title}>{title}</p>
+          <p className={styles.title}>{desc}</p>
         </div>
         <div className={styles.cardFooter}>
           {social_links.map((item, index) => (
--- DocOps/src/pages/team/index.js
+++ DocOps/src/pages/team/index.js
@@ -16,17 +16,6 @@ export default function Team() {
           <div className="container">
             <h1 className="hero__title">IOTA Identity Team Members</h1>
             <p className="hero__subtitle">
-              <ul>
-                <li>
-                Jelle is responsible for the architecture of the IOTA Identity framework and management of the team. He directly interacts with IOTAs partners and community to promote for adoption. He joined the IOTA Foundation in Aug, 2019.
-                </li>
-                <li>
-                  Devin, Philipp and Craig work on developing the core framework.
-                </li>
-                <li>
-                  Abdulrahim works on improving the usability of the framework, by building improved developer tooling, documentation, examples and bindings.
-                </li>
-              </ul>
             </p>
             <div className="row">
               {TEAM.map((item, index) => (
