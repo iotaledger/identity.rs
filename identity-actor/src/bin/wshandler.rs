14656251f98205fb57968a2e239f29d46f6dcd22
PhilippGackstatter <philipp.gackstatter@iota.org>
2021-07-29 17:42:25 +0200

    actor: Impl websocket listening actor in rust

---
+++ identity-actor/src/bin/wshandler.rs
@@ -0,0 +1,35 @@
+use communication_refactored::{InitKeypair, Keypair};
+use libp2p::{tcp::TcpConfig, websocket::WsConfig, Multiaddr};
+
+use identity_actor::{actor_builder::ActorBuilder, asyncfn::AsyncFn, errors::Result, StorageHandler};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  // pretty_env_logger::init();
+
+  let id_keys = Keypair::generate_ed25519();
+  let tcp_transport = TcpConfig::new().nodelay(true);
+  let transport = WsConfig::new(tcp_transport);
+
+  let addr: Multiaddr = "/ip4/0.0.0.0/tcp/12345/ws".parse().unwrap();
+
+  let mut comm = ActorBuilder::new()
+    .keys(InitKeypair::IdKeys(id_keys))
+    .listen_on(addr.clone())
+    .build_with_transport(transport)
+    .await?;
+
+  let handler = StorageHandler::new().await.unwrap();
+
+  comm.add_handler(handler)
+      .add_method("storage/list", AsyncFn::new(StorageHandler::list));
+
+  let peer_id = comm.peer_id();
+
+  println!("Listening on {:?} with PeerId: {}", addr, peer_id.to_base58());
+
+  // Blocks forever
+  comm.join().await;
+
+  Ok(())
+}
