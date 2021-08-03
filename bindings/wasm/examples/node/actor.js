ab78c341a92a9846108f79391748286f817c2ea0
PhilippGackstatter <philipp.gackstatter@iota.org>
2021-08-03 17:58:29 +0200

    examples: Update actor Wasm example

--- bindings/wasm/examples/node/actor.js
+++ bindings/wasm/examples/node/actor.js
@@ -1,14 +1,14 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-const { IdentityActor } = require('../../node/identity_wasm')
+const { IdentityActor, IdentityResolve, IdentityList } = require('../../node/identity_wasm')
 const { Multiaddr } = require("multiaddr")
 const PeerId = require('peer-id')
 
 async function actor() {
 
-    const addr = "/ip4/0.0.0.0/tcp/12345/ws"
-    const peer_id = "..."
+    const addr = "/ip4/0.0.0.0/tcp/12346/ws"
+    const peer_id = "12D3KooWMP7sWFDzuzxeQZojBFMazwKXcXMyZ8KGX14JGgrzUkAm"
 
     const address = new Multiaddr(addr)
 
@@ -16,13 +16,22 @@ async function actor() {
 
     const actor = IdentityActor.new();
 
+    const resolve = new IdentityResolve("did:iota:Gq9tCpiNTYGewnQbTUBxB5K1GCYsAscXdrvYEFkjJ1JJ")
+
+    // The same, but written in JavaScript.
+    // Should only be used for custom defined types or if the bindings
+    // do not export the type natively.
+    // const resolve = {
+    //     did: "did:iota:Gq9tCpiNTYGewnQbTUBxB5K1GCYsAscXdrvYEFkjJ1JJ",
+    //     requestName: () => "storage/resolve"
+    // };
+
     console.log(`Adding peer ${peerId} on address ${address}`);
     await actor.addPeer(peerId, address);
 
-    console.log(`Sending "IdentityList" request`);
-    let result = await actor.sendRequest(peerId);
+    console.log(`Sending request`);
+    let result = await actor.sendRequest(peerId, resolve);
 
-    console.log(`Available identities on the remote actor...`);
     console.log(result);
 }
 
