981329924c825a2dba54c40d3f3475a4a5bed64e
Abdulrahim Al Methiab <abdulrahim.almethiab@iota.org>
2021-08-06 00:12:55 +0200

    fix did-history node example

--- bindings/wasm/examples/node/history.js
+++ bindings/wasm/examples/node/history.js
@@ -1,12 +1,14 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-const { Client, Config } = require('../../node/identity_wasm')
+const { Client, Config } = require("../../node/identity_wasm");
 const { manipulateIdentity } = require("./manipulate_did");
 const { CLIENT_CONFIG } = require("./config");
 
 /*
-
+    An example for resolving the integration-message-history of a DID.
+    The history is usually only useful for debugging puropses.
+    
     @param {{network: string, node: string}} clientConfig
 */
 async function resolveHistory(clientConfig) {
@@ -19,13 +21,9 @@ async function resolveHistory(clientConfig) {
     // Creates a new identity, that also is updated (See "manipulate_did" example).
     const result = await manipulateIdentity(clientConfig);
 
-    console.log(result)
-    const chain = await client.resolveHistory(result.doc.id.toString())
+    const chain = await client.resolveHistory(result.doc.id.toString());
 
-    console.log(chain);
-    console.log(chain.intChainData.history);
+    return chain;
 }
 
-resolveHistory(CLIENT_CONFIG)
-
-// exports.resolveHistory = resolveHistory;
+exports.resolveHistory = resolveHistory;
