272df2d224e67083ea17bd47dc01c2f6183dae83
Eike Haß <eike-hass@web.de>
2021-09-09 10:58:11 +0200

    use hardlinks instead of symlinks

--- documentation/wiki-cli/src/commands/setup.ts
+++ documentation/wiki-cli/src/commands/setup.ts
@@ -49,12 +49,7 @@ export default class Setup extends Command {
     if (!existsSync(WIKI_CONTENT_DOCS_FOLDER)) {
       mkdirSync(WIKI_CONTENT_DOCS_FOLDER)
     }
-    ['blog', 'docs', 'src', 'sidebars.js'].forEach(folder => {
-      const target = join(PWD, folder)
-      const path = join(WIKI_CONTENT_DOCS_FOLDER, folder)
-      this.log(`linking ${target} to ${path}`)
-      symlinkSync(target, path, 'junction')
-    })
+
     spawnSync('yarn', {
       cwd: WIKI_CONTENT_REPO_FOLDER,
       shell: true,
--- documentation/wiki-cli/src/commands/start.ts
+++ documentation/wiki-cli/src/commands/start.ts
@@ -1,9 +1,10 @@
 import {Command, flags} from '@oclif/command'
-import {spawnSync} from 'child_process'
+import {spawn} from 'child_process'
 import {join} from 'path'
 const  replaceInFile = require('replace-in-file')
 import {readFileSync} from 'fs'
 import {copySync} from 'fs-extra'
+const syncDirectory = require('sync-directory')
 
 export default class Start extends Command {
   static description = 'start local wiki'
@@ -17,6 +18,7 @@ export default class Start extends Command {
     const PWD = process.env.PWD ?? ''
     const WIKI_GIT_FOLDER = join(PWD, 'local', 'iota-wiki')
     const DOCUSAURUS_CONFIG_PATH = join(WIKI_GIT_FOLDER, 'docusaurus.config.js')
+    const log = this.log
 
     const EXTERNAL_DOCS_CONFIG = readFileSync(join(PWD, 'EXTERNAL_DOCS_CONFIG'), 'utf8')
     await replaceInFile({
@@ -32,17 +34,32 @@ export default class Start extends Command {
       to: EXTERNAL_DOCS_DROPDOWN_CONFIG,
     })
 
+    const WIKI_EXTERNAL_FOLDER = join(WIKI_GIT_FOLDER, 'external')
+
+    const WIKI_CONTENT_REPO_FOLDER = join(WIKI_EXTERNAL_FOLDER, 'identity.rs')
+
     copySync(join(PWD, 'static', 'img'), join(WIKI_GIT_FOLDER, 'static', 'img'))
 
-    spawnSync('yarn', [
-      'start',
-      '--',
-      '--host',
-      '0.0.0.0',
-    ], {
-      cwd: WIKI_GIT_FOLDER,
-      shell: true,
-      stdio: 'inherit',
+    syncDirectory(join(PWD, '..'), WIKI_CONTENT_REPO_FOLDER, {
+      exclude: ['local', 'wiki-cli', 'node_modules', 'target', '.git'],
+      watch: true,
+      afterSync({type, relativePath}) {
+        log(`${type}: ${relativePath}`)
+      },
     })
+
+    setTimeout(() => {
+      const yarnProcess = spawn('yarn', [
+        'start',
+        '--host',
+        '0.0.0.0',
+      ], {
+        cwd: WIKI_GIT_FOLDER,
+        shell: true,
+      })
+      yarnProcess.stdout.on('data', function (data) {
+        log(data.toString())
+      })
+    }, 1000)
   }
 }
