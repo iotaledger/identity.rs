3ee45d64d542d606e4a0b5d820a333835b8a698e
Craig Bester <craig.bester@iota.org>
2021-09-13 22:59:02 +0200

    Add toml formatting (#393)
    * Add dprint for Cargo.toml formatting
    
    Add dprint check to format CI action.
    
    * Format toml files
    
    * Fix format workflow
    
    * Use pre-packaged dprint from npm
    
    * Disable incremental compilation in CI
    
    Attempt to reduce cache size.
    
    * Fix rustdoc lint warnings
    
    Re-enable incremental cargo compilation in CI except for build-and-test-wasm.
    
    * Remove redundant format check

--- .github/workflows/scripts/coverage.sh
+++ .github/workflows/scripts/coverage.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf coverage
 mkdir coverage
 
-NIGHTLY="+nightly-2021-07-05"
+NIGHTLY="+nightly-2021-09-07"
 
 # Run tests with profiling instrumentation
 echo "Running instrumented unit tests..."
