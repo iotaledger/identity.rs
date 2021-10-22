df185ba81973ae280d3fb42f87691f4c38d8230e
PhilippGackstatter <philipp.gackstatter@iota.org>
2021-10-22 15:13:27 +0200

    Rename `identity_setup` module

--- identity-account/src/identity/mod.rs
+++ identity-account/src/identity/mod.rs
@@ -1,12 +1,12 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-mod identity_create;
+mod identity_setup;
 mod identity_snapshot;
 mod identity_state;
 mod identity_updater;
 
-pub use self::identity_create::*;
+pub use self::identity_setup::*;
 pub use self::identity_snapshot::*;
 pub use self::identity_state::*;
 pub use self::identity_updater::*;
