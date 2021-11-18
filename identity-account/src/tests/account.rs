00faf7e65c16020851a023d2ff968cf73e7fe7af
PhilippGackstatter <philipp.gackstatter@iota.org>
2021-11-18 09:53:47 +0100

    Remove tangle dependency in autopublish test

--- identity-account/src/tests/account.rs
+++ identity-account/src/tests/account.rs
@@ -1,18 +1,23 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+use std::sync::Arc;
+
+use crate::account::Account;
+use crate::account::AccountBuilder;
+use crate::account::AccountConfig;
+use crate::account::AccountSetup;
+use crate::identity::IdentitySetup;
+use crate::storage::MemStore;
+use crate::Result;
 
 use identity_core::common::Url;
 use identity_did::verification::MethodScope;
 use identity_iota::did::IotaDID;
 use identity_iota::tangle::MessageId;
-
-use crate::account::Account;
-use crate::account::AccountBuilder;
-use crate::error::Result;
-use crate::identity::IdentitySetup;
+use identity_iota::tangle::MessageIdExt;
 
 #[tokio::test]
-async fn test_account_high_level() -> Result<()> {
+async fn test_account_builder() -> Result<()> {
   let mut builder: AccountBuilder = AccountBuilder::default().testmode(true);
 
   let account1: Account = builder.create_identity(IdentitySetup::default()).await?;
@@ -101,6 +106,134 @@ async fn test_account_chain_state() -> Result<()> {
 
   // Int message id was overwritten.
   assert_ne!(&last_int_id, account.chain_state().last_integration_message_id());
+
+  Ok(())
+}
+
+#[tokio::test]
+async fn test_account_autopublish() -> Result<()> {
+  // ===========================================================================
+  // Create, update and "publish" an identity
+  // ===========================================================================
+
+  let config = AccountConfig::default().autopublish(false).testmode(true);
+  let account_config = AccountSetup::new(Arc::new(MemStore::new())).config(config);
+
+  let mut account = Account::create_identity(account_config, IdentitySetup::new()).await?;
+
+  account
+    .update_identity()
+    .create_service()
+    .fragment("my-service")
+    .type_("LinkedDomains")
+    .endpoint(Url::parse("https://example.org").unwrap())
+    .apply()
+    .await?;
+
+  account
+    .update_identity()
+    .create_service()
+    .fragment("my-other-service")
+    .type_("LinkedDomains")
+    .endpoint(Url::parse("https://example.org").unwrap())
+    .apply()
+    .await?;
+
+  assert!(account.chain_state().last_integration_message_id().is_null());
+  assert!(account.chain_state().last_diff_message_id().is_null());
+
+  account.publish_updates().await?;
+
+  let last_int_message_id = *account.chain_state().last_integration_message_id();
+
+  assert!(!last_int_message_id.is_null());
+  assert!(account.chain_state().last_diff_message_id().is_null());
+
+  // ===========================================================================
+  // Service assertions
+  // ===========================================================================
+
+  let doc = account.document();
+
+  assert_eq!(doc.methods().count(), 1);
+  assert_eq!(doc.service().len(), 2);
+
+  for service in ["my-service", "my-other-service"] {
+    assert!(doc.service().query(service).is_some());
+  }
+
+  // ===========================================================================
+  // More updates to the identity
+  // ===========================================================================
+
+  account
+    .update_identity()
+    .delete_service()
+    .fragment("my-service")
+    .apply()
+    .await?;
+
+  account
+    .update_identity()
+    .delete_service()
+    .fragment("my-other-service")
+    .apply()
+    .await?;
+
+  account
+    .update_identity()
+    .create_method()
+    .fragment("new-method")
+    .apply()
+    .await?;
+
+  account.publish_updates().await?;
+
+  // No integration message was published
+  assert_eq!(
+    &last_int_message_id,
+    account.chain_state().last_integration_message_id()
+  );
+
+  let last_diff_message_id = *account.chain_state().last_diff_message_id();
+  assert!(!last_diff_message_id.is_null());
+
+  // ===========================================================================
+  // Second round of assertions
+  // ===========================================================================
+
+  let doc = account.document();
+
+  assert_eq!(doc.service().len(), 0);
+  assert_eq!(doc.methods().count(), 2);
+
+  for method in ["sign-0", "new-method"] {
+    assert!(doc.resolve_method(method).is_some());
+  }
+
+  // ===========================================================================
+  // More updates to the identity
+  // ===========================================================================
+
+  account
+    .update_identity()
+    .create_method()
+    .fragment("signing-key")
+    // Forces an integration update by adding a method able to update the document.
+    .scope(MethodScope::capability_invocation())
+    .apply()
+    .await?;
+
+  account.publish_updates().await?;
+
+  // Another int update was published.
+  assert_ne!(
+    &last_int_message_id,
+    account.chain_state().last_integration_message_id()
+  );
+
+  // Diff message id was reset.
+  assert!(account.chain_state().last_diff_message_id().is_null());
 
   Ok(())
 }
--- identity-account/src/tests/mod.rs
+++ identity-account/src/tests/mod.rs
@@ -2,5 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod account;
-mod lazy;
 mod updates;
