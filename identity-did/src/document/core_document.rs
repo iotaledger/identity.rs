0d5b1e196908523d7c0a77e96204699681d1a578
PhilippGackstatter <philipp.gackstatter@iota.org>
2021-11-23 10:49:57 +0100

    Remove superfluous semicolons

--- identity-did/src/document/core_document.rs
+++ identity-did/src/document/core_document.rs
@@ -242,23 +242,21 @@ impl<T, U, V> CoreDocument<T, U, V> {
     }
 
     match scope {
-      MethodScope::VerificationMethod => {
-        self.verification_method.append(method);
-      }
+      MethodScope::VerificationMethod => self.verification_method.append(method),
       MethodScope::VerificationRelationship(MethodRelationship::Authentication) => {
-        self.authentication.append(MethodRef::Embed(method));
+        self.authentication.append(MethodRef::Embed(method))
       }
       MethodScope::VerificationRelationship(MethodRelationship::AssertionMethod) => {
-        self.assertion_method.append(MethodRef::Embed(method));
+        self.assertion_method.append(MethodRef::Embed(method))
       }
       MethodScope::VerificationRelationship(MethodRelationship::KeyAgreement) => {
-        self.key_agreement.append(MethodRef::Embed(method));
+        self.key_agreement.append(MethodRef::Embed(method))
       }
       MethodScope::VerificationRelationship(MethodRelationship::CapabilityDelegation) => {
-        self.capability_delegation.append(MethodRef::Embed(method));
+        self.capability_delegation.append(MethodRef::Embed(method))
       }
       MethodScope::VerificationRelationship(MethodRelationship::CapabilityInvocation) => {
-        self.capability_invocation.append(MethodRef::Embed(method));
+        self.capability_invocation.append(MethodRef::Embed(method))
       }
     };
 
