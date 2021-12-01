d558fa08b17d51177cf5629ccee106d3e6f39307
Eike Haß <eike-hass@web.de>
2021-12-01 11:07:37 +0100

    latest iteration

--- .github/workflows/scripts/change-version/index.js
+++ .github/workflows/scripts/change-version/index.js
@@ -4,57 +4,68 @@ const path = require('path');
 const { argv } = require('process');
 
 const newVersion = argv[2];
+const target = argv[3];
 
-glob("*(identity|identity-*|bindings)/**/Cargo.toml",{cwd: '../../..'}, (err, files) => {
-
-    if(err) {
-        throw err;
-    }
-
-    files.forEach(filePath => {
-        const resolvedPath = path.join('../../..', filePath);
-        fs.readFile(resolvedPath, "utf-8", (err, file) => {
-
+switch (target) {
+    case 'rust':
+        glob("*(identity|identity-*|bindings)/**/Cargo.toml",{cwd: '../../../..'}, (err, files) => {
+        
             if(err) {
                 throw err;
             }
-
-            const newFile = file.replace(/(name = ".*"\nversion = ")\d.\d.\d(")/, '$1'+newVersion+'$2')
             
-            if(file === newFile) {
-                throw new Error(resolvedPath+' not changed');
-            }
-
-            fs.writeFile(resolvedPath, newFile, () => {
-                console.log('transformed', resolvedPath);
+            files.forEach(filePath => {
+                const resolvedPath = path.join('../../../..', filePath);
+                fs.readFile(resolvedPath, "utf-8", (err, file) => {
+        
+                    if(err) {
+                        throw err;
+                    }
+                    
+                    const newFile = file.replace(/(name = ".*"\nversion = ").*(")/, '$1'+newVersion+'$2')
+                    
+                    if(file === newFile) {
+                        throw new Error(resolvedPath+' not changed');
+                    }
+                    
+                    fs.writeFile(resolvedPath, newFile, () => {
+                        console.log('transformed', resolvedPath);
+                    })
+                })
             })
         })
-    })
-})
-
-glob("bindings/wasm/*(package|package-lock).json",{cwd: '../../..'}, (err, files) => {
-
-    if(err) {
-        throw err;
-    }
-
-    files.forEach(filePath => {
-        const resolvedPath = path.join('../../..', filePath);
-        fs.readFile(resolvedPath, "utf-8", (err, file) => {
-
+        break;
+    
+    case 'wasm':
+        glob("bindings/wasm/*(package|package-lock).json",{cwd: '../../../..'}, (err, files) => {
+            
             if(err) {
                 throw err;
             }
-
-            const newFile = JSON.stringify({...JSON.parse(file), version: newVersion}, null, 2);
-            
-            if(file === newFile) {
-                throw new Error(resolvedPath+' not changed');
-            }
-
-            fs.writeFile(resolvedPath, newFile, () => {
-                console.log('transformed', resolvedPath);
+        
+            files.forEach(filePath => {
+                const resolvedPath = path.join('../../../..', filePath);
+                fs.readFile(resolvedPath, "utf-8", (err, file) => {
+        
+                    if(err) {
+                        throw err;
+                    }
+        
+                    const newFile = JSON.stringify({...JSON.parse(file), version: newVersion}, null, 2);
+                    
+                    if(file === newFile) {
+                        throw new Error(resolvedPath+' not changed');
+                    }
+        
+                    fs.writeFile(resolvedPath, newFile, () => {
+                        console.log('transformed', resolvedPath);
+                    })
+                })
             })
         })
-    })
-})
+        break;
+
+    default:
+        throw new Error('unsupported target');
+        
+}
