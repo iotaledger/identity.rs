9b0c947d9ed8ed7998222e82ac0c409e592807ae
Craig Bester <craig.bester@iota.org>
2021-12-07 15:37:53 +0200

    Add IotaDocumentMetadata

--- identity-iota/src/document/mod.rs
+++ identity-iota/src/document/mod.rs
@@ -5,10 +5,12 @@ pub use self::diff_message::DiffMessage;
 pub use self::iota_document::IotaDocument;
 pub use self::iota_document::IotaDocumentSigner;
 pub use self::iota_document::IotaDocumentVerifier;
+pub use self::iota_document_metadata::IotaDocumentMetadata;
 pub use self::iota_verification_method::IotaVerificationMethod;
 pub use self::properties::Properties;
 
 mod diff_message;
 mod iota_document;
+mod iota_document_metadata;
 mod iota_verification_method;
 mod properties;
