2ced397e6566c588c97301c727612fd74fda08c8
Oliver E. Anderson <oliver.anderson@iota.org>
2021-12-07 09:29:57 +0100

    Update identity-core/src/common/timestamp.rs
    Co-authored-by: Craig Bester <craig.bester@iota.org>

--- identity-core/src/common/timestamp.rs
+++ identity-core/src/common/timestamp.rs
@@ -53,6 +53,9 @@ impl Timestamp {
   /// Creates a new `Timestamp` from the given Unix timestamp.
   pub fn from_unix(seconds: i64) -> Result<Self> {
     let offset_date_time = OffsetDateTime::from_unix_timestamp(seconds).map_err(time::error::Error::from)?;
+    // Reject years outside of the range 0000AD - 9999AD per Rfc3339
+    // upfront to prevent conversion errors in to_rfc3339().
+    // https://datatracker.ietf.org/doc/html/rfc3339#section-1
     if !(0..10_000).contains(&offset_date_time.year()) {
       return Err(time::error::Error::Format(time::error::Format::InvalidComponent("invalid year")).into());
     }
