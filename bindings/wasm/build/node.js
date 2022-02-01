973a88b78369cf7ce51af92e8571ffc9a39ef1a1
Eike Haß <eike-hass@web.de>
2022-02-01 18:44:21 +0100

    enable esm output for node

--- bindings/wasm/build/node.js
+++ bindings/wasm/build/node.js
@@ -9,18 +9,42 @@ const entryFileNode = fs.readFileSync(entryFilePathNode).toString()
 lintBigInt(entryFileNode);
 
 let changedFileNode = entryFileNode.replace(
-    "let imports = {};",
-    `if (!globalThis.fetch) {
-        const fetch = require('node-fetch')
-        globalThis.Headers = fetch.Headers
-        globalThis.Request = fetch.Request
-        globalThis.Response = fetch.Response
+    "wasm.__wbindgen_start();",
+    `import crypto from 'crypto'
+    if (!globalThis.crypto) {
+        globalThis.crypto = crypto
+    }
+    import fetch, {Headers, Request, Response} from 'node-fetch'
+    globalThis.crypto = crypto
+    if (!globalThis.fetch) {
+        globalThis.Headers = Headers
+        globalThis.Request = Request
+        globalThis.Response = Response
         globalThis.fetch = fetch
     }
-    let imports = {};`
+    wasm.__wbindgen_start();`
 )
 fs.writeFileSync(
     entryFilePathNode,
     changedFileNode
 )
 
+const bgFilePathNode = path.join(__dirname, '../node/identity_wasm_bg.js')
+const bgFileNode = fs.readFileSync(bgFilePathNode).toString()
+
+lintBigInt(bgFileNode);
+
+let changedBgFileNode = bgFileNode.replace(
+    "var ret = module.require(getStringFromWasm0(arg0, arg1));",
+`
+console.log(getStringFromWasm0(arg0, arg1));
+console.log(globalThis);
+if (getStringFromWasm0(arg0, arg1) === "crypto") { 
+  return addHeapObject(globalThis.crypto)
+} 
+var ret = module.require(getStringFromWasm0(arg0, arg1));`
+)
+fs.writeFileSync(
+    bgFilePathNode,
+    changedBgFileNode
+)
