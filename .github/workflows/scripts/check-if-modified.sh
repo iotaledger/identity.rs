43c80561a65f4e75982a429d313f5e3f74766d7f
Eike Haß <eike-hass@web.de>
2022-02-08 17:28:41 +0100

    chnage checkout behavior

--- .github/workflows/scripts/check-if-modified.sh
+++ .github/workflows/scripts/check-if-modified.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#https://git-scm.com/docs/git-diff#Documentation/git-diff.txt-emgitdiffemltoptionsgt--ltpathgt82308203
 if [[ $(git diff HEAD^ HEAD -- $1) != '' ]]; then
     # modified
     echo 'true'
