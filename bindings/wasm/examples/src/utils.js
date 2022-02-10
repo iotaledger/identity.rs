433768df44a817c0dad3c342962942e70aa2367a
Craig Bester <craig.bester@iota.org>
2022-02-10 18:24:11 +0200

    Remove example logExplorerUrl, logResolverUrl utility functions

--- bindings/wasm/examples/src/utils.js
+++ bindings/wasm/examples/src/utils.js
@@ -2,28 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /**
- Write out the Tangle Explorer URL given the network and message ID, with the given preamble.
-
- @param {!string} preamble
- @param {ExplorerUrl} explorer
- @param {!string} messageId
- **/
-function logExplorerUrl(preamble, explorer, messageId) {
-    console.log(`${preamble} ${explorer.messageUrl(messageId)}`);
-}
-
-/**
- Write out the Tangle Identity Resolver URL given the network and DID, with the given preamble.
-
- @param {!string} preamble
- @param {ExplorerUrl} explorer
- @param {!string} did
- **/
-function logResolverUrl(preamble, explorer, did) {
-    console.log(`${preamble} ${explorer.resolverUrl(did)}`);
-}
-
-/**
  Pretty-prints data to the console as a JSON string. This avoids nested fields being printed
  as [Object], [Array] by converting it to a full JSON string first.
 
@@ -37,4 +15,4 @@ function prettyPrintJSON(data, title = null) {
     console.log(JSON.stringify(JSON.parse(data.toString()), null, 2));
 }
 
-export {logExplorerUrl, logResolverUrl, prettyPrintJSON}
+export {prettyPrintJSON}
