c9d6954a36aa4dd7ce972ef1cd64a1ca542091d3
Eike Haß <eike-hass@web.de>
2022-02-28 11:09:03 +0100

    use cli timeout parameter

--- bindings/wasm/examples-account/src/tests/config.ts
+++ bindings/wasm/examples-account/src/tests/config.ts
@@ -1,10 +1,7 @@
 import {config} from "../config";
 
-const TIMEOUT = 1000*60*3; // 3 minutes to account for spurious network delays
-
 // Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
 describe("Test node examples", function () {
-    this.timeout(TIMEOUT);
     it("Create Verifiable Credential", async () => {
         await config();
     });
