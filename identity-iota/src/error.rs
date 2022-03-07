6bf7aadeabe45d72318483d2975440a9a481b2f0
Henrique Nogara <henrique.nogara@iota.org>
2022-03-07 18:42:09 -0300

    remove old error variants

--- identity-iota/src/error.rs
+++ identity-iota/src/error.rs
@@ -8,11 +8,7 @@ pub enum Error {
   #[error("{0}")]
   CoreError(#[from] identity_core::Error),
   #[error("{0}")]
-  DiffError(#[from] identity_core::diff::Error),
-  #[error("{0}")]
   CredError(#[from] identity_credential::Error),
-  #[error("{0}")]
-  InvalidDID(#[from] identity_did::did::DIDError),
   #[error("{0}")]
   InvalidDoc(#[from] identity_did::Error),
   #[error("{0}")]
@@ -24,24 +20,12 @@ pub enum Error {
 
   #[error("{0}")]
   DIDNotFound(String),
-  #[error("Invalid Document - Missing Message Id")]
-  InvalidDocumentMessageId,
-  #[error("Invalid Document - Signing Verification Method Type Not Supported")]
-  InvalidDocumentSigningMethodType,
-  #[error("Invalid Root Document")]
-  InvalidRootDocument,
-  #[error("Invalid Network Name")]
-  InvalidNetworkName,
-  #[error("signing failed: {0}")]
-  DocumentSignError(&'static str, #[source] Option<identity_core::Error>),
   #[error("{0}")]
   IncompatibleNetwork(String),
   #[error("Invalid Presentation Holder")]
   InvalidPresentationHolder,
   #[error("Chain Error: {error}")]
   ChainError { error: &'static str },
-  #[error("Missing Signing Key")]
-  MissingSigningKey,
   #[error("no client nodes provided for network")]
   NoClientNodesProvided,
   #[error("Invalid Explorer Url")]
