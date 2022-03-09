dab4265510edb67b577f35f2d24d2b8e3fbc2096
Abdulrahim Al Methiab <abdulrahim.almethiab@iota.org>
2022-03-09 09:48:00 +0100

    Revert "remove `records.rs`"
    This reverts commit fe8c90a8f221627168d076921b9f5260b31feb5b.

--- identity-account/src/stronghold/error.rs
+++ identity-account/src/stronghold/error.rs
@@ -30,4 +30,6 @@ pub enum StrongholdError {
   /// Caused by errors from an invalid Stronghold procedure.
   #[error("Stronghold error: {0}")]
   StrongholdResult(String),
+  #[error("Record Error")]
+  RecordError,
 }
