022583d18cb36d121d6e8d6d9ac9b56f4ffe3d42
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-03-15 15:35:02 +0100

    Add Wasm integration features

--- identity-actor/src/bin/wshandler.rs
+++ identity-actor/src/bin/wshandler.rs
@@ -1,35 +1,57 @@
-use p2p::{InitKeypair, Keypair};
-use libp2p::{tcp::TcpConfig, websocket::WsConfig, Multiaddr};
+// Copyright 2020-2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
 
-use identity_actor::{actor_builder::ActorBuilder, asyncfn::AsyncFn, errors::Result, StorageHandler};
+use identity_actor::remote_account::RemoteAccount;
+use identity_actor::Actor;
+use identity_actor::ActorRequest;
+use identity_actor::Multiaddr;
+use identity_actor::RequestContext;
+use identity_actor::Synchronous;
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct TestRequest(String);
+
+impl ActorRequest<Synchronous> for TestRequest {
+  type Response = String;
+
+  fn endpoint<'cow>(&self) -> std::borrow::Cow<'cow, str> {
+    std::borrow::Cow::Borrowed("test/request")
+  }
+}
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> identity_actor::Result<()> {
   // pretty_env_logger::init();
-
-  let id_keys = Keypair::generate_ed25519();
-  let tcp_transport = TcpConfig::new().nodelay(true);
-  let transport = WsConfig::new(tcp_transport);
 
   let addr: Multiaddr = "/ip4/0.0.0.0/tcp/12345/ws".parse().unwrap();
 
-  let mut comm = ActorBuilder::new()
-    .keys(InitKeypair::IdKeys(id_keys))
-    .listen_on(addr.clone())
-    .build_with_transport(transport)
-    .await?;
+  let mut actor_builder = identity_actor::ActorBuilder::new();
 
-  let handler = StorageHandler::new().await.unwrap();
+  actor_builder
+    .add_state(())
+    .add_sync_handler(
+      "test/request",
+      |_: (), _: Actor, request: RequestContext<TestRequest>| async move { request.input.0 },
+    )
+    .unwrap();
 
-  comm.add_handler(handler)
-      .add_method("storage/list", AsyncFn::new(StorageHandler::list));
+  let handler = RemoteAccount::new().unwrap();
 
-  let peer_id = comm.peer_id();
+  actor_builder
+    .add_state(handler)
+    .add_sync_handler("remote_account/list", RemoteAccount::list)?;
 
-  println!("Listening on {:?} with PeerId: {}", addr, peer_id.to_base58());
+  let mut actor = actor_builder.build().await?;
+
+  actor.start_listening(addr).await?;
+  let addrs = actor.addresses().await?;
+
+  let peer_id = actor.peer_id();
+
+  println!("Listening on {:#?} with PeerId: {}", addrs, peer_id.to_base58());
 
   // Blocks forever
-  comm.join().await;
+  futures::future::pending::<()>().await;
 
   Ok(())
 }
