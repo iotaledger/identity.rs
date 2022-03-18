2a2ccb7169e81d0b8cf4c87bb36e1992a079bbf2
Abdulrahim Al Methiab <abdulrahim.almethiab@iota.org>
2022-03-18 11:22:03 +0100

    remove `publicKeyJwk`

--- identity-did/src/diff/method_data.rs
+++ identity-did/src/diff/method_data.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use identity_core::diff::Diff;
-use identity_core::diff::DiffObject;
 use identity_core::diff::DiffString;
 use identity_core::diff::Result;
 
@@ -12,7 +11,6 @@ use crate::verification::MethodData;
 pub enum DiffMethodData {
   PublicKeyMultibase(#[serde(skip_serializing_if = "Option::is_none")] Option<DiffString>),
   PublicKeyBase58(#[serde(skip_serializing_if = "Option::is_none")] Option<DiffString>),
-  PublicKeyJwk(#[serde(skip_serializing_if = "Option::is_none")] Option<DiffObject>),
 }
 
 impl Diff for MethodData {
@@ -28,8 +26,6 @@ impl Diff for MethodData {
       }
       (Self::PublicKeyBase58(a), Self::PublicKeyBase58(b)) if a == b => Ok(DiffMethodData::PublicKeyBase58(None)),
       (Self::PublicKeyBase58(a), Self::PublicKeyBase58(b)) => a.diff(b).map(Some).map(DiffMethodData::PublicKeyBase58),
-      (Self::PublicKeyJwk(a), Self::PublicKeyJwk(b)) if a == b => Ok(DiffMethodData::PublicKeyJwk(None)),
-      (Self::PublicKeyJwk(a), Self::PublicKeyJwk(b)) => a.diff(b).map(Some).map(DiffMethodData::PublicKeyJwk),
       (_, _) => other.clone().into_diff(),
     }
   }
@@ -44,8 +40,6 @@ impl Diff for MethodData {
       }
       (Self::PublicKeyBase58(a), DiffMethodData::PublicKeyBase58(Some(b))) => a.merge(b).map(Self::PublicKeyBase58),
       (Self::PublicKeyBase58(a), DiffMethodData::PublicKeyBase58(None)) => Ok(Self::PublicKeyBase58(a.clone())),
-      (Self::PublicKeyJwk(a), DiffMethodData::PublicKeyJwk(Some(b))) => a.merge(b).map(Self::PublicKeyJwk),
-      (Self::PublicKeyJwk(a), DiffMethodData::PublicKeyJwk(None)) => Ok(Self::PublicKeyJwk(a.clone())),
       (_, diff) => Self::from_diff(diff),
     }
   }
@@ -56,8 +50,6 @@ impl Diff for MethodData {
       DiffMethodData::PublicKeyMultibase(None) => Ok(Self::PublicKeyMultibase(Default::default())),
       DiffMethodData::PublicKeyBase58(Some(value)) => Diff::from_diff(value).map(Self::PublicKeyBase58),
       DiffMethodData::PublicKeyBase58(None) => Ok(Self::PublicKeyBase58(Default::default())),
-      DiffMethodData::PublicKeyJwk(Some(value)) => Diff::from_diff(value).map(Self::PublicKeyJwk),
-      DiffMethodData::PublicKeyJwk(None) => Ok(Self::PublicKeyJwk(Default::default())),
     }
   }
 
@@ -65,7 +57,6 @@ impl Diff for MethodData {
     match self {
       Self::PublicKeyMultibase(value) => value.into_diff().map(Some).map(DiffMethodData::PublicKeyMultibase),
       Self::PublicKeyBase58(value) => value.into_diff().map(Some).map(DiffMethodData::PublicKeyBase58),
-      Self::PublicKeyJwk(value) => value.into_diff().map(Some).map(DiffMethodData::PublicKeyJwk),
     }
   }
 }
