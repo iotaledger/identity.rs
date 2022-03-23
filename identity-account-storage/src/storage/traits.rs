4fa244383334e0f1066731293b1159682171fee7
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-03-23 07:51:39 +0100

    Improve `Storage` doc

--- identity-account-storage/src/storage/traits.rs
+++ identity-account-storage/src/storage/traits.rs
@@ -34,9 +34,10 @@ mod storage_sub_trait {
 /// as well as key-value like storage of data structures, such as DID documents.
 ///
 /// This interface works with [`AccountId`] and [`IotaDID`] as the top-level identifiers.
-/// `AccountId` is intended as a partition-key, i.e. everything related to the account
-/// can be stored in its own partition. Keys are identified by [`KeyLocation`]s in that partition.
-/// An `IotaDID` can be resolved to an `AccountId` using an index, which is a global data structure.
+/// `AccountId` is intended as a partition-key, i.e. everything related to an account
+/// can be stored in its own partition. Keys belonging to an account are identified by [`KeyLocation`]s
+/// in that partition.
+/// An `IotaDID` can be resolved to an `AccountId` using the index, which is a global data structure.
 /// Therefore, index operations need to be carefully synchronized per [`Storage`] instance, while other operations
 /// only need to be synchronized within the same `AccountId`.
 /// For example, two `key_generate` operations with different account ids can be executed concurrently.
@@ -45,7 +46,7 @@ mod storage_sub_trait {
 #[cfg_attr(not(feature = "send-sync-storage"), async_trait(?Send))]
 #[cfg_attr(feature = "send-sync-storage", async_trait)]
 pub trait Storage: storage_sub_trait::StorageSendSyncMaybe + Debug {
-  /// Creates a new keypair for the given `account_id` at the given `location`.
+  /// Generates a new key for the given `account_id` at `location`.
   ///
   /// If a key at `location` exists, it is overwritten.
   async fn key_generate(&self, account_id: &AccountId, location: &KeyLocation) -> Result<()>;
@@ -57,7 +58,7 @@ pub trait Storage: storage_sub_trait::StorageSendSyncMaybe + Debug {
 
   /// Moves a key from `source` to `target`.
   ///
-  /// The key at the source location is removed. If a key at the target exists, it is overwritten.
+  /// The key at the source location is removed. If a key at the `target` exists, it is overwritten.
   async fn key_move(&self, account_id: &AccountId, source: &KeyLocation, target: &KeyLocation) -> Result<()>;
 
   /// Retrieves the public key from `location`.
@@ -74,16 +75,16 @@ pub trait Storage: storage_sub_trait::StorageSendSyncMaybe + Debug {
   /// Returns `true` if a key exists at the specified `location`.
   async fn key_exists(&self, account_id: &AccountId, location: &KeyLocation) -> Result<bool>;
 
-  /// Returns the chain state of the identity specified by `did`.
+  /// Returns the chain state of the identity specified by `account_id`.
   async fn chain_state(&self, account_id: &AccountId) -> Result<Option<ChainState>>;
 
-  /// Set the chain state of the identity specified by `did`.
+  /// Set the chain state of the identity specified by `account_id`.
   async fn set_chain_state(&self, account_id: &AccountId, chain_state: &ChainState) -> Result<()>;
 
-  /// Returns the [`IotaDocument`] of the identity specified by `did`.
+  /// Returns the [`IotaDocument`] of the identity specified by `account_id`.
   async fn document(&self, account_id: &AccountId) -> Result<Option<IotaDocument>>;
 
-  /// Sets a new state for the identity specified by `did`.
+  /// Sets a new state for the identity specified by `account_id`.
   async fn set_document(&self, account_id: &AccountId, state: &IotaDocument) -> Result<()>;
 
   /// Removes the keys and any state for the identity specified by `did`.
@@ -91,18 +92,18 @@ pub trait Storage: storage_sub_trait::StorageSendSyncMaybe + Debug {
   /// This operation is idempotent: it does not fail if the given `did` does not exist.
   async fn purge(&self, did: &IotaDID) -> Result<()>;
 
-  /// Adds the did -> account_id mapping to the index.
+  /// Adds the `did -> account_id` mapping to the index.
   ///
   /// Note that his operation needs to be synchronized globally for a given storage instance,
   /// in order to prevent race conditions when inserting index entries concurrently.
   async fn index_set(&self, did: IotaDID, account_id: AccountId) -> Result<()>;
 
-  // Looks up the [`AccountId`] of the given `did`.
+  /// Looks up the [`AccountId`] of the given `did`, returning `None` if no mapping exists.
   async fn index_get(&self, did: &IotaDID) -> Result<Option<AccountId>>;
 
-  // Retrieve the list of DIDs stored in the index.
+  /// Retrieves the list of [`IotaDID`]s stored in the index.
   async fn index(&self) -> Result<Vec<IotaDID>>;
 
-  /// Write any unsaved changes to disk.
+  /// Persists any unsaved changes.
   async fn flush_changes(&self) -> Result<()>;
 }
