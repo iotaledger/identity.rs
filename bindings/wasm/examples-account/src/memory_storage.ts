3602dd071202d66750e5737afab2628c34a35cd5
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-03-31 12:28:11 +0200

    Implement TS MemStore

--- bindings/wasm/examples-account/src/memory_storage.ts
+++ bindings/wasm/examples-account/src/memory_storage.ts
@@ -1,117 +1,176 @@
 // Copyright 2020-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import {ChainState, DID, Ed25519, Generation, IdentityState, KeyLocation, KeyPair, KeyType, MethodType, PrivateKey, Signature, Storage} from './../../node/identity_wasm.js';
+import { ChainState, DID, Document, Ed25519, KeyLocation, KeyPair, KeyType, Signature, Storage } from './../../node/identity_wasm.js';
 
 // TODO: add thorough comments explaining what this is and how to use it with an Account.
-class MemStore implements Storage {
+export class MemStore implements Storage {
     // TODO: check if map key comparison works as-expected.
     //       I.e. does a parsed/deserialized DID map to the same DID object?
     private _chainStates: Map<DID, ChainState>;
-    private _states: Map<DID, IdentityState>;
+    private _documents: Map<DID, Document>;
     private _vaults: Map<DID, Map<KeyLocation, KeyPair>>;
+    private _list: Set<DID>;
 
     constructor() {
         this._chainStates = new Map();
-        this._states = new Map();
+        this._documents = new Map();
         this._vaults = new Map();
+        this._list = new Set();
     }
 
-    public async flushChanges(): Promise<void> {}
-
-    public async keyNew(did: DID, keyLocation: KeyLocation): Promise<string> {
-        if (keyLocation.method !== MethodType.Ed25519VerificationKey2018()) {
-            throw new Error('Unsuported Method')
+    public async didCreate(network: string, fragment: string, privateKey: Uint8Array | undefined | null): Promise<[DID, KeyLocation]> {
+        let keyPair;
+        if (privateKey) {
+            keyPair = KeyPair.tryFromPrivateKeyBytes(KeyType.Ed25519, privateKey);
+        } else {
+            keyPair = new KeyPair(KeyType.Ed25519);
         }
-        const keyPair: KeyPair = new KeyPair(KeyType.Ed25519);
-        const publicKey: string = keyPair.public;
-        if (this._vaults.has(did)) {
-            this._vaults.get(did).set(keyLocation, keyPair);
+
+
+        const keyLocation: KeyLocation = new KeyLocation(KeyType.Ed25519, fragment, keyPair.public());
+
+        let did: DID = new DID(keyPair.public(), network);
+
+        if (this._list.has(did)) {
+            throw new Error("identity already exists");
+        } else {
+            this._list.add(did);
+        }
+
+        const vault: Map<KeyLocation, KeyPair> | undefined = this._vaults.get(did);
+
+        if (vault) {
+            vault.set(keyLocation, keyPair);
         } else {
             let newVault: Map<KeyLocation, KeyPair> = new Map([[keyLocation, keyPair]]);
             this._vaults.set(did, newVault);
         }
-        return publicKey
+
+        return [did, keyLocation];
     }
 
-    public async keyInsert(did: DID, keyLocation: KeyLocation, privateKey: string): Promise<string> {
-        if (keyLocation.method !== MethodType.Ed25519VerificationKey2018()) {
-            throw new Error('Unsuported Method')
+    public async didPurge(did: DID): Promise<boolean> {
+        if (this._list.has(did)) {
+            this._list.delete(did);
+            this._chainStates.delete(did);
+            this._documents.delete(did);
+            this._vaults.delete(did);
+            return true;
         }
-        let secretKey: PrivateKey = PrivateKey.fromBase58String(privateKey);
-        let publicKey: string = secretKey.publicKey();
-        let keyPair: KeyPair = KeyPair.fromBase58(KeyType.Ed25519, privateKey, publicKey);
-        if (this._vaults.has(did)) {
-            this._vaults.get(did).set(keyLocation, keyPair);
+
+        return false;
+    }
+
+    public async didExists(did: DID): Promise<boolean> {
+        return this._list.has(did);
+    }
+
+    public async didList(): Promise<Array<DID>> {
+        return Array.from(this._list);
+    }
+
+    public async keyGenerate(did: DID, keyType: KeyType, fragment: string): Promise<KeyLocation> {
+        const keyPair: KeyPair = new KeyPair(keyType);
+        const keyLocation: KeyLocation = new KeyLocation(KeyType.Ed25519, fragment, keyPair.public());
+
+        const vault: Map<KeyLocation, KeyPair> | undefined = this._vaults.get(did);
+
+        if (vault) {
+            vault.set(keyLocation, keyPair);
         } else {
             let newVault: Map<KeyLocation, KeyPair> = new Map([[keyLocation, keyPair]]);
             this._vaults.set(did, newVault);
         }
-        return publicKey
+
+        return keyLocation;
+    }
+
+    public async keyInsert(did: DID, keyLocation: KeyLocation, privateKey: Uint8Array): Promise<void> {
+        const keyPair: KeyPair = KeyPair.tryFromPrivateKeyBytes(keyLocation.keyType(), privateKey);
+
+        const vault: Map<KeyLocation, KeyPair> | undefined = this._vaults.get(did);
+
+        if (vault) {
+            vault.set(keyLocation, keyPair);
+        } else {
+            let newVault: Map<KeyLocation, KeyPair> = new Map([[keyLocation, keyPair]]);
+            this._vaults.set(did, newVault);
+        }
     }
 
     public async keyExists(did: DID, keyLocation: KeyLocation): Promise<boolean> {
-        if (this._vaults.has(did)) {
-            let vault: Map<KeyLocation, KeyPair> = this._vaults.get(did);
-            return vault.has(keyLocation)
+        const vault: Map<KeyLocation, KeyPair> | undefined = this._vaults.get(did);
+
+        if (vault) {
+            return vault.has(keyLocation);
+        } else {
+            return false
         }
-        return false
     }
 
-    public async keyGet(did: DID, keyLocation: KeyLocation): Promise<string> {
-        if (this._vaults.has(did)) {
-            let vault: Map<KeyLocation, KeyPair> = this._vaults.get(did);
-            if (vault.has(keyLocation)) {
-                let keyPair: KeyPair = vault.get(keyLocation);
-                return keyPair.public
+    public async keyPublic(did: DID, keyLocation: KeyLocation): Promise<Uint8Array> {
+        const vault: Map<KeyLocation, KeyPair> | undefined = this._vaults.get(did);
+
+        if (vault) {
+            const keyPair: KeyPair | undefined = vault.get(keyLocation);
+            if (keyPair) {
+                return keyPair.public()
+            } else {
+                throw new Error('Key location not found')
             }
-            throw new Error('Key location not found')
+        } else {
+            throw new Error('DID not found')
         }
-        throw new Error('DID not found')
     }
 
-    public async keyDel(did: DID, keyLocation: KeyLocation): Promise<void> {
-        if (this._vaults.has(did)) {
-            this._vaults.get(did).delete(keyLocation);
+    public async keyDelete(did: DID, keyLocation: KeyLocation): Promise<boolean> {
+        const vault: Map<KeyLocation, KeyPair> | undefined = this._vaults.get(did);
+
+        if (vault) {
+            return vault.delete(keyLocation);
+        } else {
+            return false;
         }
     }
 
     public async keySign(did: DID, keyLocation: KeyLocation, data: Uint8Array): Promise<Signature> {
-        if (!this._vaults.has(did)) {
+        if (keyLocation.keyType() !== KeyType.Ed25519) {
+            throw new Error('Unsupported Method')
+        }
+
+        const vault: Map<KeyLocation, KeyPair> | undefined = this._vaults.get(did);
+
+        if (vault) {
+            const keyPair: KeyPair | undefined = vault.get(keyLocation);
+
+            if (keyPair) {
+                let signature: Uint8Array = Ed25519.sign(data, keyPair.private());
+                return new Signature(signature)
+            } else {
+                throw new Error('Key location not found')
+            }
+        } else {
             throw new Error('DID not found')
         }
-        let vault: Map<KeyLocation, KeyPair> = this._vaults.get(did);
-        if (!vault.has(keyLocation)) {
-            throw new Error('Key location not found')
-        }
-        let keyPair: KeyPair = vault.get(keyLocation);
-        if (keyLocation.method !== MethodType.Ed25519VerificationKey2018()) {
-            throw new Error('Unsuported Method')
-        }
-        let signature: Uint8Array = Ed25519.sign(data, keyPair.private);
-        return new Signature(keyPair.public, signature)
     }
 
-    public async chainState(did: DID): Promise<ChainState> {
+    public async chainStateGet(did: DID): Promise<ChainState | null | undefined> {
         return this._chainStates.get(did);
     }
 
-    public async setChainState(did: DID, chainState: ChainState): Promise<void> {
+    public async chainStateSet(did: DID, chainState: ChainState): Promise<void> {
         this._chainStates.set(did, chainState);
     }
 
-    public async state(did: DID): Promise<IdentityState> {
-        return this._states.get(did)
+    public async documentGet(did: DID): Promise<Document | null | undefined> {
+        return this._documents.get(did)
     }
 
-    public async setState(did: DID, identityState: IdentityState): Promise<void> {
-        this._states.set(did, identityState);
+    public async documentSet(did: DID, document: Document): Promise<void> {
+        this._documents.set(did, document);
     }
 
-    public async purge(did: DID): Promise<void> {
-        this._chainStates.delete(did);
-        this._states.delete(did);
-        this._vaults.delete(did);
-    }
+    public async flushChanges(): Promise<void> { }
 }
 
