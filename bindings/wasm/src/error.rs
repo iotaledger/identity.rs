6d23081c3f065ded74cfc1ab2460b7e412040787
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-04-06 14:06:22 +0200

    Use debug format for `JsValue` error conversion

--- bindings/wasm/src/error.rs
+++ bindings/wasm/src/error.rs
@@ -211,15 +211,9 @@ impl JsValueResult {
   /// Consumes the struct and returns a Result<_, AccountStorageError>
   pub fn account_err(self) -> StdResult<JsValue, AccountStorageError> {
     self.0.map_err(|js_value| {
-      let error: std::result::Result<String, String> = js_value
-        .into_serde()
-        .map_err(|err| err.to_string())
-        .and_then(|value: serde_json::Value| serde_json::to_string(&value).map_err(|err| err.to_string()));
-
-      match error {
-        Ok(json) => AccountStorageError::JsError(json),
-        Err(error) => AccountStorageError::JsError(format!("failed to serialize JS error: {}", error)),
-      }
+      // Using the debug format includes a backtrace, which is awkwardly formatted,
+      // but no other way of extracting the error, like serialization, works.
+      AccountStorageError::JsError(format!("{:?}", js_value))
     })
   }
 }
