299ac1663ba68c55f95a7464f82c5173b6045fc2
Abdulrahim Al Methiab <abdulrahim.almethiab@iota.org>
2022-04-08 00:27:39 +0200

    remove outdated JS tests

--- bindings/wasm/examples/src/tests/node/createVP.js
+++
@@ -1,9 +0,0 @@
-import {createVP} from "../../create_vp";
-import {CLIENT_CONFIG} from "../../config";
-
-// Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
-describe("Test node examples", function () {
-    it("Create Verifiable Presentation", async () => {
-        await createVP(CLIENT_CONFIG);
-    });
-})
