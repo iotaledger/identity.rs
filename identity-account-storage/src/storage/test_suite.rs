c2f0a71ca636b0f3b68af5be5243bf3b826d47e2
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-04-08 12:03:29 +0200

    Move `did_purge` call away from `did_list` test

--- identity-account-storage/src/storage/test_suite.rs
+++ identity-account-storage/src/storage/test_suite.rs
@@ -276,24 +276,6 @@ impl StorageTestSuite {
       dids.push(did);
     }
 
-    for (i, did) in dids.into_iter().enumerate() {
-      let purged: bool = storage.did_purge(&did).await.context("did_purge returned an error")?;
-
-      ensure!(purged, "expected the did `{did}` to be purged");
-
-      let exists: bool = storage.did_exists(&did).await.context("did_exists returned an error")?;
-      ensure!(!exists, "expected did `{did}` to no longer exist");
-
-      let list_len: usize = storage.did_list().await.context("did_list returned an error")?.len();
-      let expected_len: usize = NUM_IDENTITIES - (i + 1);
-
-      ensure_eq!(
-        list_len,
-        expected_len,
-        "expected did_list to return a list of len {expected_len}, got {list_len} elements instead"
-      );
-    }
-
     Ok(())
   }
 
@@ -474,6 +456,14 @@ impl StorageTestSuite {
       .await
       .context("did_create returned an error")?;
 
+    let list_len: usize = storage.did_list().await.context("did_list returned an error")?.len();
+
+    ensure_eq!(
+      list_len,
+      1,
+      "expected did_list to return a list of size 1 after creation"
+    );
+
     let mut expected_chain_state: ChainState = ChainState::new();
     expected_chain_state.set_last_integration_message_id(MessageId::new([0xff; 32]));
 
@@ -482,7 +472,9 @@ impl StorageTestSuite {
       .await
       .context("chain_state_set returned an error")?;
 
-    storage.did_purge(&did).await.context("did_purge returned an error")?;
+    let purged: bool = storage.did_purge(&did).await.context("did_purge returned an error")?;
+
+    ensure!(purged, "expected did `{did}` to have been purged");
 
     let chain_state: Option<ChainState> = storage
       .chain_state_get(&did)
@@ -502,6 +494,13 @@ impl StorageTestSuite {
     ensure!(
       !exists,
       "expected key at location `{location}` to no longer exist after purge"
+    );
+
+    let list: Vec<IotaDID> = storage.did_list().await.context("did_list returned an error")?;
+
+    ensure!(
+      list.is_empty(),
+      "expected did_list to return an empty list after purging"
     );
 
     Ok(())
