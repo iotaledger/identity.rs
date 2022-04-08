ce546df7566752d1d2a4bc3a81476ea7ab3cf041
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-04-08 14:22:11 +0200

    Separate mocha tests

--- bindings/stronghold-nodejs/examples/src/tests/storage_test_suite.ts
+++ bindings/stronghold-nodejs/examples/src/tests/storage_test_suite.ts
@@ -2,18 +2,31 @@ import { stronghold } from '../stronghold';
 import { StorageTestSuite } from '@iota/identity-wasm/node';
 
 // Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
-describe("Test Stronghold Node.js examples", function () {
-    it("Storage Test Suite", async () => {
+describe("Test Stronghold Node.js", function () {
+    it("didCreateGenerateKey", async () => {
         await StorageTestSuite.didCreateGenerateKeyTest(await stronghold());
-        await StorageTestSuite.didCreatePrivateKeyTest(await stronghold());
-        await StorageTestSuite.keyGenerateTest(await stronghold());
-        await StorageTestSuite.keyDeleteTest(await stronghold());
-        await StorageTestSuite.keyInsertTest(await stronghold());
-        await StorageTestSuite.didListTest(await stronghold());
-        await StorageTestSuite.keySignEd25519Test(await stronghold());
-        // TODO: Deliberately exclude didPurge test because key deletion
-        // is not implemented properly in stronghold. Should be activated with #757.
-        // In that case, we can just call the storage_test_suite Wasm account example.
-        // await StorageTestSuite.didPurgeTest(await stronghold());
     });
-})
+    it("didCreatePrivateKey", async () => {
+        await StorageTestSuite.didCreatePrivateKeyTest(await stronghold());
+    });
+    it("keyGenerate", async () => {
+        await StorageTestSuite.keyGenerateTest(await stronghold());
+    });
+    it("keyGenerate", async () => {
+        await StorageTestSuite.keyDeleteTest(await stronghold());
+    });
+    it("keyInsert", async () => {
+        await StorageTestSuite.keyInsertTest(await stronghold());
+    });
+    it("didList", async () => {
+        await StorageTestSuite.didListTest(await stronghold());
+    });
+    it("keySignEd25519", async () => {
+        await StorageTestSuite.keySignEd25519Test(await stronghold());
+    });
+    // TODO: Deliberately exclude didPurge test because key deletion
+    // is not implemented properly in stronghold. Should be activated with #757.
+    // it("didPurge", async () => {
+    //     await StorageTestSuite.didPurgeTest(await stronghold());
+    // });
+});
