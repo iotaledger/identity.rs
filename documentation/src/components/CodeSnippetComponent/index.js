eae1cf80fda48c0a441b780b9f9d97cc553000ca
Eike Haß <eike-hass@web.de>
2022-04-25 16:51:04 +0200

    latest docs from dev

--- documentation/src/components/CodeSnippetComponent/index.js
+++ documentation/src/components/CodeSnippetComponent/index.js
@@ -1,74 +1,111 @@
 import clsx from "clsx";
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "./styles.css";
+import CodeBlock from "@theme/CodeBlock";
 
-export default function CodeSnippet({ nodeReplitLink, rustReplitLink }) {
-    const [lang, setLang] = React.useState("node");
+export default function CodeSnippet({nodeReplitLink, rustContent, nodeGithubLink, rustGithubLink}) {
+  const [lang, setLang] = React.useState("node");
 
-    useEffect(() => {
-        //Set Language from storage, default to node
-        let langFromStorage = localStorage.getItem("lang");
-        let lang = langFromStorage ? langFromStorage : "node";
+  const ARROW_OUT_OF_BOX_ICON = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      fill="currentColor"
+      class="bi bi-box-arrow-up-right"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+      />
+      <path
+        fill-rule="evenodd"
+        d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+      />
+    </svg>
+  );
 
-        //If Replit doesn't exist default to next option
-        if(lang === "node" && !nodeReplitLink) {
-            lang = "rust";
-        }
-        if(lang === "rust" && !rustReplitLink) {
-            lang = "node";
-        }
-        setLang(lang);
-    });
+  useEffect(() => {
+    //Set Language from storage, default to node
+    let langFromStorage = localStorage.getItem("lang");
+    let lang = langFromStorage ? langFromStorage : "node";
 
-    return (
-        <div>
-            <div className={clsx("langSelector")}>
-                { nodeReplitLink &&
-                <button
-                    className={clsx("button", "languageButton", "mr-sm", {
-                        activeButton: lang === "node",
-                        inactiveButton: lang !== "node",
-                    })}
-                    onClick={() => {
-                        localStorage.setItem("lang", "node");
-                        setLang("node");
-                    }}
-                >
-                    Node.js
-                </button>
-                }
-                { rustReplitLink &&
-                <button
-                    className={clsx("button", "languageButton", {
-                        activeButton: lang == "rust",
-                        inactiveButton: lang !== "rust",
-                    })}
-                    onClick={() => {
-                        localStorage.setItem("lang", "rust");
-                        setLang("rust");
-                    }}
-                >
-                    Rust
-                </button>
-                }
-            </div>
-            <div className={clsx("codeSnippetContainer")}>
-                {lang === "node" ? (
-                    <iframe
-                        frameborder="0"
-                        width="100%"
-                        height="600px"
-                        src={nodeReplitLink}
-                    ></iframe>
-                ) : (
-                    <iframe
-                        frameborder="0"
-                        width="100%"
-                        height="600px"
-                        src={rustReplitLink}
-                    ></iframe>
-                )}
-            </div>
-        </div>
-    );
+    //If Replit doesn't exist default to next option
+    if (lang === "node" && !nodeReplitLink) {
+      lang = "rust";
+    }
+    if (lang === "rust" && !rustContent) {
+      lang = "node";
+    }
+    setLang(lang);
+  });
+
+  return (
+    <div>
+
+      {/***** Tabs *****/}
+      <div className={clsx("langSelector")}>
+        {nodeReplitLink && (
+          <button
+            className={clsx("button", "languageButton", "mr-sm", {
+              activeButton: lang === "node",
+              inactiveButton: lang !== "node"
+            })}
+            onClick={() => {
+              localStorage.setItem("lang", "node");
+              setLang("node");
+            }}
+          >
+            Node.js
+          </button>
+        )}
+        {rustContent && (
+          <button
+            className={clsx("button", "languageButton", {
+              activeButton: lang == "rust",
+              inactiveButton: lang !== "rust"
+            })}
+            onClick={() => {
+              localStorage.setItem("lang", "rust");
+              setLang("rust");
+            }}
+          >
+            Rust
+          </button>
+        )}
+      </div>
+
+      {/***** Code Snippet *****/}
+      <div className={clsx("codeSnippetContainer")}>
+        {lang === "node" ? (
+          <>
+            <iframe frameborder="0" width="100%" height="700px" src={nodeReplitLink}></iframe>
+          </>
+        ) : (
+          <div className={clsx("rustContainer")}>
+            <CodeBlock className={clsx("noMarginBottom")} language="rust">
+              {rustContent}
+            </CodeBlock>
+          </div>
+        )}
+      </div>
+
+      {/*****  Github Link *****/}
+      <div className={clsx("githubLink")}>
+        {nodeGithubLink && lang === "node" && (
+          <a href={nodeGithubLink} target="_blank">
+            GitHub&nbsp;
+            {ARROW_OUT_OF_BOX_ICON}
+          </a>
+        )}
+        {rustGithubLink && lang === "rust" && (
+          <a href={rustGithubLink} target="_blank">
+            GitHub&nbsp;
+            {ARROW_OUT_OF_BOX_ICON}
+          </a>
+        )}
+      </div>
+    </div>
+  );
 }
