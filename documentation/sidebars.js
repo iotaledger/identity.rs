9c6f073d9adf6eac5ce362e83ea6cd0cdaba65e7
Oliver Anderson <oliver.anderson@iota.org>
2022-05-04 11:18:36 +0200

    incorporated suggestions

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -42,7 +42,6 @@ module.exports = {
                 'decentralized_identifiers/create',
                 'decentralized_identifiers/update',
                 'decentralized_identifiers/resolve',
-                'decentralized_identifiers/resolve_history',
                 'decentralized_identifiers/private_tangle',
             ],
             'Verifiable Credentials': [
