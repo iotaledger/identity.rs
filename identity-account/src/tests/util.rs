8d3bd595c297ddee03fb201b37541b84507bfc2d
Philipp <philipp.gackstatter@iota.org>
2022-05-06 16:56:16 +0200

    Upgrade to new `Stronghold` interface (#787)
    * Rename stronghold module
    
    * Postfix old stronghold with `_old`
    
    * Migrate to new stronghold interface
    
    * Impl did_create properly with client syncing
    
    * Add context to `StrongholdError`s
    
    * Add `Stronghold` wrapper test
    
    * Add `test_key_delete`
    
    * Add storage_test_suite setup & did_create test
    
    * Re-export test suite feature
    
    * Expose test suite in Wasm
    
    * Extend `did_create` test, fix index persistence
    
    * Test `key_generate`
    
    * Move `key_delete` to test suite
    
    * Remove test suite from this branch
    
    * Add initial test suite and expose to Wasm
    
    * rm `Error` postfix from `StrongholdError` variants
    
    * Remove duplicate `mod tests` in Wasm
    
    * Handle client sync error; document syncing
    
    * Use updated stronghold
    
    * Use dedicated `load_snapshot` function
    
    * Purge client in `did_purge`
    
    * Revert cfg_attr shenanigans
    
    * Make `Stronghold::client` not async
    
    * Remove asyncness from fns where not necessary
    
    * Make `mutate_client` not async either
    
    * Move test_util mod where it belongs
    
    * Remove `source` errors from `Display` impl
    
    * Remove `RecordHint` everywhere
    
    * Use base crate `MemoryError`; remove engine dep
    
    * Revert temporary send/sync change
    
    * Document `Stronghold` wrapper
    
    * Use same export style as other crates
    
    * Create parent directories if they don't exist
    
    * Remove outdated TODO
    
    * Fix index writing in purge; update stronghold rev
    
    * Remove old stronghold wrapper
    
    * Reactivate multi identity example
    
    * Add `dropsave` getter/setter
    
    * Fully qualify `std::any::type_name`
    
    * Remove tests which are already in test suite
    
    * Reactivate `Send`-assertion test
    
    * Return `Stronghold` instance from test `storages`
    
    * Test incorrect password returns error
    
    * Use `OsRng` instead of `thread_rng`
    
    * Bump stronghold revision
    
    * Remove unused `getrandom` depenency
    
    * Remove unused `actix` dependency
    
    * Remove tokio `rt-multi-thread` feature
    
    * Prefer `sample_string` over `sample_iter`
    
    * Enable `didPurge` test for NAPI stronghold
    
    * Simplify `did_create` by using `mutate_client`
    
    * Rename doc/state client paths to store keys
    
    * Add procedure_error fn to reduce err map code
    
    * Remove unnecessary clone
    
    * Disable multiple identities example temporarily
    
    * Disable musl build
    
    * Remove musl target from stronghold-nodejs
    
    * use local workflow file
    
    * Revert "use local workflow file"
    
    This reverts commit 2f12afd088ebe80476f858dba13efede12463709.
    
    Co-authored-by: Eike Haß <eike-hass@web.de>

--- identity-account/src/tests/util.rs
+++ identity-account/src/tests/util.rs
@@ -5,9 +5,11 @@ use std::sync::Arc;
 
 use identity_account_storage::storage::MemStore;
 use identity_account_storage::storage::Storage;
+use identity_account_storage::storage::Stronghold;
 use identity_iota::tangle::ClientBuilder;
 use identity_iota_core::tangle::Network;
-use rand::Rng;
+use rand::distributions::DistString;
+use rand::rngs::OsRng;
 
 use crate::account::AccountConfig;
 use crate::account::AccountSetup;
@@ -31,22 +33,21 @@ pub(super) async fn account_setup_storage(storage: Arc<dyn Storage>, network: Ne
   )
 }
 
-// TODO: Unused for now, used to generate a temporary stronghold file path.
-fn _temporary_random_path() -> String {
+pub(super) fn temporary_random_path() -> String {
   let mut file = std::env::temp_dir();
   file.push("test_strongholds");
-  file.push(
-    rand::thread_rng()
-      .sample_iter(rand::distributions::Alphanumeric)
-      .take(32)
-      .map(char::from)
-      .collect::<String>(),
-  );
+  file.push(rand::distributions::Alphanumeric.sample_string(&mut OsRng, 32));
   file.set_extension("stronghold");
   file.to_str().unwrap().to_owned()
 }
 
-// TODO: This should also return a `Stronghold`.
-pub(super) async fn storages() -> [Arc<dyn Storage>; 1] {
-  [Arc::new(MemStore::new())]
+pub(super) async fn storages() -> [Arc<dyn Storage>; 2] {
+  [
+    Arc::new(MemStore::new()),
+    Arc::new(
+      Stronghold::new(&temporary_random_path(), "password".to_owned(), Some(false))
+        .await
+        .unwrap(),
+    ),
+  ]
 }
