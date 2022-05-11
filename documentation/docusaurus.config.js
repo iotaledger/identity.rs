83b38058a4c325b6d0d38d2b3d1e64d466b5bf31
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-11 14:42:23 +0200

    Clean up PR and update cli

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -10,13 +10,13 @@ module.exports = {
             '@docusaurus/plugin-content-docs',
             {
                 id: 'identity-rs',
-                path: path.resolve(__dirname, './docs'),
+                path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'identity.rs',
-                sidebarPath: path.resolve(__dirname, './sidebars.js'),
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
                 editUrl: 'https://github.com/iotaledger/identity/edit/support/v0.5',
                 remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
             }
         ],
     ],
-    staticDirectories: [path.resolve(__dirname, './static')],
+    staticDirectories: [path.resolve(__dirname, 'static')],
 };
