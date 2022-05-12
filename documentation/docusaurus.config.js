d7b420cd774862ca0e316f34ff063d4d129e6d91
Eike Haß <eike-hass@web.de>
2022-05-12 13:27:00 +0200

    Merge pull request #771 from Dr-Electron/docs/new-build-system
    Prepare for new wiki build system

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'identity-rs',
+                path: path.resolve(__dirname, 'docs'),
+                routeBasePath: 'identity.rs',
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+                editUrl: 'https://github.com/iotaledger/identity/edit/support/v0.5/documentation',
+                remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, 'static')],
+};
