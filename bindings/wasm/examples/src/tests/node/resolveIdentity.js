40186714d5c992c618d185210b8fb8993d953c30
Craig Bester <craig.bester@iota.org>
2022-06-09 18:32:34 +0200

    Fix node test

--- bindings/wasm/examples/src/tests/node/resolveIdentity.js
+++ bindings/wasm/examples/src/tests/node/resolveIdentity.js
@@ -3,7 +3,7 @@ import {CLIENT_CONFIG} from "../../config";
 
 // Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
 describe("Test node examples", function () {
-    it("Resolution", async () => {
+    it("Resolve Identity", async () => {
         await resolveIdentity(CLIENT_CONFIG);
     });
 })
