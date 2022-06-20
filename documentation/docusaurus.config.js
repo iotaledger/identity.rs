e23ded700150298efe04d5a94653e699a91dda06
Dr-Electron <dr-electr0n@protonmail.com>
2022-06-20 12:40:29 +0200

    Add version path

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 versions: {
                     current: {
                         label: '0.5',
+                        path: '0.5',
                         badge: true
                     },
                 },
