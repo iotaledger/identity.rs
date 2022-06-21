2bb3dff593b4b931bfef48eb0554462e9276c729
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-06-21 13:53:30 +0300

    Revert another unintended change

--- identity_core/src/common/timestamp.rs
+++ identity_core/src/common/timestamp.rs
@@ -59,7 +59,7 @@ impl Timestamp {
     let milliseconds_since_unix_epoch: i64 = js_sys::Date::now() as i64;
     let seconds: i64 = milliseconds_since_unix_epoch / 1000;
     // expect is okay, we assume the current time is between 0AD and 9999AD
-    Self::from_unix(seconds).expect("Timestamp failed to convert agent datetime")
+    Self::from_unix(seconds).expect("Timestamp failed to convert system datetime")
   }
 
   /// Returns the `Timestamp` as an [RFC 3339](https://tools.ietf.org/html/rfc3339) `String`.
