3504bd8d10fe448fa01b71167090fa9a3dd763cd
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-07-26 16:44:12 +0300

    Add resolve_did example

---
+++ identity_stardust/examples/resolve_did.rs
@@ -0,0 +1,22 @@
+// Copyright 2020-2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use identity_stardust::StardustClientExt;
+use identity_stardust::StardustDocument;
+use iota_client::Client;
+
+mod create_did;
+
+/// Demonstrate how to resolve an existing DID in an alias output.
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+  let (client, _, _, document): (Client, _, _, StardustDocument) = create_did::run().await?;
+
+  let resolved_doc: StardustDocument = client.resolve(document.id()).await?;
+
+  assert_eq!(resolved_doc, document);
+
+  println!("Resolved DID Document: {resolved_doc:#?}");
+
+  Ok(())
+}
