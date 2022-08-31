de7a211997ecf4831242707a462a0623176a592b
PhilippGackstatter <philipp.gackstatter@iota.org>
2022-08-31 11:20:30 +0200

    Re-export either bee-block or `iota-client::block

--- identity_stardust/src/error.rs
+++ identity_stardust/src/error.rs
@@ -31,7 +31,7 @@ pub enum Error {
   RevocationError(#[source] identity_did::Error),
   #[cfg(feature = "client")]
   #[error("alias output build error")]
-  AliasOutputBuildError(#[source] iota_client::block::Error),
+  AliasOutputBuildError(#[source] crate::block::Error),
   #[cfg(feature = "iota-client")]
   #[error("output with id `{0}` is not an alias output")]
   NotAnAliasOutput(iota_client::block::output::OutputId),
