ac5f1f11af1d250632cfcd39e2e27bfa9f9cd381
Oliver Anderson <oliver.anderson@iota.org>
2022-09-01 16:48:23 +0200

    removed code

--- identity_resolver/src/resolution/tests/success.rs
+++ identity_resolver/src/resolution/tests/success.rs
@@ -1,15 +1,10 @@
 // Copyright 2020-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use identity_core::common::Timestamp;
 use identity_core::convert::FromJson;
-use identity_core::convert::ToJson;
 use identity_core::crypto::KeyPair;
-use identity_core::crypto::KeyType;
 use identity_core::crypto::ProofOptions;
-use identity_core::utils::BaseEncoding;
 use identity_credential::presentation::Presentation;
-use identity_credential::validator::CredentialValidator;
 use identity_credential::validator::FailFast;
 use identity_credential::validator::PresentationValidationOptions;
 use identity_credential::validator::SubjectHolderRelationship;
@@ -32,11 +27,16 @@ const HOLDER_PRIVATE_KEY: [u8; 32] = [
   246, 237, 79, 184, 166, 251,
 ];
 
-// Not used, but can be useful for maintenance purposes 
-const _ISSUER_IOTA_PRIVATE_KEY: [u8; 32] = [157, 97, 177, 157, 239, 253, 90, 96, 186, 132, 74, 244, 146, 236, 44, 196, 68, 73, 197, 105, 123, 50, 105, 25, 112, 59, 172, 3, 28, 174, 127, 96]; 
+// Not used, but can be useful for maintenance purposes.
+const _ISSUER_IOTA_PRIVATE_KEY: [u8; 32] = [
+  157, 97, 177, 157, 239, 253, 90, 96, 186, 132, 74, 244, 146, 236, 44, 196, 68, 73, 197, 105, 123, 50, 105, 25, 112,
+  59, 172, 3, 28, 174, 127, 96,
+];
 
-const _ISSUER_BAR_PRIVATE_KEY: [u8; 32] = [197, 170, 141, 244, 63, 159, 131, 123, 237, 183, 68, 47, 49, 220, 183, 177, 102, 211, 133, 53, 7, 111, 9, 75, 133, 206, 58, 46, 11, 68, 88, 247]; 
-
+const _ISSUER_BAR_PRIVATE_KEY: [u8; 32] = [
+  197, 170, 141, 244, 63, 159, 131, 123, 237, 183, 68, 47, 49, 220, 183, 177, 102, 211, 133, 53, 7, 111, 9, 75, 133,
+  206, 58, 46, 11, 68, 88, 247,
+];
 
 const HOLDER_DOC_JSON: &str = r#"{
     "id": "did:foo:586Z7H2vpX9qNhN2T4e9Utugie3ogjbxzGaMtM3E6HR5",
@@ -194,35 +194,7 @@ where
   resolver.attach_handler("foo".to_owned(), resolve_foo);
   resolver.attach_handler("bar".to_owned(), resolve_bar);
 
-  let (mut presentation, challenge): (Presentation, String) = signed_presentation();
-
-  let issuer_iota_doc: StardustDocument = resolve_iota("did:iota:0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".parse().unwrap()).await.unwrap(); 
-  let issuer_bar_doc: CoreDocument = resolve_bar("did:bar:Hyx62wPQGyvXCoihZq1BrbUjBRh2LuNxWiiqMkfAuSZr".parse().unwrap()).await.unwrap(); 
-
-
-  for mut credential in presentation.clone().verifiable_credential.into_iter() {
-    credential.expiration_date = Some(Timestamp::parse("2050-09-01T08:35:44Z").unwrap()); 
-    let did_str: String = CredentialValidator::extract_issuer::<CoreDID,_>(&credential).map(|value| value.to_string()).unwrap_or("".into()); 
-    if did_str == issuer_bar_doc.id().as_str() {
-      issuer_bar_doc.signer(
-        KeyPair::try_from_private_key_bytes(KeyType::Ed25519, &_ISSUER_BAR_PRIVATE_KEY).unwrap().private()
-      )
-      .options(ProofOptions::default())
-      .method(issuer_bar_doc.methods().next().unwrap().id())
-      .sign(&mut credential).unwrap(); 
-    } else if did_str == issuer_iota_doc.id().as_str() {
-      issuer_iota_doc.signer(
-        KeyPair::try_from_private_key_bytes(KeyType::Ed25519, &_ISSUER_IOTA_PRIVATE_KEY).unwrap().private()
-      )
-      .options(ProofOptions::default())
-      .method(issuer_iota_doc.methods().next().unwrap().id())
-      .sign(&mut credential).unwrap(); 
-    }
-
-    println!("{}", credential.to_json_pretty().unwrap()); 
-    }
-
-  
+  let (presentation, challenge): (Presentation, String) = signed_presentation();
 
   // resolve the DID documents of the presentation's holder and credential issuers.
   let holder_doc = resolver.resolve_presentation_holder(&presentation).await.unwrap();
@@ -234,7 +206,7 @@ where
     for pass_issuers_as_arg in [true, false] {
       let holder: Option<&DOC> = pass_holder_as_arg.then_some(&holder_doc);
       let issuers: Option<&[DOC]> = pass_issuers_as_arg.then_some(&issuer_docs);
-      let validation_result = resolver
+      assert!(resolver
         .verify_presentation(
           &presentation,
           &PresentationValidationOptions::new()
@@ -244,23 +216,8 @@ where
           holder,
           issuers
         )
-        .await; 
-        if validation_result.is_err() {
-          dbg!(holder); 
-          dbg!(issuers);
-          dbg!(&validation_result); 
-       
-
-          let issuer_iota: &str = "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60";
-          let issuer_iota_decoded = BaseEncoding::decode(issuer_iota, identity_core::utils::Base::Base16Lower).unwrap(); 
-          println!("issuer_iota_key: {:?}", issuer_iota_decoded); 
-
-          let issuer_bar: &str = "c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7";
-          let issuer_bar_decoded = BaseEncoding::decode(issuer_bar, identity_core::utils::Base::Base16Lower).unwrap(); 
-          println!("issuer_bar_key: {:?}", issuer_bar_decoded);
-
-          panic!();
-        }
+        .await
+        .is_ok());
     }
   }
 }
