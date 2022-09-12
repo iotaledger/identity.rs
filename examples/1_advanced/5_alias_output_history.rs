a810bdf268b4902affe8d698db1474ffac779747
Henrique Nogara <henrique.nogara@iota.org>
2022-09-12 14:33:33 -0300

    Fix clippy warnings

--- examples/1_advanced/5_alias_output_history.rs
+++ examples/1_advanced/5_alias_output_history.rs
@@ -6,20 +6,20 @@ use std::str::FromStr;
 use examples::create_did;
 use examples::random_stronghold_path;
 use examples::NETWORK_ENDPOINT;
-use identity_core::common::Timestamp;
-use identity_core::convert::FromJson;
-use identity_core::json;
-use identity_did::did::DID;
-use identity_did::service::Service;
-use identity_did::verification::MethodRelationship;
-use identity_stardust::block::address::Address;
-use identity_stardust::block::output::RentStructure;
-use identity_stardust::StardustClientExt;
-use identity_stardust::StardustDID;
-use identity_stardust::StardustDocument;
-use identity_stardust::StardustIdentityClient;
-use identity_stardust::StardustIdentityClientExt;
-use identity_stardust::StardustService;
+use identity_iota::core::json;
+use identity_iota::core::FromJson;
+use identity_iota::core::Timestamp;
+use identity_iota::did::MethodRelationship;
+use identity_iota::did::Service;
+use identity_iota::did::DID;
+use identity_iota::iota::IotaService;
+use identity_iota::iota::block::address::Address;
+use identity_iota::iota::block::output::RentStructure;
+use identity_iota::iota::IotaClientExt;
+use identity_iota::iota::IotaDID;
+use identity_iota::iota::IotaDocument;
+use identity_iota::iota::IotaIdentityClient;
+use identity_iota::iota::IotaIdentityClientExt;
 use iota_client::api_types::responses::OutputMetadataResponse;
 use iota_client::block::input::Input;
 use iota_client::block::output::AliasId;
@@ -45,14 +45,14 @@ async fn main() -> anyhow::Result<()> {
   let mut secret_manager: SecretManager = SecretManager::Stronghold(
     StrongholdSecretManager::builder()
       .password("secure_password")
-      .try_build(random_stronghold_path())?,
+      .build(random_stronghold_path())?,
   );
 
   // Create a new DID in an Alias Output for us to modify.
-  let (_, did): (Address, StardustDID) = create_did(&client, &mut secret_manager).await?;
+  let (_, did): (Address, IotaDID) = create_did(&client, &mut secret_manager).await?;
 
   // Resolve the latest state of the document.
-  let mut document: StardustDocument = client.resolve_did(&did).await?;
+  let mut document: IotaDocument = client.resolve_did(&did).await?;
 
   // Attach a new method relationship to the existing method.
   document.attach_method_relationship(
@@ -67,7 +67,7 @@ async fn main() -> anyhow::Result<()> {
     json!({"id": document.id().to_url().join("#my-service-2")?, "type": "MyService", "serviceEndpoint": "https://iota.org/"}),
   ];
   for service in services {
-    let service: StardustService = Service::from_json_value(service)?;
+    let service: IotaService = Service::from_json_value(service)?;
     assert!(document.insert_service(service));
     document.metadata.updated = Some(Timestamp::now_utc());
 
@@ -110,7 +110,7 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn current_block(client: &Client, output_id: &OutputId) -> anyhow::Result<Block> {
-  let output_metadata: OutputMetadataResponse = client.get_output_metadata(&output_id).await?;
+  let output_metadata: OutputMetadataResponse = client.get_output_metadata(output_id).await?;
   let block_id: BlockId = BlockId::from_str(&output_metadata.block_id)?;
   let block: Block = client.get_block(&block_id).await?;
   Ok(block)
