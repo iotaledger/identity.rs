da182a7c172be86aec1a26fa57f3242b4beb0d40
Philipp Gackstatter <philipp.gackstatter@iota.org>
2022-09-15 14:20:43 +0200

    Add new examples as tests

--- bindings/wasm/examples/src/tests/2_resolve_did.ts
+++ bindings/wasm/examples/src/tests/2_resolve_did.ts
@@ -1,4 +1,4 @@
-import { resolveIdentity } from "../2_resolve_did";
+import { resolveIdentity } from "../0_basic/2_resolve_did";
 
 // Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
 describe("Test node examples", function () {
--- bindings/wasm/examples/src/tests/4_delete_did.ts
+++ bindings/wasm/examples/src/tests/4_delete_did.ts
@@ -1,4 +1,4 @@
-import { deactivateIdentity } from "../3_deactivate_did";
+import { deactivateIdentity } from "../0_basic/3_deactivate_did";
 
 // Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
 describe("Test node examples", function () {
