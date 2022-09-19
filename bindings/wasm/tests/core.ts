4fa43983980f5313207f35d5d6d0d1d1c686ed25
Philipp Gackstatter <philipp.gackstatter@iota.org>
2022-09-19 17:47:00 +0200

    Set `"arrayExpression.preferHanging": true`

--- bindings/wasm/tests/core.ts
+++ bindings/wasm/tests/core.ts
@@ -14,40 +14,8 @@ const {
 
 const VALID_DID_KEY = "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK";
 const VALID_DID_EXAMPLE = "did:example:123";
-const KEY_BYTES = new Uint8Array([
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-    24,
-    25,
-    26,
-    27,
-    28,
-    29,
-    30,
-    31,
-    32,
-]);
+const KEY_BYTES = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    25, 26, 27, 28, 29, 30, 31, 32]);
 
 describe("CoreDID", function() {
     describe("#parse", function() {
