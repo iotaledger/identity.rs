2e3fbff58fbd082261cb275d8ac6ceef98ab11ec
Philipp Gackstatter <philipp.gackstatter@iota.org>
2022-09-20 16:18:59 +0200

    Implement `index` query in revocation status

--- identity_credential/src/credential/revocation_bitmap_status.rs
+++ identity_credential/src/credential/revocation_bitmap_status.rs
@@ -19,14 +19,37 @@ use crate::error::Result;
 pub struct RevocationBitmapStatus(Status);
 
 impl RevocationBitmapStatus {
-  const INDEX_PROPERTY_NAME: &'static str = "revocationBitmapIndex";
+  const REVOCATION_BITMAP_INDEX_PROPERTY: &'static str = "revocationBitmapIndex";
   /// Type name of the revocation bitmap.
   pub const TYPE: &'static str = "RevocationBitmap2022";
 
   /// Creates a new `RevocationBitmapStatus`.
-  pub fn new<D: DID>(id: DIDUrl<D>, index: u32) -> Self {
+  ///
+  /// The query of the `id` url is overwritten where "index" is set to `index`.
+  ///
+  /// # Example
+  ///
+  /// ```
+  /// # use identity_credential::credential::RevocationBitmapStatus;
+  /// # use identity_did::did::DIDUrl;
+  /// # use identity_did::did::CoreDID;
+  /// let did_url: DIDUrl<CoreDID> = DIDUrl::parse("did:method:0xffff#revocation-1").unwrap();
+  /// let status: RevocationBitmapStatus = RevocationBitmapStatus::new(did_url, 5);
+  /// assert_eq!(
+  ///   status.id::<CoreDID>().unwrap().to_string(),
+  ///   "did:method:0xffff?index=5#revocation-1"
+  /// );
+  /// assert_eq!(status.index().unwrap(), 5);
+  /// ```
+  pub fn new<D: DID>(mut id: DIDUrl<D>, index: u32) -> Self {
+    id.set_query(Some(&format!("index={index}")))
+      .expect("the string should be non-empty and a valid URL query");
+
     let mut object = Object::new();
-    object.insert(Self::INDEX_PROPERTY_NAME.to_owned(), Value::String(index.to_string()));
+    object.insert(
+      Self::REVOCATION_BITMAP_INDEX_PROPERTY.to_owned(),
+      Value::String(index.to_string()),
+    );
     RevocationBitmapStatus(Status::new_with_properties(
       Url::from(id),
       Self::TYPE.to_owned(),
@@ -43,18 +66,18 @@ impl RevocationBitmapStatus {
 
   /// Returns the index of the credential in the issuer's revocation bitmap if it can be decoded.
   pub fn index(&self) -> Result<u32> {
-    if let Some(Value::String(index)) = self.0.properties.get(Self::INDEX_PROPERTY_NAME) {
+    if let Some(Value::String(index)) = self.0.properties.get(Self::REVOCATION_BITMAP_INDEX_PROPERTY) {
       u32::from_str(index).map_err(|err| {
         Error::InvalidStatus(format!(
           "expected {} to be an unsigned 32-bit integer: {}",
-          Self::INDEX_PROPERTY_NAME,
+          Self::REVOCATION_BITMAP_INDEX_PROPERTY,
           err
         ))
       })
     } else {
       Err(Error::InvalidStatus(format!(
         "expected {} to be an unsigned 32-bit integer expressed as a string",
-        Self::INDEX_PROPERTY_NAME
+        Self::REVOCATION_BITMAP_INDEX_PROPERTY
       )))
     }
   }
@@ -65,19 +88,57 @@ impl TryFrom<Status> for RevocationBitmapStatus {
 
   fn try_from(status: Status) -> Result<Self> {
     if status.type_ != Self::TYPE {
-      Err(Error::InvalidStatus(format!(
+      return Err(Error::InvalidStatus(format!(
         "expected type '{}', got '{}'",
         Self::TYPE,
         status.type_
-      )))
-    } else if !status.properties.contains_key(Self::INDEX_PROPERTY_NAME) {
-      Err(Error::InvalidStatus(format!(
-        "missing required property '{}'",
-        Self::INDEX_PROPERTY_NAME
-      )))
-    } else {
-      Ok(Self(status))
+      )));
     }
+
+    let revocation_bitmap_index: &Value =
+      if let Some(revocation_bitmap_index) = status.properties.get(Self::REVOCATION_BITMAP_INDEX_PROPERTY) {
+        revocation_bitmap_index
+      } else {
+        return Err(Error::InvalidStatus(format!(
+          "missing required property '{}'",
+          Self::REVOCATION_BITMAP_INDEX_PROPERTY
+        )));
+      };
+
+    let revocation_bitmap_index: u32 = if let Value::String(index) = revocation_bitmap_index {
+      index.parse::<u32>().map_err(|err| {
+        Error::InvalidStatus(format!(
+          "property '{}' cannot be converted to an unsigned, 32-bit integer: {err}",
+          Self::REVOCATION_BITMAP_INDEX_PROPERTY
+        ))
+      })?
+    } else {
+      return Err(Error::InvalidStatus(format!(
+        "property '{}' is not a string",
+        Self::REVOCATION_BITMAP_INDEX_PROPERTY
+      )));
+    };
+
+    // If the index query is present it must match the revocationBitmapIndex.
+    // It is allowed to not be present to maintain backwards-compatibility
+    // with an earlier version of the RevocationBitmap spec.
+    for pair in status.id.query_pairs() {
+      if pair.0 == "index" {
+        let index: u32 = pair.1.parse::<u32>().map_err(|err| {
+          Error::InvalidStatus(format!(
+            "value of index query cannot be converted to an unsigned, 32-bit integer: {err}"
+          ))
+        })?;
+
+        if index != revocation_bitmap_index {
+          return Err(Error::InvalidStatus(format!(
+            "value of index query `{index}` does not match revocationBitmapIndex `{revocation_bitmap_index}`"
+          )));
+        }
+      }
+    }
+
+    Ok(Self(status))
   }
 }
 
@@ -103,14 +164,14 @@ mod tests {
 
   #[test]
   fn test_embedded_status_invariants() {
-    let url: Url = Url::parse(format!("did:iota:{}#{}", TAG, SERVICE)).unwrap();
+    let url: Url = Url::parse(format!("did:iota:{}?index=0#{}", TAG, SERVICE)).unwrap();
     let did_url: DIDUrl<CoreDID> = DIDUrl::parse(url.clone().into_string()).unwrap();
     let revocation_list_index: u32 = 0;
     let embedded_revocation_status: RevocationBitmapStatus =
       RevocationBitmapStatus::new(did_url, revocation_list_index);
 
     let object: Object = Object::from([(
-      RevocationBitmapStatus::INDEX_PROPERTY_NAME.to_owned(),
+      RevocationBitmapStatus::REVOCATION_BITMAP_INDEX_PROPERTY.to_owned(),
       Value::String(revocation_list_index.to_string()),
     )]);
     let status: Status =
