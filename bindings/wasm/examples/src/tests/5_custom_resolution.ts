47e3ae528d9db1603d085c4e50a032e92809c9d0
Oliver E. Anderson <oliver.anderson@iota.org>
2022-09-21 17:26:50 +0200

    Update bindings/wasm/examples/src/tests/5_custom_resolution.ts
    Co-authored-by: Philipp Gackstatter <philipp.gackstatter@iota.org>

--- bindings/wasm/examples/src/tests/5_custom_resolution.ts
+++ bindings/wasm/examples/src/tests/5_custom_resolution.ts
@@ -2,7 +2,7 @@ import { customResolution } from "../1_advanced/5_custom_resolution";
 
 // Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
 describe("Test node examples", function() {
-    it("Key exchange", async () => {
+    it("Custom Resolution", async () => {
         await customResolution();
     });
 });
