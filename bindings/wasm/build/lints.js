bb624bb9e9bb31ed2d96e40d3e2f6404fd28bfe2
Philipp Gackstatter <philipp.gackstatter@iota.org>
2022-09-30 09:33:43 +0200

    Remove bigInt lint

--- bindings/wasm/build/lints.js
+++ bindings/wasm/build/lints.js
@@ -1,17 +1,3 @@
-/** Aborts the build process if disallowed occurrences are found in identity_wasm.js **/
-function lintBigInt(content) {
-    /*
-    if (content.includes("BigInt64Array") || content.includes("BigUint64Array")) {
-        throw (
-            "Build artifacts should not include BigInt64Array/BigUint64Array imports\n"
-            + "to ensure React Native/WebKit compatibility.\n"
-            + "Remove any u64 and i64 occurrence from the public Wasm interface.\n"
-            + "See: https://github.com/iotaledger/identity.rs/issues/362\n"
-        );
-    }
-    */
-}
-
 /**
  * Rejects any `<obj>.ptr = 0;` occurrence, excluding `this.ptr = 0;` in `free()` implementations.
  *
@@ -80,7 +66,6 @@ See:
 /** Runs all custom lints on the generated code. Exits the process immediately with code 1 if any fail. **/
 function lintAll(content) {
     try {
-        lintBigInt(content);
         lintPtrNullWithoutFree(content);
         lintImportEnv(content);
     } catch (err) {
