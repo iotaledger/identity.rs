08c7d24c19c831021b6844ff3e7554f24321be2f
Philipp Gackstatter <philipp.gackstatter@iota.org>
2022-10-07 10:36:06 +0200

    Clean up unused mods

--- identity_storage/src/signing_algorithm.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2020-2022 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-#[non_exhaustive]
-pub enum SigningAlgorithm {
-  Ed25519,
-  Other(String),
-}
