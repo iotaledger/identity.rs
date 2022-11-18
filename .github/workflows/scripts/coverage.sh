00a5562ede811b3934f9ba54a1d0179eb8889b43
Eike Haß <eike-hass@web.de>
2022-11-18 14:04:24 +0100

    remove nightly from coverage

--- .github/workflows/scripts/coverage.sh
+++ .github/workflows/scripts/coverage.sh
@@ -5,11 +5,9 @@ set -e
 rm -rf coverage
 mkdir coverage
 
-NIGHTLY="+nightly-2022-06-30"
-
 # Run tests with profiling instrumentation
 echo "Running instrumented unit tests..."
-RUSTFLAGS="-Zinstrument-coverage" LLVM_PROFILE_FILE="identity-%m.profraw" cargo $NIGHTLY test --tests --all --all-features
+RUSTFLAGS="-Zinstrument-coverage" LLVM_PROFILE_FILE="identity-%m.profraw" cargo test --tests --all --all-features
 
 # Merge all .profraw files into "identity.profdata"
 echo "Merging coverage data..."
@@ -20,7 +18,7 @@ do
   PROFRAW="${PROFRAW} $file"
 done
 
-cargo $NIGHTLY profdata -- merge ${PROFRAW} -o identity.profdata
+cargo profdata -- merge ${PROFRAW} -o identity.profdata
 
 # List the test binaries
 echo "Locating test binaries..."
@@ -29,7 +27,7 @@ BINARIES=""
 for file in \
   $( \
     RUSTFLAGS="-Zinstrument-coverage" \
-      cargo $NIGHTLY test --tests --all --all-features --no-run --message-format=json \
+      cargo test --tests --all --all-features --no-run --message-format=json \
         | jq -r "select(.profile.test == true) | .filenames[]" \
         | grep -v dSYM - \
   ); \
@@ -40,7 +38,7 @@ done
 
 # Generate and export the coverage report to lcov format
 echo "Generating lcov file..."
-cargo $NIGHTLY cov -- export ${BINARIES} \
+cargo cov -- export ${BINARIES} \
   --instr-profile=identity.profdata \
   --ignore-filename-regex="/.cargo|rustc|target|tests|/.rustup" \
   --format=lcov --Xdemangler=rustfilt \
