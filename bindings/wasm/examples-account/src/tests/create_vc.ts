46afccf8e5def1598fdd564ca69ea627c7f4fbd0
Eike Haß <eike-hass@web.de>
2022-11-18 14:22:57 +0100

    remove legacy account examples

--- bindings/wasm/examples-account/src/tests/create_vc.ts
+++
@@ -1,8 +0,0 @@
-import { createVC } from "../create_vc";
-
-// Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
-describe("Test node examples", function() {
-    it("Create verifiable credential", async () => {
-        await createVC();
-    });
-});
--- bindings/wasm/examples-account/src/tests/create_vp.ts
+++
@@ -1,8 +0,0 @@
-import { createVP } from "../create_vp";
-
-// Only verifies that no uncaught exceptions are thrown, including syntax errors etc.
-describe("Test node examples", function() {
-    it("Create verifiable presentation", async () => {
-        await createVP();
-    });
-});
