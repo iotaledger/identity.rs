65a754127d17315c9f941925c29322a250a8334e
Philipp Gackstatter <philipp.gackstatter@iota.org>
2022-12-16 14:54:49 +0100

    Rename `Codec` to `Multicodec`

--- identity_data_integrity/src/verification_material/multikey.rs
+++ identity_data_integrity/src/verification_material/multikey.rs
@@ -10,9 +10,9 @@ use crate::error::Result;
 // The internal representation is a u64 because of: https://github.com/multiformats/unsigned-varint#practical-maximum-of-9-bytes-for-security.
 #[derive(Debug, PartialEq)]
 #[repr(transparent)]
-pub struct Codec(u64);
+pub struct Multicodec(u64);
 
-impl Codec {
+impl Multicodec {
   /// The `ed25519-pub` multicodec.
   pub const ED25519_PUB: Self = Self::new(0xed);
 
@@ -77,11 +77,16 @@ impl Codec {
 pub struct Multikey(String);
 
 impl Multikey {
-  pub fn new(codec: Codec, key: &[u8]) -> Self {
+  /// Creates a new Multikey of the given multicodec type and raw key material.
+  ///
+  /// This will use the Base58Btc multibase encoding. Use `MultiKey::new_with_base` to
+  /// customize the multibase encoding.
+  pub fn new(codec: Multicodec, key: &[u8]) -> Self {
     Multikey::new_with_base(Base::Base58Btc, codec, key)
   }
 
-  pub fn new_with_base(base: Base, codec: Codec, key: &[u8]) -> Self {
+  /// Creates a new Multikey of the given multibase encoding, multicodec type and raw key material.
+  pub fn new_with_base(base: Base, codec: Multicodec, key: &[u8]) -> Self {
     let mut input: Vec<u8> = codec.encode();
     input.extend_from_slice(key);
     let key: String = identity_core::utils::BaseEncoding::encode_multibase(&input, Some(base));
@@ -89,11 +94,11 @@ impl Multikey {
   }
 
   /// Decodes the multikey into its codec and data. The data should be checked for validity.
-  pub fn decode(&self) -> crate::error::Result<(Codec, Vec<u8>)> {
+  pub fn decode(&self) -> crate::error::Result<(Multicodec, Vec<u8>)> {
     let mut decoded: Vec<u8> = identity_core::utils::BaseEncoding::decode_multibase(&self.0)
       .map_err(|err| Error::MultikeyDecode("multibase decoding", Some(Box::new(err))))?;
 
-    let (codec, tail) = Codec::decode(decoded.as_slice())?;
+    let (codec, tail) = Multicodec::decode(decoded.as_slice())?;
 
     // Remove the bytes representing the codec from the vector.
     // SAFETY: This is fine because tail.len() is always < decoded.len().
@@ -107,14 +112,14 @@ impl Multikey {
 mod tests {
   use std::error::Error;
 
-  use super::Codec;
+  use super::Multicodec;
   use super::Multikey;
 
   #[test]
   fn test_multicodec_encode_decode() {
     for number in [0, 1, 127, 256, 300, 1024, 10_000, 64_000, 66_000] {
-      let encoded = Codec::new(number).encode();
-      assert!(Codec::decode(encoded.as_slice()).is_ok());
+      let encoded = Multicodec::new(number).encode();
+      assert!(Multicodec::decode(encoded.as_slice()).is_ok());
     }
   }
 
@@ -122,7 +127,7 @@ mod tests {
   fn test_multicodec_decode_error_cases() {
     // Two bytes indicating continuation, i.e. input is too short.
     let invalid_encoding: [u8; 2] = [0b1000_0000; 2];
-    let result = Codec::decode(&invalid_encoding);
+    let result = Multicodec::decode(&invalid_encoding);
     let error = result.unwrap_err();
     let source = error.source().unwrap();
     assert_eq!(
@@ -132,7 +137,7 @@ mod tests {
 
     // Input is too long and doesn't fit in u64.
     let too_long: [u8; 11] = [0xff; 11];
-    let result = Codec::decode(&too_long);
+    let result = Multicodec::decode(&too_long);
     let error = result.unwrap_err();
     let source = error.source().unwrap();
     assert_eq!(
@@ -144,12 +149,12 @@ mod tests {
   #[test]
   fn test_multikey_encoded_decode() {
     let key: [u8; 32] = [0x25; 32];
-    let codec: Codec = Codec::ED25519_PUB;
+    let codec: Multicodec = Multicodec::ED25519_PUB;
     let multikey = Multikey::new(codec, &key);
 
     let (codec, decoded_key) = multikey.decode().unwrap();
 
-    assert_eq!(codec, Codec::ED25519_PUB);
+    assert_eq!(codec, Multicodec::ED25519_PUB);
     assert_eq!(key.to_vec(), decoded_key);
   }
 
@@ -157,7 +162,7 @@ mod tests {
   fn test_multikey_decode_len_0_key() {
     let key: Vec<u8> = vec![];
     let codec_code = 0xec;
-    let multikey = Multikey::new(Codec::new(codec_code), &key);
+    let multikey = Multikey::new(Multicodec::new(codec_code), &key);
 
     let (codec, decoded_key) = multikey.decode().unwrap();
 
