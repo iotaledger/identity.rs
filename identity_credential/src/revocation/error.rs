552d97ae2b8a3c731f9b0adb70c647e67dadffd7
Oliver Anderson <oliver.anderson@iota.org>
2023-01-17 20:28:22 +0100

    error docs

--- identity_credential/src/revocation/error.rs
+++ identity_credential/src/revocation/error.rs
@@ -8,14 +8,20 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
   #[error("revocation bitmap could not be deserialized or decompressed")]
+  /// Indicates that the bitmap could not be reconstructed.
   BitmapDecodingError(#[source] std::io::Error),
   #[error("revocation bitmap could not be serialized or compressed")]
+  /// Indicates that the bitmap could not be encoded.
   BitmapEncodingError(#[source] std::io::Error),
+  /// This variant is typically used to indicate invalid conversions between `Services`, `ServiceEndpoints` and
+  /// `RevocationBitmaps`.
   #[error("{0}")]
   InvalidService(&'static str),
+  /// Indicates a failure to decode a bitmap from a base64 string representation.
   #[error("unable to decode base64 string: `{0}`")]
   Base64DecodingError(String, #[source] identity_core::error::Error),
   #[error("could not parse url")]
   #[non_exhaustive]
+  /// Indicates a failure to construct a URL when attempting to construct a `ServiceEndpoint`.
   UrlConstructionError(#[source] Box<dyn std::error::Error + Send + Sync + 'static>),
 }
