a3af6a7e03326410950a694fc1a44e246717b3e4
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-02-08 16:46:11 +0100

    Add license headers

--- identity_storage/src/key_storage/mod.rs
+++ identity_storage/src/key_storage/mod.rs
@@ -1,3 +1,6 @@
+// Copyright 2020-2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 mod key_gen;
 mod key_id;
 mod key_storage_error;
--- identity_storage/src/lib.rs
+++ identity_storage/src/lib.rs
@@ -1,1 +1,4 @@
+// Copyright 2020-2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 pub mod key_storage;
