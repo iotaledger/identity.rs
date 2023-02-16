a16c048dfbf762e5cd26559bc04c91f6ac859b54
Eike Haß <eike-hass@web.de>
2023-02-16 17:28:55 +0100

    organize TS Enums

--- bindings/wasm/lib/index.ts
+++ bindings/wasm/lib/index.ts
@@ -1,11 +1,8 @@
 // Copyright 2021-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-export enum JwsAlgorithm {
-    EdDSA = "EdDSA",
-    None = "none",
-}
-
 export * from "./iota_identity_client";
+
+export * from "./enums";
 
 export * from "~identity_wasm";
