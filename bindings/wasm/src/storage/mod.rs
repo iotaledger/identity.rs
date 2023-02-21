88a5193d64a6c896f2a609b7ac25b20f99c0b4a9
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-02-21 11:39:51 +0100

    Remove test function

--- bindings/wasm/src/storage/mod.rs
+++ bindings/wasm/src/storage/mod.rs
@@ -2,27 +2,3 @@ mod jwk_gen_output;
 mod jwk_storage;
 
 pub use jwk_storage::*;
-
-pub mod athing {
-  use super::*;
-  use identity_iota::storage::key_storage::JwkGenOutput;
-  use identity_iota::storage::key_storage::JwkStorage;
-  use identity_iota::storage::key_storage::KeyType;
-  use identity_jose::jws::JwsAlgorithm;
-  use js_sys::Promise;
-  use wasm_bindgen::prelude::*;
-  use wasm_bindgen_futures::future_to_promise;
-
-  #[wasm_bindgen]
-  pub fn call_storage(storage: WasmJwkStorage) -> Promise {
-    future_to_promise(async move {
-      let JwkGenOutput { key_id, .. } = JwkStorage::generate(&storage, KeyType::new("Ed25519"), JwsAlgorithm::EdDSA)
-        .await
-        .unwrap();
-
-      let sig = JwkStorage::sign(&storage, &key_id, b"test".to_vec()).await.unwrap();
-
-      Ok(JsValue::from(format!("{sig:?}")))
-    })
-  }
-}
