af55a0964dfaf4c60f969f0f9a1a6cd7ef577d4d
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-02-21 08:45:02 +0100

    Add jose index file

--- bindings/wasm/lib/index.ts
+++ bindings/wasm/lib/index.ts
@@ -2,11 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from "./iota_identity_client";
-export * from "./jose/ec_curve";
-export * from "./jose/ed_curve";
-export * from "./jose/jwk_operation";
-export * from "./jose/jwk_type";
-export * from "./jose/jwk_use";
-export * from "./jose/jws_algorithm";
+export * from "./jose/index";
 
 export * from "~identity_wasm";
