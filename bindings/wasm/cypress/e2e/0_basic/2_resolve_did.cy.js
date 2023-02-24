073608a15980b93ce19be273c1aa3e98050c0c33
Eike Haß <eike-hass@web.de>
2023-02-24 17:46:23 +0100

    fix formatting

--- bindings/wasm/cypress/e2e/0_basic/2_resolve_did.cy.js
+++ bindings/wasm/cypress/e2e/0_basic/2_resolve_did.cy.js
@@ -1,11 +1,11 @@
-import { setup } from '../../support/setup';
 import { resolveIdentity } from "../../../examples/dist/web/0_basic/2_resolve_did";
+import { setup } from "../../support/setup";
 
 describe(
-  "resolveIdentity",
-  () => {
-    it("Resolve Identity", async () => {
-      await setup(resolveIdentity)
-    });
-  }
+    "resolveIdentity",
+    () => {
+        it("Resolve Identity", async () => {
+            await setup(resolveIdentity);
+        });
+    },
 );
--- bindings/wasm/cypress/e2e/0_basic/5_create_vc.cy.js
+++ bindings/wasm/cypress/e2e/0_basic/5_create_vc.cy.js
@@ -1,11 +1,11 @@
-import { setup } from '../../support/setup';
 import { createVC } from "../../../examples/dist/web/0_basic/5_create_vc";
+import { setup } from "../../support/setup";
 
 describe(
-  "createVC",
-  () => {
-    it("Create Credential", async () => {
-      await setup(createVC)
-    });
-  }
+    "createVC",
+    () => {
+        it("Create Credential", async () => {
+            await setup(createVC);
+        });
+    },
 );
