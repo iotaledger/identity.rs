dd1946b7e875039cd81ff575640bce17a09ef5a1
Oliver Anderson <oliver.anderson@iota.org>
2023-02-28 14:27:40 +0100

    Fixed module level documentation

--- identity_jose/src/jws/mod.rs
+++ identity_jose/src/jws/mod.rs
@@ -1,6 +1,11 @@
 // Copyright 2020-2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
-
+//! JSON Web Signatures ([JWS](https://tools.ietf.org/html/rfc7515))
+//!
+//! The [`Encoder`] and [`Decoder`] APIs are strongly informed by the requirements of the higher level functionality
+//! offered by the IOTA Identity library. Hence these APIs may possibly not be immediately recognizable from a standard
+//! JWT/JWS perspective. See `identity_jose/examples/jws_encoding_decoding.rs` for a complete example of how to encode
+//! and then decode a JWS.
 mod algorithm;
 mod charset;
 mod decoder;
