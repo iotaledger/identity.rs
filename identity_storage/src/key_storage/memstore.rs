90beb40d6d5e67b762f387e8908fc9ec8288be61
Abdulrahim Al Methiab <abdulrahim.almethiab@iota.org>
2023-03-13 10:05:07 +0100

    fix clippy issue

--- identity_storage/src/key_storage/memstore.rs
+++ identity_storage/src/key_storage/memstore.rs
@@ -318,7 +318,7 @@ fn check_key_alg_compatibility(key_type: MemStoreKeyType, alg: JwsAlgorithm) -> 
   match (key_type, alg) {
     (MemStoreKeyType::Ed25519, JwsAlgorithm::EdDSA) => Ok(()),
     (key_type, alg) => {
-      return Err(
+      Err(
         KeyStorageError::new(crate::key_storage::KeyStorageErrorKind::KeyAlgorithmMismatch)
           .with_custom_message(format!("`cannot use key type `{key_type}` with algorithm `{alg}`")),
       )
