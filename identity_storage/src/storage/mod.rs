f4be229d440b841248193436b52479420ef6b1aa
Oliver Anderson <oliver.anderson@iota.org>
2023-03-14 13:39:51 +0100

    Added minimal storage struct

--- identity_storage/src/storage/mod.rs
+++ identity_storage/src/storage/mod.rs
@@ -2,3 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0
 mod jwk_storage_document_ext;
 mod error;
+
+pub struct Storage<K,I>{
+    key_storage: K,
+    key_id_storage: I
+}
+
+impl<K,I> Storage<K,I> {
+    pub fn new(key_storage: K, key_id_storage: I) -> Self {
+        Self{
+            key_storage,
+            key_id_storage
+        }
+    }
+
+    pub fn key_storage(&self) -> &K {
+        &self.key_storage
+    }
+
+    pub fn key_id_storage(&self) -> I {
+        &self.key_id_storage
+    }
+}
