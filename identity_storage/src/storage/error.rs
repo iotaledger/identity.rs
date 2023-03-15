f0af7e504a7e642aa0c1e871983395f7978ac263
Oliver Anderson <oliver.anderson@iota.org>
2023-03-15 09:17:54 +0100

    Added error variant

--- identity_storage/src/storage/error.rs
+++ identity_storage/src/storage/error.rs
@@ -1,11 +1,19 @@
 // Copyright 2020-2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-/// Errors that can occur when working with the [`JwkStorageDocumentExt`](crate::storage::JwkStorageDocumentExt) API. 
+/// Errors that can occur when working with the [`JwkStorageDocumentExt`](crate::storage::JwkStorageDocumentExt) API.
+#[derive(Debug, thiserror::Error)]
 pub enum JwkStorageDocumentError {
-    KeyStorageError(KeyStorageError), 
+    #[error("storage operation failed: key storage error")]
+    KeyStorageError(KeyStorageError),
+    #[error("storage operation failed: key id storage error")]
     KeyIdStorageError(KeyIdStorageError),
-    FragmentExists, 
+    #[error("could not add method. The fragment already exists")]
+    FragmentAlreadyExists,
+    #[error("method not found")]
     MethodNotFound,
-    SigningError(Box<dyn std::error::Error + Send + Sync + 'static>),
+    #[error("could not produce jwt: encoding error")]
+    EncodingError(#[source] Box<dyn std::error::Error + Send + Sync + 'static>),
+    #[error("storage operation failed after altering state. Unable to undo operation: {message}")]
+    UndoOperationFailed {message: String, source: Box<dyn std::error::Error + Send + Sync + 'static> },
 }
