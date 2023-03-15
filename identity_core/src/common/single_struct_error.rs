65beeda89b51405cb85322fd50406fd967490b02
Oliver Anderson <oliver.anderson@iota.org>
2023-03-15 18:13:20 +0100

    remove recovery data

--- identity_core/src/common/single_struct_error.rs
+++ identity_core/src/common/single_struct_error.rs
@@ -16,11 +16,11 @@ use std::fmt::Display;
 /// When used in a specialized context it is recommended to use a type alias (i.e. `type MyError =
 /// SingleStructError<MyErrorKind>` or `type MyError = SingleStructError<MyErrorKind, MyRecoveryData>`).
 #[derive(Debug)]
-pub struct SingleStructError<T: Debug + Display, S: Debug = ()> {
-  repr: Repr<T, S>,
+pub struct SingleStructError<T: Debug + Display> {
+  repr: Repr<T>,
 }
 
-impl<T: Display + Debug, S: Debug> Display for SingleStructError<T, S> {
+impl<T: Display + Debug> Display for SingleStructError<T> {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self.repr {
       Repr::Simple(ref kind) => write!(f, "{}", kind),
@@ -33,7 +33,7 @@ impl<T: Display + Debug, S: Debug> Display for SingleStructError<T, S> {
   }
 }
 
-impl<T: Debug + Display, S: Debug> Error for SingleStructError<T, S> {
+impl<T: Debug + Display> Error for SingleStructError<T> {
   fn source(&self) -> Option<&(dyn Error + 'static)> {
     self.extensive().and_then(|err| {
       err
@@ -45,34 +45,33 @@ impl<T: Debug + Display, S: Debug> Error for SingleStructError<T, S> {
 }
 
 #[derive(Debug)]
-struct Extensive<T: Debug + Display, S: Debug> {
+struct Extensive<T: Debug + Display> {
   kind: T,
   source: Option<Box<dyn Error + Send + Sync + 'static>>,
   message: Option<Cow<'static, str>>,
-  recovery_data: Option<S>,
 }
 
 #[derive(Debug)]
-enum Repr<T: Debug + Display, S: Debug> {
+enum Repr<T: Debug + Display> {
   Simple(T),
-  Extensive(Box<Extensive<T, S>>),
+  Extensive(Box<Extensive<T>>),
 }
 
-impl<T: Debug + Display, S: Debug> From<T> for SingleStructError<T, S> {
+impl<T: Debug + Display> From<T> for SingleStructError<T> {
   fn from(kind: T) -> Self {
     Self::new(kind)
   }
 }
 
-impl<T: Debug + Display, S: Debug> From<Box<Extensive<T, S>>> for SingleStructError<T, S> {
-  fn from(extensive: Box<Extensive<T, S>>) -> Self {
+impl<T: Debug + Display> From<Box<Extensive<T>>> for SingleStructError<T> {
+  fn from(extensive: Box<Extensive<T>>) -> Self {
     Self {
       repr: Repr::Extensive(extensive),
     }
   }
 }
 
-impl<T: Debug + Display, S: Debug> SingleStructError<T, S> {
+impl<T: Debug + Display> SingleStructError<T> {
   /// Constructs a new [`SingleStructError`].  
   pub fn new(kind: T) -> Self {
     Self {
@@ -115,31 +114,20 @@ impl<T: Debug + Display, S: Debug> SingleStructError<T, S> {
     self.into_extensive().source
   }
 
-  /// Returns a reference to the attached recovery data of the [`SingleStructError`] if it was set.
-  pub fn recovery_data(&self) -> Option<&S> {
-    self.extensive().and_then(|extensive| extensive.recovery_data.as_ref())
-  }
-
-  /// Converts this error into the recovery data if it was set.
-  pub fn into_recovery_data(self) -> Option<S> {
-    self.into_extensive().recovery_data
-  }
-
-  fn extensive(&self) -> Option<&Extensive<T, S>> {
+  fn extensive(&self) -> Option<&Extensive<T>> {
     match self.repr {
       Repr::Extensive(ref extensive) => Some(extensive.as_ref()),
       _ => None,
     }
   }
 
-  fn into_extensive(self) -> Box<Extensive<T, S>> {
+  fn into_extensive(self) -> Box<Extensive<T>> {
     match self.repr {
       Repr::Extensive(extensive) => extensive,
       Repr::Simple(kind) => Box::new(Extensive {
         kind,
         source: None,
         message: None,
-        recovery_data: None,
       }),
     }
   }
@@ -163,17 +151,6 @@ impl<T: Debug + Display, S: Debug> SingleStructError<T, S> {
   fn _with_custom_message(self, message: Cow<'static, str>) -> Self {
     let mut extensive = self.into_extensive();
     extensive.as_mut().message = Some(message);
-    Self::from(extensive)
-  }
-
-  /// Add recovery data to the [`SingleStructError`].
-  pub fn with_recovery_data(self, data: S) -> Self {
-    self._with_recovery_data(data)
-  }
-
-  fn _with_recovery_data(self, data: S) -> Self {
-    let mut extensive = self.into_extensive();
-    extensive.as_mut().recovery_data = Some(data);
     Self::from(extensive)
   }
 }
