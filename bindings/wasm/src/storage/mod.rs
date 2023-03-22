05f519b886bcb78a97c7ea7d14895a6e14d4aad1
Oliver Anderson <oliver.anderson@iota.org>
2023-03-22 17:34:30 +0100

    started bindings for Storage

--- bindings/wasm/src/storage/mod.rs
+++ bindings/wasm/src/storage/mod.rs
@@ -3,6 +3,8 @@
 
 mod jwk_gen_output;
 mod jwk_storage;
+mod storage;
 
 pub use jwk_gen_output::*;
 pub use jwk_storage::*;
+pub use storage::*;
---
+++ bindings/wasm/src/storage/storage.rs
@@ -0,0 +1,9 @@
+// Copyright 2020-2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use identity_iota::storage::storage::Storage;
+
+use super::WasmJwkStorage;
+
+// #[wasm_bindgen(js_name = Storage)]
+// pub struct WasmStorage(Storage<WasmJwkStorage, WasmKeyIdStorage>)
