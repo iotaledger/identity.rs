13098e871c74db61842bf2c511c63487834ea902
Oliver Anderson <oliver.anderson@iota.org>
2023-03-28 19:37:17 +0200

    Document Storage

--- identity_storage/src/storage/mod.rs
+++ identity_storage/src/storage/mod.rs
@@ -10,12 +10,16 @@ pub use error::*;
 pub use jwk_storage_document_ext::*;
 pub use signature_options::*;
 
+/// A type wrapping a [`JwkStorage`](crate::key_storage::JwkStorage) and
+/// [`KeyIdStorage`](crate::key_id_storage::KeyIdStorage) that should always be used together when calling methods from
+/// [`JwkStorageDocumentExt`](crate::storage::JwkStorageDocumentExt).
 pub struct Storage<K, I> {
   key_storage: K,
   key_id_storage: I,
 }
 
 impl<K, I> Storage<K, I> {
+  /// Constructs a new [`Storage`].
   pub fn new(key_storage: K, key_id_storage: I) -> Self {
     Self {
       key_storage,
@@ -23,10 +27,12 @@ impl<K, I> Storage<K, I> {
     }
   }
 
+  /// Obtain a reference to the wrapped [`JwkStorage`](crate::key_storage::JwkStorage).
   pub fn key_storage(&self) -> &K {
     &self.key_storage
   }
 
+  /// Obtain a reference to the wrapped [`KeyIdStorage`](crate::key_id_storage::KeyIdStorage).
   pub fn key_id_storage(&self) -> &I {
     &self.key_id_storage
   }
