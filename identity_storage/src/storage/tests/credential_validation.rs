9eb5599fe806e496b4fd4ef2950a128c8f383b59
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-05-10 16:10:37 +0200

    Add more credential validator tests

--- identity_storage/src/storage/tests/credential_validation.rs
+++ identity_storage/src/storage/tests/credential_validation.rs
@@ -4,15 +4,42 @@
 use identity_core::common::Duration;
 use identity_core::common::Object;
 use identity_core::common::Timestamp;
+use identity_core::convert::FromJson;
+use identity_credential::credential::Credential;
 use identity_credential::validator::vc_jwt_validation::CredentialValidationOptions;
 use identity_credential::validator::vc_jwt_validation::CredentialValidator;
 use identity_credential::validator::vc_jwt_validation::ValidationError;
 use identity_credential::validator::FailFast;
+use once_cell::sync::Lazy;
+use proptest::proptest;
 
 use crate::storage::tests::test_utils::Setup;
 use crate::storage::tests::test_utils::{self};
 use crate::storage::JwkStorageDocumentExt;
 use crate::storage::JwsSignatureOptions;
+
+const SIMPLE_CREDENTIAL_JSON: &str = r#"{
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://www.w3.org/2018/credentials/examples/v1"
+  ],
+  "id": "http://example.edu/credentials/3732",
+  "type": ["VerifiableCredential", "UniversityDegreeCredential"],
+  "issuer": "https://example.edu/issuers/14",
+  "issuanceDate": "2010-01-01T19:23:24Z",
+  "expirationDate": "2020-01-01T19:23:24Z",
+  "credentialSubject": {
+    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+    "degree": {
+      "type": "BachelorDegree",
+      "name": "Bachelor of Science in Mechanical Engineering"
+    }
+  }
+}"#;
+
+// A simple credential shared by some of the tests in this module
+const SIMPLE_CREDENTIAL: Lazy<Credential> =
+  Lazy::new(|| Credential::<Object>::from_json(SIMPLE_CREDENTIAL_JSON).unwrap());
 
 #[tokio::test]
 async fn invalid_expiration_date() {
@@ -51,4 +78,36 @@ async fn invalid_expiration_date() {
   };
 
   assert!(matches!(error, &ValidationError::ExpirationDate));
+}
+
+#[test]
+fn issued_on_or_before() {
+  assert!(CredentialValidator::check_issued_on_or_before(
+    &SIMPLE_CREDENTIAL,
+    SIMPLE_CREDENTIAL
+      .issuance_date
+      .checked_sub(Duration::minutes(1))
+      .unwrap()
+  )
+  .is_err());
+  // and now with a later timestamp
+  assert!(CredentialValidator::check_issued_on_or_before(
+    &SIMPLE_CREDENTIAL,
+    SIMPLE_CREDENTIAL
+      .issuance_date
+      .checked_add(Duration::minutes(1))
+      .unwrap()
+  )
+  .is_ok());
+}
+
+proptest! {
+  #[test]
+  fn property_based_issued_before(seconds in 0 ..1_000_000_000_u32) {
+
+    let earlier_than_issuance_date = SIMPLE_CREDENTIAL.issuance_date.checked_sub(Duration::seconds(seconds)).unwrap();
+    let later_than_issuance_date = SIMPLE_CREDENTIAL.issuance_date.checked_add(Duration::seconds(seconds)).unwrap();
+    assert!(CredentialValidator::check_issued_on_or_before(&SIMPLE_CREDENTIAL, earlier_than_issuance_date).is_err());
+    assert!(CredentialValidator::check_issued_on_or_before(&SIMPLE_CREDENTIAL, later_than_issuance_date).is_ok());
+  }
 }
