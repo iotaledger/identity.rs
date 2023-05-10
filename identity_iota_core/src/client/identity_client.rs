ab826f6aa57242bc32830223ccefe41469500688
Abdulrahim Al Methiab <abdulrahim.almethiab@iota.org>
2023-05-10 11:55:19 +0200

    dprint fix

--- identity_iota_core/src/client/identity_client.rs
+++ identity_iota_core/src/client/identity_client.rs
@@ -154,7 +154,7 @@ pub trait IotaIdentityClientExt: IotaIdentityClient {
   /// # Errors
   ///
   /// - [`NetworkMismatch`](Error::NetworkMismatch) if the network of the DID and client differ.
-  /// - [`NotFound`](iota_client::Error::NotFound) if the associated Alias Output was not found.
+  /// - [`NotFound`](iota_sdk::client::Error::NotFound) if the associated Alias Output was not found.
   async fn resolve_did_output(&self, did: &IotaDID) -> Result<AliasOutput> {
     validate_network(self, did).await?;
 
