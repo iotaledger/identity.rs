8b0aed365af2f7cefd3c2dc8354e28c9da1a2491
Eike Haß <eike-hass@web.de>
2023-05-10 17:31:19 +0200

    fix wasm custom init

--- bindings/wasm/build/web.js
+++ bindings/wasm/build/web.js
@@ -17,19 +17,9 @@ let changedFile = entryFile
         /input = new URL\((.*), import\.meta\.url\);/i,
         "// input = new URL($1, import.meta.url);",
     )
-    // Rename original init function, because we want to use the name for our own function.
-    .replace(
-        "async function init(input) {",
-        "async function initWasm(input) {",
-    )
-    .replace(
-        "init.__wbindgen_wasm_module = module;",
-        "initWasm.__wbindgen_wasm_module = module;",
-    )
     // Create an init function which imports the wasm file.
-    .replace(
-        "export default init;",
-        "let __initializedIotaWasm = false\r\n\r\nexport function init(path) {\r\n    if (__initializedIotaWasm) {\r\n        return Promise.resolve(wasm)\r\n    }\r\n    return initWasm(path || 'identity_wasm_bg.wasm').then(() => {\r\n        __initializedIotaWasm = true\r\n        return wasm\r\n    })\r\n}\r\n",
+    .concat(
+        "let __initializedIotaWasm = false\r\n\r\nexport function init(path) {\r\n    if (__initializedIotaWasm) {\r\n        return Promise.resolve(wasm)\r\n    }\r\n    return __wbg_init(path || 'identity_wasm_bg.wasm').then(() => {\r\n        __initializedIotaWasm = true\r\n        return wasm\r\n    })\r\n}\r\n",
     );
 
 fs.writeFileSync(
@@ -40,6 +30,7 @@ fs.writeFileSync(
 const entryFilePathTs = path.join(RELEASE_FOLDER, "identity_wasm.d.ts");
 const entryFileTs = fs.readFileSync(entryFilePathTs).toString();
 // Replace the init function in the ts file.
+// TODO
 let changedFileTs = entryFileTs.replace(
     "/**\n* If `module_or_path` is {RequestInfo} or {URL}, makes a request and\n* for everything else, calls `WebAssembly.instantiate` directly.\n*\n* @param {InitInput | Promise<InitInput>} module_or_path\n*\n* @returns {Promise<InitOutput>}\n*/\nexport default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;",
     "\/**\r\n* Loads the Wasm file so the lib can be used, relative path to Wasm file\r\n* @param {string | undefined} path\r\n*\/\r\nexport function init (path?: string): Promise<void>;",
