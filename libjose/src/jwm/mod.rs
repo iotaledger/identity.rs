7a786b731e0f10e3d74d55e26081708678ae1570
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-05-22 15:59:35 +0200

    Remove `libjose` (#1169)

--- libjose/src/jwm/mod.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2021 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-//! JSON Web Message ([JWM](https://tools.ietf.org/html/draft-looker-jwm-01))
-
-mod attributes;
-
-pub use self::attributes::*;
