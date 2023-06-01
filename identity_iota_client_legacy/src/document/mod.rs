772302385a8173952962b78d17fad01b4a342109
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-06-01 15:33:54 +0200

    Remove legacy crates (#1177)

--- identity_iota_client_legacy/src/document/mod.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2022 IOTA Stiftung
-// SPDX-License-Identifier: Apache-2.0
-
-//! Resolved IOTA Document type.
-
-pub use self::resolved_iota_document::ResolvedIotaDocument;
-
-mod resolved_iota_document;
