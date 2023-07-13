1d95d2a3467404527f385df8fdf59f841694f9c9
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-07-13 14:57:40 +0200

    Move credential-related tests to credential crate

--- identity_storage/src/storage/tests/credential_validation.rs
+++ identity_storage/src/storage/tests/credential_validation.rs
@@ -5,88 +5,26 @@ use identity_core::common::Duration;
 use identity_core::common::Object;
 use identity_core::common::Timestamp;
 use identity_core::common::Url;
-use identity_core::convert::FromJson;
-use identity_credential::credential::Credential;
 use identity_credential::credential::Jwt;
 use identity_credential::credential::RevocationBitmapStatus;
 use identity_credential::credential::Status;
-use identity_credential::credential::Subject;
 use identity_credential::revocation::RevocationBitmap;
 use identity_credential::revocation::RevocationDocumentExt;
 use identity_credential::validator::CredentialValidationOptions;
 use identity_credential::validator::CredentialValidator;
 use identity_credential::validator::FailFast;
 use identity_credential::validator::StatusCheck;
-use identity_credential::validator::SubjectHolderRelationship;
 use identity_credential::validator::ValidationError;
 use identity_did::DID;
 use identity_document::document::CoreDocument;
 use identity_document::service::Service;
 use identity_document::verifiable::JwsVerificationOptions;
-use once_cell::sync::Lazy;
-use proptest::proptest;
 
 use crate::storage::tests::test_utils;
 use crate::storage::tests::test_utils::CredentialSetup;
 use crate::storage::tests::test_utils::Setup;
 use crate::storage::JwkDocumentExt;
 use crate::storage::JwsSignatureOptions;
-
-const SIMPLE_CREDENTIAL_JSON: &str = r#"{
-  "@context": [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://www.w3.org/2018/credentials/examples/v1"
-  ],
-  "id": "http://example.edu/credentials/3732",
-  "type": ["VerifiableCredential", "UniversityDegreeCredential"],
-  "issuer": "https://example.edu/issuers/14",
-  "issuanceDate": "2010-01-01T19:23:24Z",
-  "expirationDate": "2020-01-01T19:23:24Z",
-  "credentialSubject": {
-    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-    "degree": {
-      "type": "BachelorDegree",
-      "name": "Bachelor of Science in Mechanical Engineering"
-    }
-  }
-}"#;
-
-// A simple credential shared by some of the tests in this module
-static SIMPLE_CREDENTIAL: Lazy<Credential> =
-  Lazy::new(|| Credential::<Object>::from_json(SIMPLE_CREDENTIAL_JSON).unwrap());
-
-// TODO: Move to identity_credential?
-#[test]
-fn issued_on_or_before() {
-  assert!(CredentialValidator::check_issued_on_or_before(
-    &SIMPLE_CREDENTIAL,
-    SIMPLE_CREDENTIAL
-      .issuance_date
-      .checked_sub(Duration::minutes(1))
-      .unwrap()
-  )
-  .is_err());
-  // and now with a later timestamp
-  assert!(CredentialValidator::check_issued_on_or_before(
-    &SIMPLE_CREDENTIAL,
-    SIMPLE_CREDENTIAL
-      .issuance_date
-      .checked_add(Duration::minutes(1))
-      .unwrap()
-  )
-  .is_ok());
-}
-
-proptest! {
-  #[test]
-  fn property_based_issued_before(seconds in 0 ..1_000_000_000_u32) {
-
-    let earlier_than_issuance_date = SIMPLE_CREDENTIAL.issuance_date.checked_sub(Duration::seconds(seconds)).unwrap();
-    let later_than_issuance_date = SIMPLE_CREDENTIAL.issuance_date.checked_add(Duration::seconds(seconds)).unwrap();
-    assert!(CredentialValidator::check_issued_on_or_before(&SIMPLE_CREDENTIAL, earlier_than_issuance_date).is_err());
-    assert!(CredentialValidator::check_issued_on_or_before(&SIMPLE_CREDENTIAL, later_than_issuance_date).is_ok());
-  }
-}
 
 async fn invalid_expiration_or_issuance_date_impl<T>(setup: Setup<T, T>)
 where
@@ -345,121 +283,6 @@ async fn verify_invalid_signature() {
     fragment,
   )
   .await;
-}
-
-async fn check_subject_holder_relationship_impl<T>(setup: Setup<T, T>)
-where
-  T: JwkDocumentExt + AsRef<CoreDocument>,
-{
-  let Setup { issuer_doc, .. } = setup;
-
-  let mut credential: Credential = SIMPLE_CREDENTIAL.clone();
-
-  // first ensure that holder_url is the subject and set the nonTransferable property
-  let actual_holder_url = credential.credential_subject.first().unwrap().id.clone().unwrap();
-  assert_eq!(credential.credential_subject.len(), 1);
-  credential.non_transferable = Some(true);
-
-  // checking with holder = subject passes for all defined subject holder relationships:
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential,
-    &actual_holder_url,
-    SubjectHolderRelationship::AlwaysSubject
-  )
-  .is_ok());
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential,
-    &actual_holder_url,
-    SubjectHolderRelationship::SubjectOnNonTransferable
-  )
-  .is_ok());
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential,
-    &actual_holder_url,
-    SubjectHolderRelationship::Any
-  )
-  .is_ok());
-
-  // check with a holder different from the subject of the credential:
-  let issuer_url = Url::parse(issuer_doc.as_ref().id().as_str()).unwrap();
-  assert!(actual_holder_url != issuer_url);
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential,
-    &issuer_url,
-    SubjectHolderRelationship::AlwaysSubject
-  )
-  .is_err());
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential,
-    &issuer_url,
-    SubjectHolderRelationship::SubjectOnNonTransferable
-  )
-  .is_err());
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential,
-    &issuer_url,
-    SubjectHolderRelationship::Any
-  )
-  .is_ok());
-
-  let mut credential_transferable = credential.clone();
-
-  credential_transferable.non_transferable = Some(false);
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential_transferable,
-    &issuer_url,
-    SubjectHolderRelationship::SubjectOnNonTransferable
-  )
-  .is_ok());
-
-  credential_transferable.non_transferable = None;
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential_transferable,
-    &issuer_url,
-    SubjectHolderRelationship::SubjectOnNonTransferable
-  )
-  .is_ok());
-
-  // two subjects (even when they are both the holder) should fail for all defined values except "Any"
-
-  let mut credential_duplicated_holder = credential;
-  credential_duplicated_holder
-    .credential_subject
-    .push(Subject::with_id(actual_holder_url));
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential_duplicated_holder,
-    &issuer_url,
-    SubjectHolderRelationship::AlwaysSubject
-  )
-  .is_err());
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential_duplicated_holder,
-    &issuer_url,
-    SubjectHolderRelationship::SubjectOnNonTransferable
-  )
-  .is_err());
-
-  assert!(CredentialValidator::check_subject_holder_relationship(
-    &credential_duplicated_holder,
-    &issuer_url,
-    SubjectHolderRelationship::Any
-  )
-  .is_ok());
-}
-
-#[tokio::test]
-async fn check_subject_holder_relationship() {
-  check_subject_holder_relationship_impl(test_utils::setup_coredocument(None, None).await).await;
-  check_subject_holder_relationship_impl(test_utils::setup_iotadocument(None, None).await).await;
 }
 
 fn check_status_impl<T, F>(setup: Setup<T, T>, insert_service: F)
