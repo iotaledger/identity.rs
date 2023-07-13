75b8579902568930d7145e104970f38180326117
Philipp Gackstatter <philipp.gackstatter@iota.org>
2023-07-13 11:38:33 +0200

    Enable lints, add docs

--- identity_credential/src/domain_linkage/mod.rs
+++ identity_credential/src/domain_linkage/mod.rs
@@ -1,6 +1,8 @@
 // Copyright 2020-2023 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+//! Implementation of Domain Linkage according to <https://identity.foundation/.well-known/resources/did-configuration/>.
+
 mod domain_linkage_configuration;
 mod domain_linkage_credential_builder;
 mod domain_linkage_validator;
--- identity_credential/src/lib.rs
+++ identity_credential/src/lib.rs
@@ -6,22 +6,13 @@
 #![warn(
   rust_2018_idioms,
   unreachable_pub,
-  // missing_docs,
+  missing_docs,
   rustdoc::missing_crate_level_docs,
   rustdoc::broken_intra_doc_links,
   rustdoc::private_intra_doc_links,
   rustdoc::private_doc_tests,
-  clippy::missing_safety_doc,
-  // clippy::missing_errors_doc
+  clippy::missing_safety_doc
 )]
-
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate serde;
-
-pub use self::error::Error;
-pub use self::error::Result;
 
 #[cfg(feature = "credential")]
 pub mod credential;
@@ -32,6 +23,13 @@ pub mod error;
 pub mod presentation;
 #[cfg(feature = "revocation-bitmap")]
 pub mod revocation;
-
 #[cfg(feature = "validator")]
 pub mod validator;
+
+pub use error::Error;
+pub use error::Result;
+
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde;
