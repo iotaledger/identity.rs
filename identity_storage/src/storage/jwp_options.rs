21b9588d8cbd40d7e55d1219aa986817cf5e06ae
Alberto Solavagione <albertosolavagione30@gmail.com>
2023-11-29 16:09:35 +0100

    new JwpOptions

--- identity_storage/src/storage/jwp_options.rs
+++ identity_storage/src/storage/jwp_options.rs
@@ -10,33 +10,12 @@ use identity_core::common::Url;
 #[serde(rename_all = "camelCase")]
 #[serde(default)]
 pub struct JwpOptions {
-  /// Whether to attach the public key in the corresponding method
-  /// to the JWS header.
-  pub attach_jwk: bool,
-
-  /// Whether to Base64url encode the payload or not.
-  ///
-  /// [More Info](https://tools.ietf.org/html/rfc7797#section-3)
-  #[serde(skip_serializing_if = "Option::is_none")]
-  pub b64: Option<bool>,
 
   /// The Type value to be placed in the protected header.
   ///
   /// [More Info](https://tools.ietf.org/html/rfc7515#section-4.1.9)
   #[serde(skip_serializing_if = "Option::is_none")]
   pub typ: Option<String>,
-
-  /// Content Type to be placed in the protected header.
-  ///
-  /// [More Info](https://tools.ietf.org/html/rfc7515#section-4.1.10)
-  #[serde(skip_serializing_if = "Option::is_none")]
-  pub cty: Option<String>,
-
-  /// The URL to be placed in the protected header.
-  ///
-  /// [More Info](https://tools.ietf.org/html/rfc8555#section-6.4.1)
-  #[serde(skip_serializing_if = "Option::is_none")]
-  pub url: Option<Url>,
 
   /// The nonce to be placed in the protected header.
   ///
@@ -50,16 +29,7 @@ pub struct JwpOptions {
   ///
   /// [More Info](https://www.rfc-editor.org/rfc/rfc7515#section-4.1.4)
   #[serde(skip_serializing_if = "Option::is_none")]
-  pub kid: Option<String>,
-
-  /// Whether the payload should be detached from the JWS.
-  ///
-  /// [More Info](https://www.rfc-editor.org/rfc/rfc7515#appendix-F).
-  pub detached_payload: bool,
-
-  /// Additional header parameters.
-  #[serde(skip_serializing_if = "Option::is_none")]
-  pub custom_header_parameters: Option<Object>,
+  pub kid: Option<String>
 }
 
 impl JwpOptions {
@@ -68,33 +38,9 @@ impl JwpOptions {
     Self::default()
   }
 
-  /// Replace the value of the `attach_jwk` field.
-  pub fn attach_jwk_to_header(mut self, value: bool) -> Self {
-    self.attach_jwk = value;
-    self
-  }
-
-  /// Replace the value of the `b64` field.
-  pub fn b64(mut self, value: bool) -> Self {
-    self.b64 = Some(value);
-    self
-  }
-
   /// Replace the value of the `typ` field.
   pub fn typ(mut self, value: impl Into<String>) -> Self {
     self.typ = Some(value.into());
-    self
-  }
-
-  /// Replace the value of the `cty` field.
-  pub fn cty(mut self, value: impl Into<String>) -> Self {
-    self.cty = Some(value.into());
-    self
-  }
-
-  /// Replace the value of the `url` field.
-  pub fn url(mut self, value: Url) -> Self {
-    self.url = Some(value);
     self
   }
 
@@ -109,16 +55,5 @@ impl JwpOptions {
     self.kid = Some(value.into());
     self
   }
-
-  /// Replace the value of the `detached_payload` field.
-  pub fn detached_payload(mut self, value: bool) -> Self {
-    self.detached_payload = value;
-    self
-  }
-
-  /// Adds additional header parameters.
-  pub fn custom_header_parameters(mut self, value: Object) -> Self {
-    self.custom_header_parameters = Some(value);
-    self
-  }
+  
 }
