ecddea3dbc291f96738a8be45d43c0a8e41b4954
Abdulrahim Al Methiab <aalmethiab@gmail.com>
2024-01-17 13:16:46 +0100

    fix cypress test

--- bindings/wasm/cypress/e2e/1_advanced/6_sd_jwt.cy.js
+++ bindings/wasm/cypress/e2e/1_advanced/6_sd_jwt.cy.js
@@ -2,9 +2,9 @@ import { sdJwt } from "../../../examples/dist/web/1_advanced/6_sd_jwt";
 import { setup } from "../../support/setup";
 
 describe(
-    "domainLinkage",
+    "SdJwt",
     () => {
-        it("Domain Linkage", async () => {
+        it("SD Jwt", async () => {
             await setup(sdJwt);
         });
     },
