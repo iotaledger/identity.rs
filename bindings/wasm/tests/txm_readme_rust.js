29022629f647f9eb2bc07f4f0925050c08b91234
Eike Haß <eike-hass@web.de>
2024-01-25 14:20:23 +0100

    fmt

--- bindings/wasm/tests/txm_readme_rust.js
+++ bindings/wasm/tests/txm_readme_rust.js
@@ -1,22 +1,21 @@
-const assert = require('assert')
-const spawn = require('child_process').spawn
+const assert = require("assert");
+const spawn = require("child_process").spawn;
 
 describe("Test TXM", () => {
-
     before((done) => {
-        let process = spawn("txm", ["../../README.md"])
-        process.stdout.on('data', function(data) {
-            console.log(data.toString()); 
+        let process = spawn("txm", ["../../README.md"]);
+        process.stdout.on("data", function(data) {
+            console.log(data.toString());
         });
-        process.stderr.on('data', function(data) {
-            console.log(data.toString()); 
+        process.stderr.on("data", function(data) {
+            console.log(data.toString());
         });
-        process.on('exit', (code) => {
-            exitCode = code
-            done()
-        })
-    })
-    it('exit code should be zero', () => {
-        assert.equal(exitCode, 0)
-    })
-})
+        process.on("exit", (code) => {
+            exitCode = code;
+            done();
+        });
+    });
+    it("exit code should be zero", () => {
+        assert.equal(exitCode, 0);
+    });
+});
