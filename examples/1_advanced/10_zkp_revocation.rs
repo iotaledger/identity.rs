1992b2884a35a2a67e8ed553490621da389ce623
Alberto Solavagione <albertosolavagione30@gmail.com>
2024-02-01 16:56:12 +0100

    update 10_zkp_revocation example

--- examples/1_advanced/10_zkp_revocation.rs
+++ examples/1_advanced/10_zkp_revocation.rs
@@ -1,8 +1,16 @@
 use std::thread;
-use std::time::Duration;
 use examples::get_address_with_funds;
 use examples::random_stronghold_path;
 use examples::MemStorage;
+use identity_iota::core::Duration;
+use identity_iota::credential::RevocationBitmap;
+use identity_iota::credential::StatusCheck;
+use identity_iota::did::DIDUrl;
+use identity_iota::document::Service;
+use identity_iota::storage::TimeframeRevocationExtension;
+use iota_sdk::types::block::output::AliasOutputBuilder;
+use iota_sdk::types::block::output::RentStructure;
+use std::time::Duration as SleepDuration;
 use identity_iota::core::FromJson;
 use identity_iota::core::Object;
 use identity_iota::core::Url;
@@ -24,7 +32,6 @@ use identity_iota::credential::RevocationTimeframeStatus;
 use identity_iota::credential::SelectiveDisclosurePresentation;
 use identity_iota::credential::Status;
 use identity_iota::credential::Subject;
-use identity_iota::credential::ValidityTimeframeGranularity;
 use identity_iota::did::CoreDID;
 use identity_iota::did::DID;
 use identity_iota::iota::IotaClientExt;
@@ -58,34 +65,43 @@ const FAUCET_ENDPOINT: &str = "http://localhost:8091/api/enqueue";
 
 async fn create_did(client: &Client, secret_manager: &SecretManager, storage: &MemStorage, key_type: KeyType, alg: ProofAlgorithm ) -> anyhow::Result<(Address, IotaDocument, String)> {
 
-  // Get an address with funds for testing.
-  let address: Address = get_address_with_funds(&client, &secret_manager, FAUCET_ENDPOINT).await?;
+    // Get an address with funds for testing.
+    let address: Address = get_address_with_funds(&client, &secret_manager, FAUCET_ENDPOINT).await?;
 
-  // Get the Bech32 human-readable part (HRP) of the network.
-  let network_name: NetworkName = client.network_name().await?;
-  
-  // Create a new DID document with a placeholder DID.
-  // The DID will be derived from the Alias Id of the Alias Output after publishing.
-  let mut document: IotaDocument = IotaDocument::new(&network_name);
+    // Get the Bech32 human-readable part (HRP) of the network.
+    let network_name: NetworkName = client.network_name().await?;
+    
+    // Create a new DID document with a placeholder DID.
+    // The DID will be derived from the Alias Id of the Alias Output after publishing.
+    let mut document: IotaDocument = IotaDocument::new(&network_name);
 
-  // New Verification Method containing a BBS+ key
-  let fragment = document.generate_method_jwp(
-    &storage, 
-    key_type, 
-    alg, 
-    None, 
-    MethodScope::VerificationMethod
-  ).await?;
+    // New Verification Method containing a BBS+ key
+    let fragment = document.generate_method_jwp(
+        &storage, 
+        key_type, 
+        alg, 
+        None, 
+        MethodScope::VerificationMethod
+    ).await?;
 
-  // Construct an Alias Output containing the DID document, with the wallet address
-  // set as both the state controller and governor.
-  let alias_output: AliasOutput = client.new_did_output(address, document, None).await?;
+    // Create a new empty revocation bitmap. No credential is revoked yet.
+    let revocation_bitmap: RevocationBitmap = RevocationBitmap::new();
 
-  // Publish the Alias Output and get the published DID document.
-  let document: IotaDocument = client.publish_did_output(&secret_manager, alias_output).await?;
-  println!("Published DID document: {document:#}");
+    // Add the revocation bitmap to the DID document of the issuer as a service.
+    let service_id: DIDUrl = document.id().to_url().join("#my-revocation-service")?;
+    let service: Service = revocation_bitmap.to_service(service_id)?;
 
-  Ok((address, document, fragment))
+    assert!(document.insert_service(service).is_ok());
+
+    // Construct an Alias Output containing the DID document, with the wallet address
+    // set as both the state controller and governor.
+    let alias_output: AliasOutput = client.new_did_output(address, document, None).await?;
+
+    // Publish the Alias Output and get the published DID document.
+    let document: IotaDocument = client.publish_did_output(&secret_manager, alias_output).await?;
+    println!("Published DID document: {document:#}");
+
+    Ok((address, document, fragment))
 }
 
 
@@ -107,7 +123,7 @@ async fn main() -> anyhow::Result<()> {
     
     let storage_issuer: MemStorage = MemStorage::new(JwkMemStore::new(), KeyIdMemstore::new());
 
-    let (_, issuer_document, fragment_issuer): (Address, IotaDocument, String) = 
+    let (_, mut issuer_document, fragment_issuer): (Address, IotaDocument, String) = 
     create_did(&client, &mut secret_manager_issuer, &storage_issuer, JwkMemStore::BLS12381SHA256_KEY_TYPE, ProofAlgorithm::BLS12381_SHA256).await?;
 
     
@@ -126,7 +142,11 @@ async fn main() -> anyhow::Result<()> {
     // =========================================================================================
     // Step 1: Create a new RevocationTimeframeStatus containing the current validityTimeframe
     // =======================================================================================
-    let status: Status = RevocationTimeframeStatus::new(ValidityTimeframeGranularity::MINUTE).into();
+    let granularity = Duration::minutes(1);
+    // The issuer also chooses a unique `RevocationBitmap` index to be able to revoke it later.
+    let service_url = issuer_document.id().to_url().join("#my-revocation-service")?;
+    let credential_index: u32 = 5;
+    let status: Status = RevocationTimeframeStatus::new(granularity, service_url, credential_index)?.into();
 
 
     // Build credential using subject above and issuer.
@@ -167,23 +187,28 @@ async fn main() -> anyhow::Result<()> {
     println!("Sending credential (as JPT) to the holder: {}\n", credential_jpt.as_str());
 
 
-    let mut resolver: Resolver<IotaDocument> = Resolver::new();
-    resolver.attach_iota_handler(client);
-
-    let issuer: CoreDID = JptCredentialValidatorUtils::extract_issuer_from_issued_jpt(&credential_jpt).unwrap();
-    let issuer_document: IotaDocument = resolver.resolve(&issuer).await?;
-
-
     // Holder validate the credential and retrieve the JwpIssued, needed to construct the JwpPresented
-    let decoded_credential = JptCredentialValidator::validate::<_, Object>(
+    // Checks also that the credentialStatus is correct, otherwise asks Issuer for an update
+    let validation_result = JptCredentialValidator::validate::<_, Object>(
         &credential_jpt,
         &issuer_document,
         &JptCredentialValidationOptions::default(),
         FailFast::FirstError,
-    )
-    .unwrap();
+    );
+
+    if validation_result.as_ref().is_err_and(|e| matches!(e.validation_errors[0], JwtValidationError::Revoked)) {
+        //request update
+        todo!()
+    }
+
+    let decoded_credential = validation_result.unwrap();
+
+    // Holder checks if his credential has been revoked by the Issuer
+    let revocation_result = JptCredentialValidatorUtils::check_revocation_with_validity_timeframe_2024(&decoded_credential.credential, &issuer_document, StatusCheck::Strict);
+    assert!(revocation_result.is_ok());
 
 
+    
     let challenge: &str = "475a7984-1bb5-4c4c-a56f-822bccd46440";
 
     let method_id = decoded_credential.decoded_jwp.get_issuer_protected_header().kid().unwrap();
@@ -201,8 +226,6 @@ async fn main() -> anyhow::Result<()> {
         )
         .await?;
 
-    
-
 
     // Holder sends a Presentation JPT to the Verifier.
     println!("Sending presentation (as JPT) to the verifier: {}\n", presentation_jpt.as_str());
@@ -210,9 +233,6 @@ async fn main() -> anyhow::Result<()> {
     // ===========================================================================
     // Step 2a: Verifier receives the Presentation and verifies it.
     // ===========================================================================
-
-    let issuer: CoreDID = JptPresentationValidatorUtils::extract_issuer_from_presented_jpt(&presentation_jpt).unwrap();
-    let issuer_document: IotaDocument = resolver.resolve(&issuer).await?;
     
     let presentation_validation_options = 
         JptPresentationValidationOptions::default().nonce(challenge);
@@ -235,7 +255,7 @@ async fn main() -> anyhow::Result<()> {
     // Step 2b: Waiting for the next validityTimeframe, will result in the Credential being revoked
     // ===========================================================================
 
-    thread::sleep(Duration::from_secs(61)); //Will result revoked with GRANULARITY set to MINUTE
+    thread::sleep(SleepDuration::from_secs(61)); //Will result revoked with the granularity of 1 minute
 
     let validation_result = JptPresentationValidator::validate::<_, Object>(
         &presentation_jpt,
@@ -247,10 +267,88 @@ async fn main() -> anyhow::Result<()> {
     println!("VC validation result: {validation_result:?}");
 
     // We expect validation to no longer succeed because the credential was NOT updated.
-    assert!(matches!(
+    if matches!(
         validation_result.unwrap_err().validation_errors[0],
         JwtValidationError::Revoked
-    ));
+    ) {
+        println!("validityTimeframe NOT valid, Credential Revoked");
+    }
 
-  Ok(())
+
+    // ===========================================================================
+    // Update credential
+    // ===========================================================================
+
+    // Holder sends its credential to Issuer asking for an update
+
+    // Issuer checks the Credential integrity.
+    // Issuer checks Credential's status. If validityTimeframe still valid does not perform the update
+
+    let validation_result = JptCredentialValidator::validate::<_, Object>(
+        &credential_jpt,
+        &issuer_document,
+        &JptCredentialValidationOptions::default(),
+        FailFast::FirstError,
+    );
+    assert!(validation_result.as_ref().is_err_and(|e| matches!(e.validation_errors[0], JwtValidationError::Revoked)));
+
+
+    // Issuer checks if the Credential has been revoked
+
+    let revocation_result = JptCredentialValidatorUtils::check_revocation_with_validity_timeframe_2024(&decoded_credential.credential, &issuer_document, StatusCheck::Strict);
+    assert!(!revocation_result.is_err_and(|e| matches!(e, JwtValidationError::Revoked)));
+
+
+    // Issuer updates the credential
+
+    let new_credential_jpt = issuer_document
+        .update(
+            &storage_issuer, 
+            &fragment_issuer, 
+            granularity, 
+            &mut decoded_credential.decoded_jwp.clone())
+        .await?;
+
+
+    // Issuer sends back the credential updated
+
+    println!("Sending updated credential (as JPT) to the holder: {}\n", new_credential_jpt.as_str());
+
+    // Holder check validity of the updated credential
+
+    let validation_result = JptCredentialValidator::validate::<_, Object>(
+        &new_credential_jpt,
+        &issuer_document,
+        &JptCredentialValidationOptions::default(),
+        FailFast::FirstError,
+    );
+
+    assert!(!validation_result.as_ref().is_err_and(|e| matches!(e.validation_errors[0], JwtValidationError::Revoked)));
+    println!("Updated credential is VALID!");
+
+
+    // ===========================================================================
+    // Issuer decides to Revoke Holder's Credential
+    // ===========================================================================
+
+
+    // Update the RevocationBitmap service in the issuer's DID Document.
+    // This revokes the credential's unique index.
+
+    issuer_document.revoke_credentials("my-revocation-service", &[credential_index])?;
+    
+    // Publish the changes.
+    let alias_output: AliasOutput = client.update_did_output(issuer_document.clone()).await?;
+    let rent_structure: RentStructure = client.get_rent_structure().await?;
+    let alias_output: AliasOutput = AliasOutputBuilder::from(&alias_output)
+        .with_minimum_storage_deposit(rent_structure)
+        .finish()?;
+    issuer_document = client.publish_did_output(&secret_manager_issuer, alias_output).await?;
+
+
+    // Holder checks if his credential has been revoked by the Issuer
+    let revocation_result = JptCredentialValidatorUtils::check_revocation_with_validity_timeframe_2024(&decoded_credential.credential, &issuer_document, StatusCheck::Strict);
+    assert!(revocation_result.is_err_and(|e| matches!(e, JwtValidationError::Revoked)));
+    println!("Credential Revoked!");
+    Ok(())
 }
