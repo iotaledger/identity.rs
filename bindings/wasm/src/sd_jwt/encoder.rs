7dd5d5a6beeca7629305592557c35bcf694fdae1
Abdulrahim Al Methiab <aalmethiab@gmail.com>
2024-02-05 20:46:38 +0100

    update wasm bindings

--- bindings/wasm/src/sd_jwt/encoder.rs
+++ bindings/wasm/src/sd_jwt/encoder.rs
@@ -2,14 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::disclosure::WasmDisclosure;
-use crate::common::ArrayString;
 use crate::common::RecordStringAny;
 use crate::error::Result;
 use crate::error::WasmResult;
 use identity_iota::sd_jwt_payload::SdObjectEncoder;
 use identity_iota::sd_jwt_payload::Sha256Hasher;
-use js_sys::Array;
-use js_sys::JsString;
 use serde_json::Value;
 use wasm_bindgen::prelude::*;
 
@@ -32,51 +29,33 @@ impl WasmSdObjectEncoder {
   /// Substitutes a value with the digest of its disclosure.
   /// If no salt is provided, the disclosure will be created with a random salt value.
   ///
-  /// The value of the key specified in `path` will be concealed. E.g. for path
-  /// `["claim", "subclaim"]` the value of `claim.subclaim` will be concealed.
+  /// `path` indicates the pointer to the value that will be concealed using the syntax of
+  /// [JSON pointer](https://datatracker.ietf.org/doc/html/rfc6901).
   ///
-  /// ## Error
-  /// `InvalidPath` if path is invalid or the path slice is empty.
-  /// `DataTypeMismatch` if existing SD format is invalid.
+  /// For the following object:
   ///
-  /// ## Note
-  /// Use `concealArrayEntry` for values in arrays.
+  ///  ```
+  /// {
+  ///   "id": "did:value",
+  ///   "claim1": {
+  ///      "abc": true
+  ///   },
+  ///   "claim2": ["val_1", "val_2"]
+  /// }
+  /// ```
+  ///
+  /// Path "/id" conceals `"id": "did:value"`
+  /// Path "/claim1/abc" conceals `"abc": true`
+  /// Path "/claim2/0" conceals `val_1`
+  /// ```
+  ///
+  /// ## Errors
+  /// * `InvalidPath` if pointer is invalid.
+  /// * `DataTypeMismatch` if existing SD format is invalid.
+  ///
   #[wasm_bindgen(js_name = conceal)]
-  pub fn conceal(&mut self, path: ArrayString, salt: Option<String>) -> Result<WasmDisclosure> {
-    let path: Vec<String> = path
-      .dyn_into::<Array>()?
-      .iter()
-      .map(|item| item.dyn_into::<JsString>().map(String::from))
-      .collect::<Result<Vec<String>>>()?;
-    let path: Vec<&str> = path.iter().map(|s| &**s).collect();
+  pub fn conceal(&mut self, path: String, salt: Option<String>) -> Result<WasmDisclosure> {
     let disclosure = self.0.conceal(&path, salt).wasm_result()?;
-    Ok(WasmDisclosure(disclosure))
-  }
-
-  /// Substitutes a value within an array with the digest of its disclosure.
-  /// If no salt is provided, the disclosure will be created with random salt value.
-  ///
-  /// `path` is used to specify the array in the object, while `element_index` specifies
-  /// the index of the element to be concealed (index start at 0).
-  ///
-  /// ## Error
-  /// `InvalidPath` if path is invalid or the path slice is empty.
-  /// `DataTypeMismatch` if existing SD format is invalid.
-  /// `IndexOutofBounds` if `element_index` is out of bounds.
-  #[wasm_bindgen(js_name = concealArrayEntry)]
-  pub fn conceal_array_entry(
-    &mut self,
-    path: ArrayString,
-    element_index: usize,
-    salt: Option<String>,
-  ) -> Result<WasmDisclosure> {
-    let path: Vec<String> = path
-      .dyn_into::<Array>()?
-      .iter()
-      .map(|item| item.dyn_into::<JsString>().map(String::from))
-      .collect::<Result<Vec<String>>>()?;
-    let path: Vec<&str> = path.iter().map(|s| &**s).collect();
-    let disclosure = self.0.conceal_array_entry(&path, element_index, salt).wasm_result()?;
     Ok(WasmDisclosure(disclosure))
   }
 
@@ -103,7 +82,7 @@ impl WasmSdObjectEncoder {
   #[wasm_bindgen(js_name = encodeToObject)]
   pub fn encode_to_object(&self) -> Result<RecordStringAny> {
     Ok(
-      JsValue::from_serde(&self.0.object())
+      JsValue::from_serde(&self.0.object().wasm_result()?)
         .wasm_result()?
         .unchecked_into::<RecordStringAny>(),
     )
@@ -112,19 +91,13 @@ impl WasmSdObjectEncoder {
   /// Returns the modified object.
   #[wasm_bindgen(js_name = toJSON)]
   pub fn to_json(&self) -> Result<JsValue> {
-    JsValue::from_serde(&self.0.object()).wasm_result()
+    JsValue::from_serde(&self.0.object().wasm_result()?).wasm_result()
   }
 
   /// Adds a decoy digest to the specified path.
   /// If path is an empty slice, decoys will be added to the top level.
   #[wasm_bindgen(js_name = addDecoys)]
-  pub fn add_decoys(&mut self, path: ArrayString, number_of_decoys: usize) -> Result<()> {
-    let path: Vec<String> = path
-      .dyn_into::<Array>()?
-      .iter()
-      .map(|item| item.dyn_into::<JsString>().map(String::from))
-      .collect::<Result<Vec<String>>>()?;
-    let path: Vec<&str> = path.iter().map(|s| &**s).collect();
+  pub fn add_decoys(&mut self, path: String, number_of_decoys: usize) -> Result<()> {
     self.0.add_decoys(&path, number_of_decoys).wasm_result()?;
     Ok(())
   }
