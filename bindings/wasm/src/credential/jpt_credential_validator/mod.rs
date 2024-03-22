fd2070cf6e6d825d698db609c53b28b484c77c60
Enrico Marconi <enrico.marconi@hotmail.it>
2024-03-22 15:54:12 +0100

    binding coverage for jsonprooftoken

--- bindings/wasm/src/credential/jpt_credential_validator/mod.rs
+++ bindings/wasm/src/credential/jpt_credential_validator/mod.rs
@@ -2,10 +2,12 @@ mod decoded_jpt_credential;
 mod jpt_credential_validation_options;
 mod jpt_credential_validator;
 mod jpt_credential_validator_utils;
+mod jwp_credential_options;
 mod jwp_verification_options;
 
 pub use decoded_jpt_credential::*;
 pub use jpt_credential_validation_options::*;
 pub use jpt_credential_validator::*;
 pub use jpt_credential_validator_utils::*;
+pub use jwp_credential_options::*;
 pub use jwp_verification_options::*;
--- bindings/wasm/src/jpt/mod.rs
+++ bindings/wasm/src/jpt/mod.rs
@@ -1,7 +1,13 @@
 mod encoding;
-mod jpt_issued;
+mod jwp_issued;
+mod jwp_presentation_builder;
 mod payload;
+mod presentation_protected_header;
+mod proof_algorithm;
 
 pub use encoding::*;
-pub use jpt_issued::*;
+pub use jwp_issued::*;
+pub use jwp_presentation_builder::*;
 pub use payload::*;
+pub use presentation_protected_header::*;
+pub use proof_algorithm::*;
