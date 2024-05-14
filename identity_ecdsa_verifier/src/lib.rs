9abdb3868d76ccb39da2145346e201640448870a
Sven <sven.feuchtmueller@gmx.de>
2024-05-14 09:16:09 +0200

    Add EcDSA verifier (#1353)
    * add ecdsa verifier
    
    * add identity_ecdsa_verifier to workspace, add license headers
    
    * Update identity_ecdsa_verifier/Cargo.toml
    
    Co-authored-by: wulfraem <wulfraem@users.noreply.github.com>
    
    * Update identity_ecdsa_verifier/src/secp256k1.rs
    
    Co-authored-by: wulfraem <wulfraem@users.noreply.github.com>
    
    * Update identity_ecdsa_verifier/Cargo.toml
    
    Co-authored-by: wulfraem <wulfraem@users.noreply.github.com>
    
    * Update identity_ecdsa_verifier/src/secp256k1.rs
    
    Co-authored-by: wulfraem <wulfraem@users.noreply.github.com>
    
    * Update identity_ecdsa_verifier/src/secp256r1.rs
    
    Co-authored-by: wulfraem <wulfraem@users.noreply.github.com>
    
    * add feedback
    
    * add OpenSSL installation to windows runner in CI
    
    * update license headers and authors for ecdsa verifier
    
    * update license template to allow multiple contributors
    
    ---------
    
    Co-authored-by: Sebastian Wolfram <wulfraem@users.noreply.github.com>

---
+++ identity_ecdsa_verifier/src/lib.rs
@@ -0,0 +1,29 @@
+// Copyright 2020-2024 IOTA Stiftung, Filancore GmbH
+// SPDX-License-Identifier: Apache-2.0
+
+#![doc = include_str!("./../README.md")]
+#![warn(
+  rust_2018_idioms,
+  unreachable_pub,
+  missing_docs,
+  rustdoc::missing_crate_level_docs,
+  rustdoc::broken_intra_doc_links,
+  rustdoc::private_intra_doc_links,
+  rustdoc::private_doc_tests,
+  clippy::missing_safety_doc
+)]
+
+mod ecdsa_jws_verifier;
+#[cfg(feature = "es256k")]
+mod secp256k1;
+#[cfg(feature = "es256")]
+mod secp256r1;
+
+pub use ecdsa_jws_verifier::*;
+#[cfg(feature = "es256k")]
+pub use secp256k1::*;
+#[cfg(feature = "es256")]
+pub use secp256r1::*;
+
+#[cfg(test)]
+mod tests;
