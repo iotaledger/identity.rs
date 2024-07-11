2a782eda2c9f8eb007e7b8e4a45305ca73d2da01
Eike Haß <eike-hass@web.de>
2024-07-11 13:56:50 +0200

    add firefox specific config to enable subtle crypto

--- bindings/wasm/cypress.config.ts
+++ bindings/wasm/cypress.config.ts
@@ -1,14 +1,26 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-    screenshotOnRunFailure: false,
-    video: false,
-    requestTimeout: 10000,
-    defaultCommandTimeout: 60000,
-    retries: {
-        runMode: 3,
+  screenshotOnRunFailure: false,
+  video: false,
+  requestTimeout: 10000,
+  defaultCommandTimeout: 60000,
+  retries: {
+    runMode: 3,
+  },
+  e2e: {
+    supportFile: false,
+    // Fix to make subtle crypto work in cypress firefox
+    // https://github.com/cypress-io/cypress/issues/18217
+    setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.family === "firefox") {
+          launchOptions.preferences[
+            "network.proxy.testing_localhost_is_secure_when_hijacked"
+          ] = true;
+        }
+        return launchOptions;
+      });
     },
-    e2e: {
-        supportFile: false,
-    },
+  },
 });
