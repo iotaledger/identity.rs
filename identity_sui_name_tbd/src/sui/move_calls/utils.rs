89489415699807d64af9490540ddecc248d16584
Sebastian Wolfram <wulfraem@users.noreply.github.com>
2024-07-25 15:40:43 +0200

    update error handling
    - remove panics
    - align ptb.pure and ptb.obj error handling
    - align identifier parsing

--- identity_sui_name_tbd/src/sui/move_calls/utils.rs
+++ identity_sui_name_tbd/src/sui/move_calls/utils.rs
@@ -1,20 +1,18 @@
 use std::str::FromStr;
 
 use iota_sdk::types::base_types::ObjectID;
-use iota_sdk::types::Identifier;
 use move_core_types::account_address::AccountAddress;
 use move_core_types::language_storage::StructTag;
 
+use crate::utils::parse_identifier;
 use crate::Error;
 
 pub fn identity_tag(package_id: ObjectID) -> Result<StructTag, Error> {
   Ok(StructTag {
     address: AccountAddress::from_str(&package_id.to_string())
       .map_err(|err| Error::ParsingFailed(format!("package id\"{package_id}\" to account address; {err}")))?,
-    module: Identifier::from_str("identity")
-      .map_err(|err| Error::ParsingFailed(format!("\"identity\" to identifier; {err}")))?,
-    name: Identifier::from_str("Identity")
-      .map_err(|err| Error::ParsingFailed(format!("\"Identity\" to identifier; {err}")))?,
+    module: parse_identifier("identity")?,
+    name: parse_identifier("Identity")?,
     type_params: vec![],
   })
 }
--- identity_sui_name_tbd/src/utils.rs
+++ identity_sui_name_tbd/src/utils.rs
@@ -1,10 +1,17 @@
 // Copyright 2020-2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::str::FromStr;
+
 use anyhow::Context;
 use iota_sdk::types::base_types::IotaAddress;
+use iota_sdk::types::programmable_transaction_builder::ProgrammableTransactionBuilder;
+use iota_sdk::types::transaction::Argument;
+use iota_sdk::types::transaction::ObjectArg;
+use iota_sdk::types::Identifier;
 use iota_sdk::IotaClient;
 use iota_sdk::IotaClientBuilder;
+use serde::Serialize;
 use tokio::process::Command;
 
 use crate::Error;
@@ -39,4 +46,29 @@ pub async fn request_funds(address: &IotaAddress) -> anyhow::Result<()> {
   }
 
   Ok(())
+}
+
+pub(crate) fn parse_identifier(name: &str) -> Result<Identifier, Error> {
+  Identifier::from_str(name).map_err(|err| Error::ParsingFailed(format!(r#""{name}" to identifier; {err}"#)))
+}
+
+pub(crate) fn ptb_pure<T>(ptb: &mut ProgrammableTransactionBuilder, name: &str, value: T) -> Result<Argument, Error>
+where
+  T: Serialize + core::fmt::Debug,
+{
+  ptb.pure(&value).map_err(|err| {
+    Error::InvalidArgument(format!(
+      r"could not serialize pure value {name} with value {value:?}; {err}"
+    ))
+  })
+}
+
+pub(crate) fn ptb_obj(
+  ptb: &mut ProgrammableTransactionBuilder,
+  name: &str,
+  value: ObjectArg,
+) -> Result<Argument, Error> {
+  ptb
+    .obj(value)
+    .map_err(|err| Error::InvalidArgument(format!("could not serialize object {name} {value:?}; {err}")))
 }
