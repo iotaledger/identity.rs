4378537c32317f9882543c22655a35a08f218bf0
Eike Haß <eike-hass@web.de>
2024-08-29 16:09:47 +0200

    add docs

--- bindings/wasm/cypress.config.ts
+++ bindings/wasm/cypress.config.ts
@@ -10,14 +10,17 @@ export default defineConfig({
     },
     e2e: {
         supportFile: false,
-        // Fix to make subtle crypto work in cypress firefox
-        // https://github.com/cypress-io/cypress/issues/18217
         setupNodeEvents(on, config) {
             on("before:browser:launch", (browser, launchOptions) => {
                 if (browser.family === "firefox") {
+                    // Fix to make subtle crypto work in cypress firefox
+                    // https://github.com/cypress-io/cypress/issues/18217
                     launchOptions.preferences[
                         "network.proxy.testing_localhost_is_secure_when_hijacked"
                     ] = true;
+                    // Temporary fix to allow cypress to control Firefox via CDP
+                    // https://github.com/cypress-io/cypress/issues/29713
+                    // https://fxdx.dev/deprecating-cdp-support-in-firefox-embracing-the-future-with-webdriver-bidi/
                     launchOptions.preferences[
                         "remote.active-protocols"
                     ] = 3;
