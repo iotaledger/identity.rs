14fc0f14ac8e4062e553c588a4c6a5746b4fb13d
wulfraem <wulfraem@users.noreply.github.com>
2024-11-27 16:42:48 +0100

    Fix clippy and fmt issues (#1455)
    * fix cargo fmt issues
    
    * fix clippy issues

--- identity_iota_core/src/rebased/sui/move_calls/identity/mod.rs
+++ identity_iota_core/src/rebased/sui/move_calls/identity/mod.rs
@@ -3,19 +3,19 @@
 
 mod borrow_asset;
 mod config;
+mod controller_execution;
 mod create;
 mod deactivate;
 pub(crate) mod proposal;
 mod send_asset;
 mod update;
-mod controller_execution;
 mod upgrade;
 
 pub(crate) use borrow_asset::*;
 pub(crate) use config::*;
+pub(crate) use controller_execution::*;
 pub(crate) use create::*;
 pub(crate) use deactivate::*;
 pub(crate) use send_asset::*;
 pub(crate) use update::*;
-pub(crate) use controller_execution::*;
 pub(crate) use upgrade::*;
