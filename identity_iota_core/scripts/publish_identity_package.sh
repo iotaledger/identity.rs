abf20c75c519c6c7e747b2b285d5f6469d3d060a
Sebastian Wolfram <wulfraem@users.noreply.github.com>
2024-11-28 15:07:32 +0100

    comment out debug log in publishing script

--- identity_iota_core/scripts/publish_identity_package.sh
+++ identity_iota_core/scripts/publish_identity_package.sh
@@ -6,6 +6,6 @@
 script_dir=$(cd "$(dirname $0)" && pwd)
 package_dir=$script_dir/../packages/iota_identity
 
-echo "publishing package from $package_dir"
+# echo "publishing package from $package_dir"
 package_id=$(iota client publish --with-unpublished-dependencies --skip-dependency-verification --silence-warnings --json --gas-budget 500000000 $package_dir | jq --raw-output '.objectChanges[] | select(.type | contains("published")) | .packageId')
 export IOTA_IDENTITY_PKG_ID=$package_id
