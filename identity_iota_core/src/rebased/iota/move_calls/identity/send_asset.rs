11b487ead8536c7896b634c55acb7463d1362b66
umr1352 <enrico.marconi@iota.org>
2024-12-09 15:52:28 +0100

    send proposals are chain-executed if controller has enough voting power

--- identity_iota_core/src/rebased/iota/move_calls/identity/send_asset.rs
+++ identity_iota_core/src/rebased/iota/move_calls/identity/send_asset.rs
@@ -6,6 +6,7 @@ use iota_sdk::types::base_types::IotaAddress;
 use iota_sdk::types::base_types::ObjectID;
 use iota_sdk::types::base_types::ObjectRef;
 use iota_sdk::types::programmable_transaction_builder::ProgrammableTransactionBuilder;
+use iota_sdk::types::transaction::Argument;
 use iota_sdk::types::transaction::ObjectArg;
 use iota_sdk::types::transaction::ProgrammableTransaction;
 use iota_sdk::types::TypeTag;
@@ -17,13 +18,22 @@ use crate::rebased::utils::MoveType;
 
 use self::move_calls::utils;
 
-pub(crate) fn propose_send(
+struct SendProposalContext {
+  ptb: ProgrammableTransactionBuilder,
+  controller_cap: Argument,
+  delegation_token: Argument,
+  borrow: Argument,
+  identity: Argument,
+  proposal_id: Argument,
+}
+
+fn send_proposal_impl(
   identity: OwnedObjectRef,
   capability: ObjectRef,
   transfer_map: Vec<(ObjectID, IotaAddress)>,
   expiration: Option<u64>,
   package_id: ObjectID,
-) -> Result<ProgrammableTransaction, anyhow::Error> {
+) -> anyhow::Result<SendProposalContext> {
   let mut ptb = ProgrammableTransactionBuilder::new();
   let cap_arg = ptb.obj(ObjectArg::ImmOrOwnedObject(capability))?;
   let (delegation_token, borrow) = move_calls::utils::get_controller_delegation(&mut ptb, cap_arg, package_id);
@@ -37,7 +47,7 @@ pub(crate) fn propose_send(
     (objects, recipients)
   };
 
-  let _proposal_id = ptb.programmable_move_call(
+  let proposal_id = ptb.programmable_move_call(
     package_id,
     ident_str!("identity").into(),
     ident_str!("propose_send").into(),
@@ -45,24 +55,44 @@ pub(crate) fn propose_send(
     vec![identity_arg, delegation_token, exp_arg, objects, recipients],
   );
 
-  utils::put_back_delegation_token(&mut ptb, cap_arg, delegation_token, borrow, package_id);
+  Ok(SendProposalContext {
+    ptb,
+    identity: identity_arg,
+    controller_cap: cap_arg,
+    delegation_token,
+    borrow,
+    proposal_id,
+  })
+}
+
+pub(crate) fn propose_send(
+  identity: OwnedObjectRef,
+  capability: ObjectRef,
+  transfer_map: Vec<(ObjectID, IotaAddress)>,
+  expiration: Option<u64>,
+  package_id: ObjectID,
+) -> Result<ProgrammableTransaction, anyhow::Error> {
+  let SendProposalContext {
+    mut ptb,
+    controller_cap,
+    delegation_token,
+    borrow,
+    ..
+  } = send_proposal_impl(identity, capability, transfer_map, expiration, package_id)?;
+
+  utils::put_back_delegation_token(&mut ptb, controller_cap, delegation_token, borrow, package_id);
 
   Ok(ptb.finish())
 }
 
-pub(crate) fn execute_send(
-  identity: OwnedObjectRef,
-  capability: ObjectRef,
-  proposal_id: ObjectID,
+fn execute_send_impl(
+  ptb: &mut ProgrammableTransactionBuilder,
+  identity: Argument,
+  delegation_token: Argument,
+  proposal_id: Argument,
   objects: Vec<(ObjectRef, TypeTag)>,
   package: ObjectID,
-) -> Result<ProgrammableTransaction, anyhow::Error> {
-  let mut ptb = ProgrammableTransactionBuilder::new();
-  let identity = move_calls::utils::owned_ref_to_shared_object_arg(identity, &mut ptb, true)?;
-  let controller_cap = ptb.obj(ObjectArg::ImmOrOwnedObject(capability))?;
-  let (delegation_token, borrow) = move_calls::utils::get_controller_delegation(&mut ptb, controller_cap, package);
-  let proposal_id = ptb.pure(proposal_id)?;
-
+) -> anyhow::Result<()> {
   // Get the proposal's action as argument.
   let send_action = ptb.programmable_move_call(
     package,
@@ -71,8 +101,6 @@ pub(crate) fn execute_send(
     vec![SendAction::move_type(package)],
     vec![identity, delegation_token, proposal_id],
   );
-
-  utils::put_back_delegation_token(&mut ptb, controller_cap, delegation_token, borrow, package);
 
   // Send each object in this send action.
   // Traversing the map in reverse reduces the number of operations on the move side.
@@ -96,6 +124,50 @@ pub(crate) fn execute_send(
     vec![],
     vec![send_action],
   );
+
+  Ok(())
+}
+
+pub(crate) fn execute_send(
+  identity: OwnedObjectRef,
+  capability: ObjectRef,
+  proposal_id: ObjectID,
+  objects: Vec<(ObjectRef, TypeTag)>,
+  package: ObjectID,
+) -> Result<ProgrammableTransaction, anyhow::Error> {
+  let mut ptb = ProgrammableTransactionBuilder::new();
+  let identity = move_calls::utils::owned_ref_to_shared_object_arg(identity, &mut ptb, true)?;
+  let controller_cap = ptb.obj(ObjectArg::ImmOrOwnedObject(capability))?;
+  let (delegation_token, borrow) = move_calls::utils::get_controller_delegation(&mut ptb, controller_cap, package);
+  let proposal_id = ptb.pure(proposal_id)?;
+
+  execute_send_impl(&mut ptb, identity, delegation_token, proposal_id, objects, package)?;
+
+  utils::put_back_delegation_token(&mut ptb, controller_cap, delegation_token, borrow, package);
+
+  Ok(ptb.finish())
+}
+
+pub(crate) fn create_and_execute_send(
+  identity: OwnedObjectRef,
+  capability: ObjectRef,
+  transfer_map: Vec<(ObjectID, IotaAddress)>,
+  expiration: Option<u64>,
+  objects: Vec<(ObjectRef, TypeTag)>,
+  package: ObjectID,
+) -> anyhow::Result<ProgrammableTransaction> {
+  let SendProposalContext {
+    mut ptb,
+    identity,
+    controller_cap,
+    delegation_token,
+    borrow,
+    proposal_id,
+  } = send_proposal_impl(identity, capability, transfer_map, expiration, package)?;
+
+  execute_send_impl(&mut ptb, identity, delegation_token, proposal_id, objects, package)?;
+
+  utils::put_back_delegation_token(&mut ptb, controller_cap, delegation_token, borrow, package);
 
   Ok(ptb.finish())
 }
