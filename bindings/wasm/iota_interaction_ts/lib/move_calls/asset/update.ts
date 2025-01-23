e4ade829447c9de53fa1f8ada187ce4a44fb6efe
chrisgitiota <christof.gerritsma@iota.org>
2025-01-23 15:36:23 +0100

    Remove npm dependency @iota/iota-sdk from iota_interaction_ts package.json and use @iota/iota.js instead

--- bindings/wasm/iota_interaction_ts/lib/move_calls/asset/update.ts
+++ bindings/wasm/iota_interaction_ts/lib/move_calls/asset/update.ts
@@ -1,7 +1,7 @@
 // Copyright 2020-2024 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { ObjectRef, Transaction } from "@iota/iota-sdk/transactions";
+import { ObjectRef, Transaction } from "@iota/iota.js/transactions";
 
 export function update(
     asset: ObjectRef,
