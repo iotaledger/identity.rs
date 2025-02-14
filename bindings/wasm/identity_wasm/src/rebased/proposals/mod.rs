19d2a1f3d28c29f4e73af823bbb5a7964186be0a
umr1352 <enrico.marconi@iota.org>
2025-02-14 16:45:55 +0100

    DeactivateDid proposal

---
+++ bindings/wasm/identity_wasm/src/rebased/proposals/mod.rs
@@ -0,0 +1,14 @@
+// Copyright 2020-2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+mod deactivate_did;
+
+pub use deactivate_did::*;
+
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+extern "C" {
+  #[wasm_bindgen(typescript_type = "Set<string>")]
+  pub type StringSet;
+}
