559f400f1224e39a00a6fb22b3c70905f97921b3
Eike Haß <eike-hass@web.de>
2025-02-21 09:35:01 +0100

    use existing package deploy script

--- identity_iota_core/packages/iota_identity/deploy-package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
-set -o pipefail  # don't hide errors within pipes
-
-json_output=$(iota client publish --skip-dependency-verification --with-unpublished-dependencies --json --gas-budget 500000000 .)
-package_id=$(echo $json_output | jq --raw-output '.objectChanges[] | select(.type | contains("published")) | .packageId')
-echo "IOTA_IDENTITY_PKG_ID=$package_id"
