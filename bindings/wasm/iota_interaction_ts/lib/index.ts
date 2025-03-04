9c233fffbd3a70a9999606e673a0c3ceced6c18b
Eike Haß <eike-hass@web.de>
2025-03-04 08:17:45 +0100

    fmt

--- bindings/wasm/iota_interaction_ts/lib/index.ts
+++ bindings/wasm/iota_interaction_ts/lib/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2020-2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-export * as move_calls from "./move_calls";
 export * from "~iota_interaction_ts";
+export * as move_calls from "./move_calls";
