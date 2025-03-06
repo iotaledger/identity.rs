7aee4a8b51cb53d0215329299e4656aa6948ab1c
Eike Haß <eike-hass@web.de>
2025-03-06 10:03:44 +0100

    Fix Interaction Wasm build (#1588)

--- bindings/wasm/iota_interaction_ts/lib/index.ts
+++ bindings/wasm/iota_interaction_ts/lib/index.ts
@@ -1,4 +1,5 @@
 // Copyright 2020-2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+export * as iota_client_helpers from "./iota_client_helpers";
 export * as move_calls from "./move_calls";
