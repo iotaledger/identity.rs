bd96b1dc3beb229488724fcebf9a2587e6c606c9
Eike Haß <eike-hass@web.de>
2025-03-21 09:35:08 +0100

    simplify web example app

--- bindings/wasm/identity_wasm/cypress/app/src/main.ts
+++ bindings/wasm/identity_wasm/cypress/app/src/main.ts
@@ -1,28 +1,12 @@
-window.global ||= window;
-
-import "./style.css";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 import { runTest } from "./identity.ts";
 import typescriptLogo from "./typescript.svg";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
+    <h1>Identity</h1>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `;
 globalThis.runTest = runTest;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
