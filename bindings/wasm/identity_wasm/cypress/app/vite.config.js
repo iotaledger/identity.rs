b91bca412859f025120ce0a35c7df629f5f78f16
Eike Haß <eike-hass@web.de>
2025-03-21 20:20:18 +0100

    add all env vars to cypress docker image

--- bindings/wasm/identity_wasm/cypress/app/vite.config.js
+++ bindings/wasm/identity_wasm/cypress/app/vite.config.js
@@ -3,11 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig(({ command, mode }) => {
     return {
         define: {
+            // variables will be set during build time
             "process.env.IOTA_IDENTITY_PKG_ID": JSON.stringify(process.env.IOTA_IDENTITY_PKG_ID),
             "process.env.NETWORK_NAME_FAUCET": JSON.stringify(process.env.NETWORK_NAME_FAUCET),
             "process.env.NETWORK_URL": JSON.stringify(process.env.NETWORK_URL),
         },
         server: {
+            // open on default port or fail to make CI consistent
             strictPort: true,
         },
         build: {
