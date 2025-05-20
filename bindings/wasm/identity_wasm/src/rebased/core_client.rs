2ef0d73e5af1fdaf35edd28f45244c9cb8467ac7
chrisgitiota <christof.gerritsma@iota.org>
2025-05-20 13:22:03 +0200

    Remove redundant code and update use statements according to new core_client location

--- bindings/wasm/identity_wasm/src/rebased/core_client.rs
+++ bindings/wasm/identity_wasm/src/rebased/core_client.rs
@@ -6,6 +6,9 @@ use iota_interaction::types::base_types::ObjectID;
 use iota_interaction::types::base_types::ObjectIDParseError;
 use iota_interaction::types::crypto::PublicKey;
 use iota_interaction_ts::bindings::WasmIotaClient;
+use iota_interaction_ts::bindings::WasmTransactionSigner;
+use iota_interaction_ts::core_client::WasmCoreClientReadOnly;
+use iota_interaction_ts::core_client::WasmCoreClient;
 use iota_interaction_ts::IotaClientAdapter;
 use iota_interaction_ts::WasmPublicKey;
 use product_common::core_client::CoreClient;
@@ -15,36 +18,7 @@ use wasm_bindgen::prelude::*;
 
 use crate::error::Result;
 use crate::error::WasmResult;
-use crate::storage::WasmTransactionSigner;
 
-#[wasm_bindgen]
-extern "C" {
-  #[derive(Clone)]
-  #[wasm_bindgen(typescript_type = CoreClientReadOnly)]
-  pub type WasmCoreClientReadOnly;
-
-  #[wasm_bindgen(method, js_name = packageId)]
-  fn package_id(this: &WasmCoreClientReadOnly) -> String;
-
-  #[wasm_bindgen(method, js_name = network)]
-  fn network(this: &WasmCoreClientReadOnly) -> String;
-
-  #[wasm_bindgen(method, js_name = iotaClient)]
-  fn iota_client(this: &WasmCoreClientReadOnly) -> WasmIotaClient;
-
-  #[derive(Clone)]
-  #[wasm_bindgen(typescript_type = CoreClient, extends = WasmCoreClientReadOnly)]
-  pub type WasmCoreClient;
-
-  #[wasm_bindgen(method)]
-  fn signer(this: &WasmCoreClient) -> WasmTransactionSigner;
-
-  #[wasm_bindgen(method, js_name = senderAddress)]
-  fn sender_address(this: &WasmCoreClient) -> String;
-
-  #[wasm_bindgen(method, js_name = senderPublicKey)]
-  fn sender_public_key(this: &WasmCoreClient) -> WasmPublicKey;
-}
 
 #[derive(Clone)]
 #[wasm_bindgen]
